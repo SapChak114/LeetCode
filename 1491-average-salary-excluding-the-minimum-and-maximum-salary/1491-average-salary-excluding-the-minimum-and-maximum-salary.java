46f6eae2b5553dec284c0f58575c8c75b9dda19c
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-05-01 13:21:16 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (29.30%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -2,24 +2,23 @@ class Solution {
     public double average(int[] salary) {
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
-        
-        //System.out.println(min+"  "+max);
-        //[8000,9000,2000,3000,6000,1000]
+        double sal = 0;
         for(int i = 0; i<salary.length; i++){
             min = Math.min(min,salary[i]);
             max = Math.max(max,salary[i]);
-            //System.out.println(salary[i]);
+            sal += salary[i];
         }
-        int count = 0;
-        double sal = 0;
-        //System.out.println(min+"  "+max);
-        for(int i = 0; i<salary.length; i++){
-            if(salary[i]!=min && salary[i]!=max){
-                sal += salary[i];
-                count++;
-            }
-        }
+        // int count = 0;
+        // double sal = 0;
+        // for(int i = 0; i<salary.length; i++){
+        //     if(salary[i]!=min && salary[i]!=max){
+        //         sal += salary[i];
+        //         count++;
+        //     }
+        // }
+        sal -= min;
+        sal -= max;
         
-        return sal/count;
+        return sal/(salary.length-2);
     }
 }
