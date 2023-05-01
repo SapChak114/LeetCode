419e95519dd209ebbd8032297bc81e3cadca2630
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-05-01 13:23:46 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (39.35%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -8,17 +8,7 @@ class Solution {
             max = Math.max(max,salary[i]);
             sal += salary[i];
         }
-        // int count = 0;
-        // double sal = 0;
-        // for(int i = 0; i<salary.length; i++){
-        //     if(salary[i]!=min && salary[i]!=max){
-        //         sal += salary[i];
-        //         count++;
-        //     }
-        // }
-        sal -= min;
-        sal -= max;
-        
+        sal -= (min+max);
         return sal/(salary.length-2);
     }
 }
