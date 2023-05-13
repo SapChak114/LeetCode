d0334b417d5eab5685a1e858b22672c81cebdd64
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-05-13 21:08:10 +0530

    Time: 103 ms (21.44%), Space: 50.1 MB (44.32%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.java
+++ 0403-frog-jump/0403-frog-jump.java
@@ -7,33 +7,32 @@ class Solution {
         
         for(int i = 1; i<stones.length; i++){
             int pos = stones[i];
-            System.out.println(pos);
             if(map.containsKey(pos)){
-                Set<Integer> s0 = map.get(pos);
-                System.out.println(s0);
-                System.out.println();
-                for(int it : new HashSet<>(s0)){
-                    Set<Integer> s1 = map.get(pos + it);
-                    if(s1!=null) s1.add(it);
-                    else{
-                        s1 = new HashSet<>();
-                        s1.add(it);
+                for(int it : new HashSet<>(map.get(pos))){
+                    Set<Integer> set1 = map.get(pos+it);
+                    if(set1!=null){
+                        set1.add(it);
+                    } else{
+                        set1 = new HashSet<>();
+                        set1.add(it);
                     }
-                    map.put(pos + it,s1);
-                    Set<Integer> s2 = map.get(pos + it+1);
-                    if(s2!=null) s2.add(it+1);
-                    else{
-                        s2 = new HashSet<>();
-                        s2.add(it+1);
+                    map.put(pos+it,set1);
+                    Set<Integer> set2 = map.get(pos+it+1);
+                    if(set2!=null){
+                        set2.add(it+1);
+                    } else{
+                        set2 = new HashSet<>();
+                        set2.add(it+1);
                     }
-                    map.put(pos + it+1,s2);
-                    Set<Integer> s3 = map.get(pos + it-1);
-                    if(s3!=null) s3.add(it-1);
-                    else{
-                        s3 = new HashSet<>();
-                        s3.add(it-1);
+                    map.put(pos+it+1,set2);
+                    Set<Integer> set3 = map.get(pos+it-1);
+                    if(set3!=null){
+                        set3.add(it-1);
+                    } else{
+                        set3 = new HashSet<>();
+                        set3.add(it-1);
                     }
-                    map.put(pos + it-1,s3);
+                    map.put(pos+it-1,set3);
                 }
             }
         }
