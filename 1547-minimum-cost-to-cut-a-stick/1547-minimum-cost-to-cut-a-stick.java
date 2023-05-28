ce25b2da926d79e81d6af44694acb085ea4b4b73
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-05-28 15:28:55 +0530

    Time: 25 ms (57.65%), Space: 41.6 MB (94.02%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -1,8 +1,8 @@
 class Solution {
     int[][] dp;
-    LinkedList<Integer> cuts;
+    List<Integer> cuts;
     public int minCost(int n, int[] cutsArr) {
-        cuts = new LinkedList<>();
+        cuts = new ArrayList<>();
         cuts.add(0);
         
         for(int cut : cutsArr){
@@ -18,23 +18,21 @@ class Solution {
         for(int[] d : dp){
             Arrays.fill(d,-1);
         }
+        
         Collections.sort(cuts);
         return rec(1,c);
     }
     
     int rec(int i, int j){
-        if(i>j){
-            return 0;
-        }
+        if(i>j) return 0;
         
         if(dp[i][j]!=-1) return dp[i][j];
         
+        int len = cuts.get(j+1) - cuts.get(i-1);
         int ans = Integer.MAX_VALUE;        
+        
         for(int ind = i; ind<=j; ind++){
-            int res = cuts.get(j+1) - cuts.get(i-1)
-                        + rec(i,ind-1) + rec(ind+1,j);
-            
-            ans = Math.min(ans,res);
+            ans = Math.min(ans,len + rec(i,ind-1) + rec(ind+1,j));
         }
         
         return dp[i][j] = ans;
