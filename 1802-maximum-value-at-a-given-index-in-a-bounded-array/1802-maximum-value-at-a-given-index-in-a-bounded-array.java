5bc5c2456b96fd4037a27b566810587eab45638b
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-06-10 17:07:48 +0530

    Time: 1 ms (100.00%), Space: 40 MB (24.60%) - LeetHub

--- 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.java
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.java
@@ -1,20 +1,18 @@
 class Solution {
     public int maxValue(int n, int index, int maxSum) {
-        long r = n - index - 1;
         long l = index;
+        long r = n - index - 1;
         
-        long hi = maxSum;
         long lo = 1;
-        
+        long hi = maxSum;
         long res = 0;
         
         while(lo<=hi){
             long mid = lo + (hi-lo)/2;
-            long sum = mid;
-            
-            long ls = 0;//sum on left
-            long rs = 0;//sum on right
+            long ans = mid;
             long m = mid - 1;
+            long rs = 0;
+            long ls = 0;
             
             if(r<=m){
                 rs = m*(m+1)/2 - (m-r)*(m-r+1)/2;
@@ -28,11 +26,11 @@ class Solution {
                 ls = m*(m+1)/2 + 1*(l-m);
             }
             
-            sum +=ls + rs;
+            ans += ls + rs;
             
-            if(sum<=maxSum){
-                res=mid;
-                lo=mid+1;
+            if(ans<=maxSum){
+                res = mid;
+                lo = mid+1;
             } else{
                 hi = mid-1;
             }
