533ced2fe4e78ac9e9303a054ce3e804412fd8c1
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-06-14 00:19:38 +0530

    Time: 17 ms (12.58%), Space: 43.7 MB (28.62%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.java
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.java
@@ -1,50 +1,59 @@
+class Vertex implements Comparable<Vertex>{
+    int dist;
+    int label;
+    
+    public Vertex(int dist,int label){
+        this.dist=dist;
+        this.label=label;
+    }
+    
+    public int compareTo(Vertex v) {
+        return this.dist - v.dist;
+    }
+}
+
 class Solution {
     public int snakesAndLadders(int[][] board) {
         int n = board.length;
-        Pair<Integer, Integer>[] cells = new Pair[n * n + 1];
-        int label = 1;
+        
+        Pair<Integer,Integer>[] cells = new Pair[n*n+1];
+        
         Integer[] columns = new Integer[n];
-        for (int i = 0; i < n; i++) {
-            columns[i] = i;
+        
+        for(int i = 0; i<n; i++){
+            columns[i]=i;
         }
-        for (int row = n - 1; row >= 0; row--) {
-            for (int column : columns) {
-                cells[label++] = new Pair<>(row, column);
+        
+        int pos = 1;
+        for(int i = n-1; i>=0; i--){
+            for(int col : columns){
+                cells[pos++]=new Pair(i,col);
             }
             Collections.reverse(Arrays.asList(columns));
         }
-        int[] dist = new int[n * n + 1];
-        Arrays.fill(dist, -1);
-        class Vertex implements Comparable<Vertex> {
-            int distance, label;
-
-            public Vertex(int distance, int label) {
-                this.distance = distance;
-                this.label = label;
-            }
-
-            public int compareTo(Vertex v) {
-                return this.distance - v.distance;
-            }
-        }
-        PriorityQueue<Vertex> q = new PriorityQueue<Vertex>();
-        dist[1] = 0;
-        q.add(new Vertex(0, 1));
-        while (!q.isEmpty()) {
-            Vertex vertex = q.remove();
-            int d = vertex.distance, curr = vertex.label;
-            if (d != dist[curr]) {
+        
+        PriorityQueue<Vertex> pq = new PriorityQueue<>();
+        int[] dist = new int[n*n+1];
+        Arrays.fill(dist,-1);
+        pq.add(new Vertex(0,1));
+        dist[1]=0;
+        
+        while(!pq.isEmpty()){
+            Vertex vertex = pq.poll();
+            int d = vertex.dist, curr = vertex.label;
+            if(d!=dist[curr]){
                 continue;
             }
-            for (int next = curr + 1; next <= Math.min(curr + 6, n * n); next++) {
-                int row = cells[next].getKey(), column = cells[next].getValue();
-                int destination = board[row][column] != -1 ? board[row][column] : next;
-                if (dist[destination] == -1 || dist[curr] + 1 < dist[destination]) {
-                    dist[destination] = dist[curr] + 1;
-                    q.add(new Vertex(dist[destination], destination));
+            for(int next = curr+1; next<=Math.min(curr+6,n*n); next++){
+                int row = cells[next].getKey(), col = cells[next].getValue();
+                int dest = board[row][col]!=-1?board[row][col]:next;
+                if(dist[dest]==-1 || dist[curr] + 1 < dist[dest]){
+                    dist[dest] = dist[curr]+1;
+                    pq.add(new Vertex(dist[dest],dest));
                 }
             }
         }
-        return dist[n * n];
+        
+        return dist[n*n];
     }
 }
