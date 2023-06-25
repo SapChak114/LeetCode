11e129beaa1cdc01560baa4474ef09b626d1a223
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-06-25 22:39:22 +0530

    Time: 817 ms (11.82%), Space: 71.6 MB (9.29%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.java
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.java
@@ -1,15 +1,18 @@
 class Solution {
+    int prime = (int)1e9+7;
     public int longestArithSeqLength(int[] nums) {
-        int maxLength = 0;
-        HashMap<Integer, Integer>[] dp = new HashMap[nums.length];
-        for (int right = 0; right < nums.length; ++right) {
-            dp[right] = new HashMap<>();
-            for (int left = 0; left < right; ++left) {
-                int diff = nums[left] - nums[right];
-                dp[right].put(diff, dp[left].getOrDefault(diff, 1) + 1);
-                maxLength = Math.max(maxLength, dp[right].get(diff));
+        Map<Long,Integer> map = new HashMap<>();
+        int n = nums.length;
+        
+        for(int right = 0; right<n; right++){
+            for(int left=0; left<right; left++){
+                int d = nums[right]-nums[left];
+                long numRight = (long)right*prime + (long)d;
+                long numLeft = (long)left*prime + (long)d;
+                map.put(numRight,map.getOrDefault(numLeft,1)+1);
             }
         }
-        return maxLength;
+        
+        return Collections.max(map.values());
     }
 }
