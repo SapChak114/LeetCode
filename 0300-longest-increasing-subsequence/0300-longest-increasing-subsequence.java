80792b4e5740b9ad39cc378ccf6854093cca1d34
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-06-25 21:35:18 +0530

    Time: 5 ms (87.84%), Space: 43.2 MB (48.82%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,27 +1,31 @@
 class Solution {
-    Map<Integer,Integer> map;
-    public int lengthOfLIS(int[] nums) {
-        map = new HashMap<>();
-        return dfs(nums,0,-1);
+    int bisectLeft(List<Integer> nums, int targ){
+        int l = 0, r = nums.size();
+        
+        while(l<r){
+            int mid = (l+r)/2;
+            
+            if(nums.get(mid)<targ){
+                l = mid+1;
+            } else{
+                r = mid;        
+            }
+        }
+        
+        return l;
     }
-    int dfs(int[] nums,int idx, int mtn){
+    
+    public int lengthOfLIS(int[] nums) {
+        List<Integer> res = new ArrayList<>();
         
-        if(idx==nums.length){
-            return 0;
+        for(int num : nums){
+            int idx = bisectLeft(res,num);
+            if(idx==res.size()){
+                res.add(num);
+            }
+            res.set(idx,num);
         }
         
-        if(map.containsKey(mtn)) return map.get(mtn);
-        
-        int take=0,dont=0;
-        if(mtn==-1 || (nums[idx]>nums[mtn])){
-            take = dfs(nums,idx+1,idx)+1;
-        }
-        dont = dfs(nums,idx+1,mtn);
-        
-        int result = Math.max(take,dont);
-        
-        map.put(mtn,result);
-        
-        return result;
+        return res.size();
     }
 }
