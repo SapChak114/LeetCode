7dc1719d1d370baab7e10d78b754a027ba81c702
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-06-30 21:38:34 +0530

    Time: 497 ms (6.10%), Space: 55.9 MB (7.93%) - LeetHub

--- 0864-shortest-path-to-get-all-keys/0864-shortest-path-to-get-all-keys.java
+++ 0864-shortest-path-to-get-all-keys/0864-shortest-path-to-get-all-keys.java
@@ -1,82 +1,138 @@
-class Solution {
-    public int shortestPathAllKeys(String[] grid) {
-        int m = grid.length, n = grid[0].length();
-        Queue<int[]> queue = new LinkedList<>();
-        int[][] moves = new int[][] {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-        
-        // seen['key'] is only for BFS with key state equals 'keys'
-        Map<Integer, Set<Pair<Integer, Integer>>> seen = new HashMap<>();
-        
-        Set<Character> keySet = new HashSet<>();
-        Set<Character> lockSet = new HashSet<>();
-        int allKeys = 0;
-        int startR = -1, startC = -1;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                char cell = grid[i].charAt(j);
-                if (cell >= 'a' && cell <= 'f') {
-                    allKeys += (1 << (cell - 'a'));
-                    keySet.add(cell);
-                }
-                if (cell >= 'A' && cell <= 'F') {
-                    lockSet.add(cell);
-                }
-                if (cell == '@') {
-                    startR = i;
-                    startC = j;
-                }
-            }
-        }
+class Pair{
+    int x;
+    int y;
+    String keys;
+    int steps;
+    
+    public Pair(int x, int y, String keys, int steps){
+        this.x = x;
+        this.y = y;
+        this.keys = keys;
+        this.steps = steps;
+    }
+    
+    public String toString(){
+        return x+" "+y+" "+keys;
+    }
+}
 
-        // [row, column, key state, distance]
-        queue.offer(new int[] {startR, startC, 0, 0});
-        seen.put(0, new HashSet<>());
-        seen.get(0).add(new Pair<>(startR, startC));    
+class Solution {
+    int[] dirX = {0,1,0,-1};
+    int[] dirY = {1,0,-1,0};
+    
+    public int shortestPathAllKeys(String[] grid) {
+        List<List<Character>> li = new ArrayList<>();
         
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int curR = cur[0], curC = cur[1], keys = cur[2], dist = cur[3];
-            for (int[] move : moves) {
-                int newR = curR + move[0], newC = curC + move[1];
+        for(String s : grid){
+            char[] ch = s.toCharArray();
+            List<Character> cList = new ArrayList<>();
+            for(int i = 0; i<ch.length; i++){
+                cList.add(ch[i]);
+            }
+            li.add(cList);
+        }
+        
+        int[] posOfAt = findPosOfAt(li);
+        String ke = "";
+        
+        for(int i = 0; i<posOfAt[2]; i++){
+            ke += "0";
+        }
+        
+        Queue<Pair> q = new LinkedList<>();
+        
+        Set<String> set = new HashSet<>();
+        
+        Pair p = new Pair(posOfAt[0],posOfAt[1],ke,0);
+        
+        set.add(p.toString());
+        
+        q.add(p);
+        
+        while(!q.isEmpty()){
+            Pair pp = q.poll();
+            System.out.println(pp);
+            int x = pp.x;
+            int y = pp.y;
+            String keys = pp.keys;
+            int steps = pp.steps;
+            
+            for(int i = 0; i<dirX.length; i++){
+                int ii = x + dirX[i];
+                int jj = y + dirY[i];
                 
-                // If this cell (newR, newC) is reachable.
-                if (newR >= 0 && newR < m && newC >= 0 && newC < n && grid[newR].charAt(newC) != '#') {
-                    char cell = grid[newR].charAt(newC);
-                    
-                    // If it is a key:
-                    if (keySet.contains(cell)) {
-                        // If we have collected it before, no need to revisit this cell.
-                        if (((1 << (cell - 'a')) & keys) != 0) {
-                            continue;
+                if(check(li,ii,jj)){
+                    //System.out.println(li.get(ii).get(jj)+" "+ii+" "+jj);
+                    if(isLock(li,ii,jj)){
+                      //  System.out.println(keys);
+                        int index = li.get(ii).get(jj)-65;
+                        if(keys.charAt(index)=='1'){
+                            Pair qp = new Pair(ii,jj,keys,steps+1);
+                            if(set.contains(qp.toString())) continue;
+                            q.add(qp);
+                            set.add(qp.toString());
                         }
-                        
-                        // Otherwise, we can walk to this cell and pick it up.
-                        int newKeys = (keys | (1 << (cell - 'a')));
-                        
-                        // If we collect all keys, return dist + 1. 
-                        // Otherwise, just add this state to seen and queue.
-                        if (newKeys == allKeys) {
-                            return dist + 1;
-                        }
-                        seen.putIfAbsent(newKeys, new HashSet<>());
-                        seen.get(newKeys).add(new Pair<>(newR, newC));
-                        queue.offer(new int[] {newR, newC, newKeys, dist + 1});
                     }
-                    
-                    // If it is a lock and we don't have its key, continue.
-                    if (lockSet.contains(cell) && ((keys & (1 << (cell - 'A'))) == 0)) {
-                        continue;
-                    }
-                        
-                    // We can walk to this cell if we haven't been here before with the same key state.
-                    else if (!seen.get(keys).contains(new Pair<>(newR, newC))) {
-                        seen.get(keys).add(new Pair<>(newR, newC));
-                        queue.offer(new int[] {newR, newC, keys, dist + 1});
+                    else if(isKey(li,ii,jj)){
+                        int index = li.get(ii).get(jj)-97;
+                        char[] ch = keys.toCharArray();
+                        ch[index]='1';
+                        //System.out.println(keys);
+                        boolean flag = true;
+                        for(char c : ch) if(c=='0') flag=false;
+                        if(flag) return (steps + 1);
+                        String key = new String(ch);
+                        Pair qp = new Pair(ii,jj,key,steps+1);
+                        if(set.contains(qp.toString())) continue;
+                        q.add(qp);
+                        set.add(qp.toString());
+                    } else{
+                        Pair qp = new Pair(ii,jj,keys,steps+1);
+                        if(set.contains(qp.toString())) continue;
+                        q.add(qp);
+                        set.add(qp.toString());
                     }
                 }
             }
         }
         
         return -1;
+        
+    }
+    
+    boolean isLock(List<List<Character>> li,int i, int j){
+        if(li.get(i).get(j)>=65 && li.get(i).get(j)<=90) return true;
+        return false;
+    }
+    
+    boolean isKey(List<List<Character>> li,int i, int j){
+        if(li.get(i).get(j)>=97 && li.get(i).get(j)<=122) return true;
+        return false;
+    }
+    
+    boolean check(List<List<Character>> ch, int i, int j){
+        int n = ch.size();
+        int m = ch.get(0).size();
+        if(i<0 || j<0 || i>=n || j>=m || ch.get(i).get(j)=='#') return false;
+        return true;
+    }
+    
+    int[] findPosOfAt(List<List<Character>> ch){
+        int a = 0;
+        int b = 0;
+        int keysCount = 0;
+        for(int i = 0 ; i<ch.size(); i++){
+            for(int j = 0; j<ch.get(i).size(); j++){
+                if(ch.get(i).get(j)=='@'){
+                    a=i;
+                    b=j;
+                }
+                if(ch.get(i).get(j)>=97 && ch.get(i).get(j)<=122){
+                    keysCount++;
+                }
+            }
+        }
+        
+        return new int[]{a,b,keysCount};
     }
 }
