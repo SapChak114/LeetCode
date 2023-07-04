949febc3ff7c969039e06b873c1bc0d97f8ed103
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-07-04 23:43:07 +0530

    Time: 36 ms (10.95%), Space: 45.5 MB (5.55%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int i = 1;
+        int n = ratings.length;
+        int total = n;
+            
+        while(i<n){
+            System.out.println(total);
+            if(ratings[i-1]==ratings[i]){
+                i++;
+                continue;
+            }
+        
+            int peek = 0;
+            while(ratings[i-1]<ratings[i]){
+                peek++;
+                total += peek;
+                i++;
+                if(i == n) return total;
+            }
+
+            int slope = 0;
+            while(i<n && ratings[i-1]>ratings[i]){
+                slope++;
+                total += slope;
+                i++;
+            }
+            total -= Math.min(peek,slope);
+        }
+        
+        return total;
+    }
+}
