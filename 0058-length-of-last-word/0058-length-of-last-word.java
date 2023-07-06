c670bcd6801cc8607366bbb7eb717e6e5bc17f98
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-07-06 06:54:59 +0530

    Time: 0 ms (100.00%), Space: 41 MB (26.84%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int len = 0;
+        s= s.trim();
+        for(int i = s.length()-1; i>=0; i--){
+            if(s.charAt(i)==' ') return len;
+            else len++;
+        }
+        
+        return len;
+    }
+}
