c0ce130aa2a41578417e7363198e9b4d836a2008
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-07-09 16:12:08 +0530

    Time: 1 ms (99.32%), Space: 41.6 MB (63.18%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.java
+++ 0859-buddy-strings/0859-buddy-strings.java
@@ -1,25 +1,34 @@
 class Solution {
-    String goal;
-    public boolean buddyStrings(String s, String goal) {
-        if(s.length() != goal.length()) return false;
+    public boolean buddyStrings(String s, String g) {
+        if(s.length()!=g.length()) return false;
         
-        if(s.equals(goal)){
-            Set<Character> set = new HashSet<>();
-            for(char c : s.toCharArray()){
-                if(set.contains(c)) return true;
-                set.add(c);
+        if(s.equals(g)){
+            char[] ch = new char[26];
+            
+            for(int i = 0; i<s.length(); i++){
+                ch[s.charAt(i) - 'a']++;
+                if(ch[s.charAt(i) -'a']==2) return true;
             }
             return false;
-        } else{
-            List<Integer> list = new ArrayList<>();
-            for(int i = 0; i<s.length(); i++){
-                if(s.charAt(i)!=goal.charAt(i)){
-                    list.add(i);
-                }
-            }
-            
-            return list.size()==2 && s.charAt(list.get(0))==goal.charAt(list.get(1)) && goal.charAt(list.get(0))==s.charAt(list.get(1));
         }
+        
+        int firstChar=-1, secondChar=-1;
+        
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i)!=g.charAt(i)){
+                if(firstChar==-1){
+                    firstChar=i;
+                }
+                else if(secondChar==-1){
+                    secondChar=i;
+                }
+                
+                else if(secondChar!=-1 && firstChar!=-1) return false;
+            }
+        }
+        
+        if(secondChar==-1) return false;
+        
+        return s.charAt(firstChar)==g.charAt(secondChar) && g.charAt(firstChar)==s.charAt(secondChar);
     }
-    
 }
