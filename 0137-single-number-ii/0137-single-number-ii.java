e1555555c57ddc8d0210556357460be1980800e2
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-07-09 15:16:38 +0530

    Time: 4 ms (62.75%), Space: 44.2 MB (37.44%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,13 +1,23 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int one = 0;
-        int two = 0;
+        int res = 0, n = nums.length;
         
-        for(int num : nums){
-            one = (one^num) & ~two;
-            two = (two^num) & ~one;
+        for(int i=0; i<32; i++){
+            int one = 0;
+            
+            for(int num : nums){
+                if(((num>>i)&1)==1){
+                    one++;
+                }
+            }
+            
+            one %=3;
+            
+            if(one!=0){
+                res |= 1<<i;
+            }
         }
         
-        return one;
+        return res;
     }
 }
