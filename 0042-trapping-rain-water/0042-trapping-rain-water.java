201513f8f4e1322767ca73567ceae32dfc7fabf0
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-07-09 16:46:21 +0530

    Time: 1 ms (96.09%), Space: 44.8 MB (22.08%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -4,9 +4,9 @@ class Solution {
         int n = ar.length, l = 0, r = n-1, maxOfMin = Integer.MIN_VALUE, water = 0;
         
         while(l<=r){            
-            int min = 0;
-            if(ar[l]<ar[r]) min = ar[l++];
-            else min = ar[r--];
+            int min = Math.min(ar[l],ar[r]);
+            if(ar[l]<ar[r]) l++;
+            else r--;
             
             maxOfMin = Math.max(maxOfMin,min);
             water += maxOfMin - min;
