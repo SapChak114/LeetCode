11e04b5d84056dcab9f76c679d7524330e0d1f09
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-07-22 20:23:11 +0530

    Time: 698 ms (7.13%), Space: 95 MB (16.36%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,20 +1,27 @@
 class Solution {
+    int[] nums;
+    int[][] dp;
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp,1);
-        
-        for(int right = 0; right<n; right++){
-            for(int left = 0; left<right; left++){
-                if(nums[left]<nums[right]){
-                    dp[right] = Math.max(dp[right],1+dp[left]);
-                }
-            }
+        this.nums = nums;
+        dp = new int[n+1][n+1];
+        for(int[] d: dp) Arrays.fill(d,Integer.MIN_VALUE);
+        return rec(0,-1);
+    }
+    
+    int rec(int idx, int prev){
+        if(idx==nums.length){
+            return 0;
         }
         
-        int max = 0;
-        for(int d : dp) max = Math.max(max,d);
+        if(dp[idx][prev+1]!=Integer.MIN_VALUE) return dp[idx][prev+1];
         
-        return max;
+        int take = 0;
+        int dont = rec(idx+1,prev);
+        if(prev==-1) take = (1 + rec(idx+1,idx));
+        else take = (nums[prev] < nums[idx])? (1 + rec(idx+1,idx)): dont;
+        
+        dp[idx][prev+1] = Math.max(take,dont);
+        return Math.max(take,dont);
     }
 }
