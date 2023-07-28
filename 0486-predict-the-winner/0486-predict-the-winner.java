952389f937c034a24179ef114f9499b86c79194b
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-07-28 21:18:11 +0530

    Time: 10 ms (36.95%), Space: 41.3 MB (5.31%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,24 +1,46 @@
 class Solution {
-    int[][] dp;
-    int[] nums;
+    Map<String,Integer> map;
     public boolean PredictTheWinner(int[] nums) {
-        int n = nums.length;
-        this.nums = nums;
-        dp = new int[n+1][n+1];
-        for(int[] d : dp) Arrays.fill(d,-1);
-        return rec(0,n-1)>=0;
-    }
-    
-    int rec(int left, int right){
-        if(left == right){
-            return nums[left];
+        map = new HashMap<>();
+        int player1 = rec(nums,0,nums.length-1,1);
+        int sum = 0;
+        for(int s : nums){
+            sum += s;
         }
         
-        if(dp[left][right]!=-1) return dp[left][right];
+        int player2 = sum - player1;
         
-        int leftTake = nums[left] - rec(left+1,right);
-        int rightTake = nums[right] - rec(left,right-1);
-        
-        return dp[left][right] = Math.max(leftTake,rightTake);
+        return player1>=player2;
     }
+    
+    
+    public int rec(int[] nums, int left, int right,int player){
+        if(left>right){
+            return 0;
+        }
+        
+        String key = left+"-"+right+"-"+player;
+        
+        if(map.containsKey(key)) return map.get(key);
+        
+        if(player==1){
+            int first = nums[left] + rec(nums,left+1,right,2);
+            int second = nums[right] + rec(nums,left,right-1,2);
+            
+            map.put(key,Math.max(first,second));
+            
+            return Math.max(first,second);
+        } else{
+            
+            int first = rec(nums,left+1,right,1);
+            int second = rec(nums,left,right-1,1);
+            
+            map.put(key,Math.min(first,second));
+            
+            return Math.min(first,second);
+        }
+        
+    }
+    
+    
 }
