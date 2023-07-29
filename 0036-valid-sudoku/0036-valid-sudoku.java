b2f123ce33d4753b3b4a53516c9ee0ec60f9ede2
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-07-29 13:44:36 +0530

    Time: 15 ms (14.75%), Space: 44 MB (25.15%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,60 +1,21 @@
 class Solution {
-    char[][] board;
     public boolean isValidSudoku(char[][] board) {
-        this.board = board;
-        return sudoku(0);
-    }
-    
-    boolean sudoku(int pos){
-        if(pos==81){
-            return true;
-        }
-        int row = pos/9;
-        int col = pos%9;
-        
-        if(board[row][col]!='.' && check(row,col,board[row][col],pos)){
-            return sudoku(pos+1);
-        }
-        else if(board[row][col]=='.'){
-            return sudoku(pos+1);
-        }
-        
-        return false;
-    }
-    
-    boolean check(int row, int col, char elem, int pos){
-        
-        int r = pos/9;
-        int c = pos%9;
-        
-        int rowCell = (r/3)*3;
-        int colCell = (c/3)*3;
-        
-        int count = 0;
-        for(int i = 0; i<9; i++){
-            if(board[i][col]==elem){
-                count++;
-            }
-            if(count>1) return false;
-        }
-        count = 0;
-        for(int i = 0; i<9; i++){
-            if(board[row][i]==elem){
-                count++;
-            }
-            if(count>1) return false;
-        }
-        
-        count = 0;
-        for(int i = rowCell; i<rowCell+3; i++){
-            for(int j = colCell; j<colCell+3; j++){
-                if(board[i][j]==elem){
-                    count++;
+        HashSet<String> hs = new HashSet<>();
+        int n = board.length;
+        int m = board[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j] == '.') continue;
+                int num = board[i][j];
+                String rowKey = "r"+i+"-"+num;
+                String colKey = "c"+j+"-"+num;
+                String bolKey = "r"+(i/3)+"c"+(j/3)+"-"+num;
+                for(String key : Arrays.asList(rowKey, colKey, bolKey)){
+                    if(hs.contains(key)) return false;
+                    hs.add(key);
                 }
-                if(count>1) return false;
             }
         }
-        
         return true;
     }
 }
