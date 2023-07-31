10d6f557ce4c2d487489f2177f03bb5f717cba99
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-07-31 23:29:17 +0530

    Time: 34 ms (42.63%), Space: 49.5 MB (5.38%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -1,50 +1,44 @@
 class Solution {
+    String s1;
+    String s2;
+    int n;
+    int m;
+    Integer[][] dp;
     public int minimumDeleteSum(String s1, String s2) {
-        
-        // Make sure s2 is smaller string
-        if (s1.length() < s2.length()) {
-            return minimumDeleteSum(s2, s1);
+        this.s1=s1;
+        this.s2=s2;
+        this.n=s1.length();
+        this.m=s2.length();
+        dp = new Integer[n+1][m+1];
+        return rec(0,0);
+    }
+    
+    int rec(int i, int j){
+        if(i>=n && j>=m){
+            return 0;
         }
         
-        // Case for empty s1
-        int m = s1.length(), n = s2.length();
-        int[] currRow = new int[n + 1];
-        for (int j = 1; j <= n; j++) {
-            currRow[j] = currRow[j - 1] + s2.charAt(j - 1);
+        if(i>=n){
+            int c = s2.charAt(j);
+            return c + rec(i,j+1);
         }
         
-        // Compute answer row-by-row
-        for (int i = 1; i <= m; i++) {
-            
-            int diag = currRow[0];
-            currRow[0] += s1.charAt(i - 1);
-            
-            for (int j = 1; j <= n; j++) {
-
-                int answer;
-                
-                // If characters are the same, the answer is top-left-diagonal value
-                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    answer = diag;
-                }
-                
-                // Otherwise, the answer is minimum of top and left values with
-                // deleted character's ASCII value
-                else {
-                    answer = Math.min(
-                        s1.charAt(i - 1) + currRow[j],
-                        s2.charAt(j - 1) + currRow[j - 1]
-                    );
-                }
-                
-                // Before overwriting currRow[j] with answer, save it in diag
-                // for the next column
-                diag = currRow[j];
-                currRow[j] = answer;
-            }
+        if(j>=m){
+            int c = s1.charAt(i);
+            return c + rec(i+1,j);
         }
         
-        // Return the answer
-        return currRow[n];
+        if(dp[i][j]!=null) return dp[i][j];
+        
+        if(s1.charAt(i)==s2.charAt(j)){
+            return rec(i+1,j+1);
+        }
+        
+        
+        
+        int del1 = (int)s1.charAt(i) + rec(i+1,j);
+        int del2 = (int)s2.charAt(j) + rec(i,j+1);
+        
+        return dp[i][j]=Math.min(del1,del2);
     }
 }
