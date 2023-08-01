1ae4b7e94d430764d91ecd9b09701fd3979ef6c8
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-08-01 23:14:40 +0530

    Time: 0 ms (100.00%), Space: 44.2 MB (23.45%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,37 +14,20 @@
  * }
  */
 class Solution {
-//     public boolean isValidBST(TreeNode root) {
-//         return val(root,null,null);
-//     }
-    
-//     boolean val(TreeNode root,Integer min, Integer max){
-//         if(root==null){
-//             return true;
-//         }
-        
-//         if((min!=null && root.val<=min) || (max!=null && root.val>=max)){
-//             return false;
-//         }
-        
-//         return val(root.left,min,root.val) || val(root.right,root.val,max);
-//     }
     public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, null, null);
+        return val(root,null,null);
     }
     
-    private boolean isValidBST(TreeNode node, Integer min, Integer max) {
-        if (node == null) {
-            return true; // Empty subtree is a valid BST.
+    boolean val(TreeNode root,Integer min, Integer max){
+        if(root==null){
+            return true;
         }
         
-        // Check if the current node's value is within the specified range.
-        if ((min != null && node.val <= min) || (max != null && node.val >= max)) {
+        if((min!=null && root.val<=min) || (max!=null && root.val>=max)){
             return false;
         }
         
-        // For the left subtree, the maximum value should be the current node's value.
-        // For the right subtree, the minimum value should be the current node's value.
-        return isValidBST(node.left, min, node.val) && isValidBST(node.right, node.val, max);
+        return val(root.left,min,root.val) && val(root.right,root.val,max);
     }
+    
 }
