c3f32885224b9c1868923a697e1be3addd9d7294
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-08-07 09:30:14 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (98.68%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for(int[] arr : matrix){
+            if(bs(arr,target)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    
+     boolean bs(int[] arr, int targ){
+         int l = 0, r = arr.length-1;
+         
+         while(l<r){
+             int mid = l + (r-l)/2;
+             
+             if(arr[mid]==targ){
+                 return true;
+             }
+             else if(arr[mid]<targ){
+                 l = mid + 1;
+             } else {
+                 r = mid;
+             }
+         }
+         if(arr[l]==targ) return true;
+         return false;
+     }
+}
