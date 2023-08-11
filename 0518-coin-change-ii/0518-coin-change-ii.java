fea11cad9417fbf63040dd0a822d6c1e73f69256
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-08-12 00:06:38 +0530

    Time: 16 ms (16.39%), Space: 44.2 MB (64.87%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -2,16 +2,15 @@ class Solution {
     int[] coins;
     Integer[][] dp;
     public int change(int amount, int[] coins) {
-        dp = new Integer[coins.length+1][amount+1];
+        this.dp = new Integer[coins.length+1][amount+1];
         this.coins = coins;
-        return dfs(0,amount);    
+        return rec(0,amount);
     }
     
-    int dfs(int idx, int amount){
+    int rec(int idx, int amount){
         if(amount<0){
             return 0;
         }
-        
         if(idx==coins.length){
             if(amount==0){
                 return 1;
@@ -19,16 +18,12 @@ class Solution {
                 return 0;
             }
         }
-        if(dp[idx][amount]!=null) return dp[idx][amount];
-        
-        if(coins[idx]<=amount){
-        int take = dfs(idx,amount-coins[idx]);
-        int dont = dfs(idx+1,amount);
-        
-        return dp[idx][amount] = take + dont;
-        } else{
-            return dp[idx][amount] = dfs(idx+1,amount);
+        if(dp[idx][amount]!=null){
+            return dp[idx][amount];
         }
-            
+        int take = rec(idx,amount-coins[idx]);
+        int dont = rec(idx+1,amount);
+        
+        return dp[idx][amount] = take+dont;
     }
 }
