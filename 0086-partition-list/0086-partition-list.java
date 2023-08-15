044509e3cd3d20260b4c3e6e0e7cffca48aff656
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-08-15 10:48:17 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (73.24%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -1,35 +1,26 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        ListNode dummy1 = new ListNode(-1);
-        ListNode dummy2 = new ListNode(-1);
-        ListNode prev1 = dummy1;
-        ListNode prev2 = dummy2;
-        
-        while(head!=null){
+        ListNode lessHead = new ListNode(-1);
+        ListNode greaterHead = new ListNode(-1);
+        ListNode lessTail = lessHead;
+        ListNode greaterTail = greaterHead;
+
+        while (head != null) {
             ListNode temp = head;
             head = head.next;
             temp.next = null;
-            if(temp.val<x){
-                prev1.next = temp;
-                prev1 = prev1.next;
-            }else {
-                prev2.next = temp;
-                prev2 = prev2.next;
+
+            if (temp.val < x) {
+                lessTail.next = temp;
+                lessTail = lessTail.next;
+            } else {
+                greaterTail.next = temp;
+                greaterTail = greaterTail.next;
             }
         }
-        
-        prev1.next = dummy2.next;
-        
-        return dummy1.next;
+
+        lessTail.next = greaterHead.next;
+
+        return lessHead.next;
     }
 }
