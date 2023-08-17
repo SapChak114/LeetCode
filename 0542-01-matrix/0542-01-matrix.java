903cfe4f79edb881e373f18202c45c4e4c377018
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-08-17 13:30:26 +0530

    Time: 18 ms (24.95%), Space: 46.7 MB (15.63%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,50 +1,44 @@
-class Pair{
-    int x;
-    int y;
-    int dist;
-    
-    public Pair(int x, int y, int dist){
-        this.x=x;
-        this.y=y;
-        this.dist=dist;
-    }
-}
 class Solution {
     int[] dirX = {0,1,-1,0};
     int[] dirY = {1,0,0,-1};
     public int[][] updateMatrix(int[][] mat) {
-        Queue<Pair> q = new LinkedList<>();
+        Queue<Integer> row = new LinkedList<>();
+        Queue<Integer> col = new LinkedList<>();
+        Queue<Integer> dist = new LinkedList<>();
         int n = mat.length, m = mat[0].length;
-        int[][] dist = new int[n][m];
+        int[][] dis = new int[n][m];
         int[][] vis = new int[n][m];
         
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
                 if(mat[i][j]==0){
                     vis[i][j]=1;
-                    q.add(new Pair(i,j,0));  
+                    row.add(i);
+                    col.add(j);
+                    dist.add(0);
                 }
             }
         }
         
-        while(!q.isEmpty()){
-            Pair p = q.poll();
-            int x = p.x;
-            int y = p.y;
-            int d = p.dist;
-            dist[x][y]=d;
+        while(!row.isEmpty()){
+            int x = row.poll();
+            int y = col.poll();
+            int d = dist.poll();
+            dis[x][y]=d;
             for(int i = 0; i<dirX.length; i++){
-                int xx = x + dirX[i];
-                int yy = y + dirY[i];
+                int r = x + dirX[i];
+                int c = y + dirY[i];
                 
-                if(xx<0 || xx>=n || yy<0 || yy>=m || vis[xx][yy]==1 || mat[xx][yy]==0){
+                if(r<0 || r>=n || c<0 || c>=m || vis[r][c]==1 || mat[r][c]==0){
                     continue;
                 }
-                vis[xx][yy]=1;
-                q.add(new Pair(xx,yy,d+1));
+                vis[r][c]=1;
+                row.add(r);
+                col.add(c);
+                dist.add(d+1);
             }
         }
         
-        return dist;
+        return dis;
     }
 }
