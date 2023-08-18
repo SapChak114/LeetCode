f44e4169f0bc56aed09948eb8badb623bdfb15cf
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-08-18 18:23:36 +0530

    Time: 25 ms (43.89%), Space: 43.7 MB (90.79%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.java
+++ 1615-maximal-network-rank/1615-maximal-network-rank.java
@@ -1,19 +1,23 @@
 class Solution {
     public int maximalNetworkRank(int n, int[][] roads) {
-        int[] link = new int[n];
-        int[][] directLinks = new int[n][n];
+        List<Integer>[] adjList = new ArrayList[n];
         
-        for(int[] road : roads){
-            link[road[0]]++;
-            link[road[1]]++;
-            directLinks[road[0]][road[1]] = 1;
-            directLinks[road[1]][road[0]] = 1;
+        for(int i = 0; i<n; i++) adjList[i] = new ArrayList<>();
+        
+        for(int i = 0; i<roads.length; i++){
+            int u = roads[i][0];
+            int v = roads[i][1];
+            
+            adjList[u].add(v);
+            adjList[v].add(u);
         }
         
         int rank = 0;
+        
         for(int i = 0; i<n; i++){
-            for(int j = i + 1; j<n; j++){
-                rank = Math.max(rank,link[i]+link[j]-directLinks[i][j]);  
+            for(int j = i+1; j<n; j++){
+                int directLink = adjList[i].contains(j)?1:0;
+                rank = Math.max(rank,adjList[i].size()+adjList[j].size()-directLink);
             }
         }
         
