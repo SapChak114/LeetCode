a0996ce48fdfb0f474ca72582a369bc51c722611
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-08-20 14:52:02 +0530

    Time: 364 ms (100.00%), Space: 63.9 MB (100.00%) - LeetHub

--- 2831-find-the-longest-equal-subarray/2831-find-the-longest-equal-subarray.java
+++ 2831-find-the-longest-equal-subarray/2831-find-the-longest-equal-subarray.java
@@ -7,17 +7,21 @@ class Solution {
         
         for(int r = 0; r<n; r++){
             map.put(nums.get(r),map.getOrDefault(nums.get(r),0)+1);
-            freq.put(map.get(nums.get(r)),freq.getOrDefault(map.get(nums.get(r)),0)+1); 
+            freq.put(map.get(nums.get(r)),freq.getOrDefault(map.get(nums.get(r)),0)+1);
+            
+            // if(map.get(nums.get(r))>1){
+            //     freq.put(map.get(nums.get(r))-1,freq.getOrDefault(map.get(nums.get(r))-1,0)-1);
+            // }
             
             int rem = r-l+1 - freq.firstKey();
             
             while(rem>k){
                 map.put(nums.get(l),map.get(nums.get(l))-1);
                 freq.put(map.get(nums.get(l)),freq.getOrDefault(map.get(nums.get(l)),0)+1);
-                freq.put(map.get(nums.get(l))-1,freq.getOrDefault(map.get(nums.get(l))-1,0)-1);
-                if(freq.get(map.get(nums.get(l))-1)==0){
-                    freq.remove(map.get(nums.get(l))-1);
-                }
+                // freq.put(map.get(nums.get(l))-1,freq.getOrDefault(map.get(nums.get(l))-1,0)-1);
+                // if(freq.get(map.get(nums.get(l))-1)==0){
+                //     freq.remove(map.get(nums.get(l))-1);
+                // }
                 
                 l++;
                 
