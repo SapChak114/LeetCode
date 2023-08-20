09f2902ba177b7fef50d44c95fccbc50c4c9f1fa
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-08-20 10:28:58 +0530

    Time: 608 ms (100.00%), Space: 64.2 MB (100.00%) - LeetHub

---
+++ 2831-find-the-longest-equal-subarray/2831-find-the-longest-equal-subarray.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int longestEqualSubarray(List<Integer> nums, int k) {
+        Map<Integer,Integer> map = new HashMap<>();
+        TreeMap<Integer,Integer> freq = new TreeMap<>(Collections.reverseOrder());
+        
+        int n = nums.size(), l = 0, ans = 0;
+        
+        for(int r = 0; r<n; r++){
+            map.put(nums.get(r),map.getOrDefault(nums.get(r),0)+1);
+            freq.put(map.get(nums.get(r)),freq.getOrDefault(map.get(nums.get(r)),0)+1);
+            
+            if(map.get(nums.get(r))>1){
+                freq.put(map.get(nums.get(r))-1,freq.getOrDefault(map.get(nums.get(r))-1,0)-1);
+            }
+            
+            int rem = r-l+1 - freq.firstKey();
+            
+            while(rem>k){
+                map.put(nums.get(l),map.get(nums.get(l))-1);
+                freq.put(map.get(nums.get(l)),freq.getOrDefault(map.get(nums.get(l)),0)+1);
+                freq.put(map.get(nums.get(l))-1,freq.getOrDefault(map.get(nums.get(l))-1,0)-1);
+                if(freq.get(map.get(nums.get(l))-1)==0){
+                    freq.remove(map.get(nums.get(l))-1);
+                }
+                
+                l++;
+                
+                rem = r-l+1 - freq.firstKey();
+            }
+            
+            ans = Math.max(ans,freq.firstKey());
+            
+        }
+        
+        return ans;
+    }
+}
