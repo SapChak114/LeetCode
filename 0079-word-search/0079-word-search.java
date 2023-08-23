9e22a330b9d5cd11775c6f45150b67caaa4c63d7
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-08-23 12:43:07 +0530

    Time: 196 ms (52.51%), Space: 40.5 MB (80.00%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,42 +1,36 @@
 class Solution {
-    int[] dirX = {0, 1, 0, -1};
-    int[] dirY = {1, 0, -1, 0};
-
+    int[] dirX = {0,1,0,-1};
+    int[] dirY = {1,0,-1,0};
     public boolean exist(char[][] board, String word) {
         int n = board.length;
         int m = board[0].length;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (dfs(i, j, 0, board, word)) {
-                    return true;
-                }
+        
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(dfs(i,j,board,word,0)) return true;        
             }
         }
-
+        
         return false;
     }
-
-    boolean dfs(int i, int j, int pos, char[][] board, String word) {
-        if (pos == word.length()) {
+    
+    boolean dfs(int i, int j, char[][] board, String word, int pos){
+        if(pos==word.length()){
             return true;
         }
-        
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(pos)) {
+        if(i>=board.length || i<0 || j>=board[0].length || j<0 || board[i][j]!=word.charAt(pos)){
             return false;
         }
-
-        char original = board[i][j];
-        board[i][j] = '#';  // Mark the cell as visited
+        char orginal = board[i][j];
+        board[i][j] = '#';
         
-        for (int k = 0; k < 4; k++) {
-            if (dfs(i + dirX[k], j + dirY[k], pos + 1, board, word)) {
-                return true;
-            }
-        }
-
-        board[i][j] = original;  // Restore the cell
-
+         for(int k = 0; k<dirX.length; k++){
+             int newi = i+dirX[k];
+             int newj = j+dirY[k];
+             if(dfs(newi,newj,board,word,pos+1)) return true;
+         }
+        board[i][j] = orginal;
+        
         return false;
     }
 }
