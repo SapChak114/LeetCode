08142ec5b23c0a38e24bd0bb256466c3459d20c1
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-08-25 20:53:33 +0530

    Time: 5 ms (48.77%), Space: 63.7 MB (5.10%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -0,0 +1,34 @@
+class Solution {
+    String s1, s2, s3;
+    Boolean[][][] dp;
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if(s1.length()+s2.length()!=s3.length()){
+            return false;
+        }
+        this.s1=s1;
+        this.s2=s2;
+        this.s3=s3;
+        dp = new Boolean[s1.length()+1][s2.length()+1][s3.length()+1];
+        return rec(0,0,0);
+    }
+    
+    boolean rec(int i, int j, int k){
+        if(k==s3.length()){
+            return true;
+        }
+        
+        if(dp[i][j][k]!=null) return dp[i][j][k];
+        
+        if(i<s1.length() && j<s2.length() && s1.charAt(i)==s3.charAt(k) && s2.charAt(j)==s3.charAt(k)){
+            return dp[i][j][k] = rec(i+1,j,k+1) || rec(i,j+1,k+1);
+        }
+        else if(i<s1.length() && s1.charAt(i)==s3.charAt(k)){
+            return dp[i][j][k] = rec(i+1,j,k+1);
+        } 
+        else if(j<s2.length() && s2.charAt(j)==s3.charAt(k)){
+            return dp[i][j][k] = rec(i,j+1,k+1);
+        }
+        
+        return dp[i][j][k] = false;
+    }
+}
