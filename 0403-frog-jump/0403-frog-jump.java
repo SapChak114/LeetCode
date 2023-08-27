433e23c652c99257c47ccb9424fc4bde913489d2
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-08-27 18:44:32 +0530

    Time: 36 ms (71.83%), Space: 47.7 MB (79.13%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.java
+++ 0403-frog-jump/0403-frog-jump.java
@@ -1,41 +1,22 @@
 class Solution {
+    Map<Integer, Integer> stoneIndex;
+    Set<String> cache;
+    int [] stones;
     public boolean canCross(int[] stones) {
-        Map<Integer,Set<Integer>> map = new HashMap<>();
-        Set<Integer> set = new HashSet<>();
-        set.add(1);
-        map.put(stones[0]+1,set);
-        
-        for(int i = 1; i<stones.length; i++){
-            int pos = stones[i];
-            if(map.containsKey(pos)){
-                for(int it : new HashSet<>(map.get(pos))){
-                    Set<Integer> set1 = map.get(pos+it);
-                    if(set1!=null){
-                        set1.add(it);
-                    } else{
-                        set1 = new HashSet<>();
-                        set1.add(it);
-                    }
-                    map.put(pos+it,set1);
-                    Set<Integer> set2 = map.get(pos+it+1);
-                    if(set2!=null){
-                        set2.add(it+1);
-                    } else{
-                        set2 = new HashSet<>();
-                        set2.add(it+1);
-                    }
-                    map.put(pos+it+1,set2);
-                    Set<Integer> set3 = map.get(pos+it-1);
-                    if(set3!=null){
-                        set3.add(it-1);
-                    } else{
-                        set3 = new HashSet<>();
-                        set3.add(it-1);
-                    }
-                    map.put(pos+it-1,set3);
-                }
-            }
-        }
-        return map.containsKey(stones[stones.length-1]);
+        stoneIndex = new HashMap<>();
+        for(int i = 0 ; i < stones.length ; i++ ) stoneIndex.put(stones[i] , i);
+        cache = new HashSet<>();
+        this.stones = stones;
+        return isReach(0, 0);
     }
+    private boolean isReach(int idx, int prevJumpIdx){
+        if(idx == stones.length - 1) return true;
+        if(cache.contains(idx + " " + prevJumpIdx)) return false;
+        for(int i = prevJumpIdx - 1 ; i <= prevJumpIdx + 1 ; i++){
+            if(i > 0 && stoneIndex.containsKey(stones[idx] + i) && isReach(stoneIndex.get(stones[idx] + i), i)) return true;
+        }
+        cache.add(idx +" "+ prevJumpIdx);
+        return false;
+    }
+
 }
