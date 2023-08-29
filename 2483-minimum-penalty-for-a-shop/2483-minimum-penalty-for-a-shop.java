5b918a50b6c8da2ee1c4e388e4de9becc9219f5b
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-08-30 02:06:19 +0530

    Time: 7 ms (99.66%), Space: 43.9 MB (87.84%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
@@ -1,36 +1,20 @@
 class Solution {
     public int bestClosingTime(String cust) {
-        int n = cust.length();
-        int[] suffixY = new int[n];
-        int[] preN = new int[n];
-        
-        if(cust.charAt(n-1)=='Y') suffixY[n-1] = 1;
-        if(cust.charAt(0)=='N') preN[0] = 1;
+        int ans = 0, score = 0, n = cust.length();
         
         for(int i = 0; i<n; i++){
-            if(i==0) continue;
-            int isY = 0;
-            int isN = 0;
-            if(cust.charAt(n-i-1)=='Y') isY = 1;
-            if(cust.charAt(i)=='N') isN = 1;
-            suffixY[n-i-1] = suffixY[n-i] + isY;
-            preN[i] = preN[i-1] + isN;
-        }
-        
-        int minLoss = Integer.MAX_VALUE;
-        int minIndex = n;
-        
-        for(int i = 0; i<=n; i++){
-            int yGonnaMiss = i==n?0:suffixY[i];
-            int nOccuredTillNow = i==0?0:preN[i-1];
-            int currLoss = yGonnaMiss + nOccuredTillNow;
-            if(currLoss<minLoss){
-                minLoss = currLoss;
-                minIndex = i;
+            if(cust.charAt(i)=='Y'){
+                score++;
+                
+                if(score>0){
+                    ans = i+1;
+                    score=0;
+                }
+            } else{
+                score--;
             }
         }
         
-        return minIndex;
-        
+        return ans;
     }
 }
