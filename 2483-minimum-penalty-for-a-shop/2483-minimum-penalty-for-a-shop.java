b3942aab2d21eb855a60db403d455dbc727c81a5
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-08-30 01:47:07 +0530

    Time: 18 ms (22.30%), Space: 44.8 MB (15.88%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
@@ -3,28 +3,19 @@ class Solution {
         int n = cust.length();
         int[] suffixY = new int[n];
         int[] preN = new int[n];
-        int yCount = 0;
-        int nCount = 0;
         
         if(cust.charAt(n-1)=='Y') suffixY[n-1] = 1;
         if(cust.charAt(0)=='N') preN[0] = 1;
         
         for(int i = 0; i<n; i++){
-            if(cust.charAt(i)=='Y') yCount++;
-            if(cust.charAt(i)=='N') nCount++;
-            
-            if(i!=0){
-                int isY = 0;
-                int isN = 0;
-                if(cust.charAt(n-i-1)=='Y') isY = 1;
-                if(cust.charAt(i)=='N') isN = 1;
-                suffixY[n-i-1] = suffixY[n-i] + isY;
-                preN[i] = preN[i-1] + isN;
-            }
+            if(i==0) continue;
+            int isY = 0;
+            int isN = 0;
+            if(cust.charAt(n-i-1)=='Y') isY = 1;
+            if(cust.charAt(i)=='N') isN = 1;
+            suffixY[n-i-1] = suffixY[n-i] + isY;
+            preN[i] = preN[i-1] + isN;
         }
-        
-        // if(yCount==n) return n;
-        // if(nCount==n) return 0;
         
         int minLoss = Integer.MAX_VALUE;
         int minIndex = n;
@@ -32,10 +23,8 @@ class Solution {
         for(int i = 0; i<=n; i++){
             int yGonnaMiss = i==n?0:suffixY[i];
             int nOccuredTillNow = i==0?0:preN[i-1];
-            //System.out.println(" i: "+i+" y: "+yGonnaMiss+" n: "+nOccuredTillNow);
             int currLoss = yGonnaMiss + nOccuredTillNow;
             if(currLoss<minLoss){
-                //System.out.println(" i: "+i+" currLoss: "+currLoss+" minLoss: "+minLoss);
                 minLoss = currLoss;
                 minIndex = i;
             }
