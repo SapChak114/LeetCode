9cf9c22a853719eff3a6ba83dd905e485b9e85f0
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-09-01 00:03:53 +0530

    Time: 5 ms (65.79%), Space: 44.2 MB (49.60%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.java
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.java
@@ -1,45 +1,23 @@
 class Solution {
     public int minTaps(int n, int[] ranges) {
-        // Create an array to track the maximum reach for each position
-        int[] maxReach = new int[n + 1];
-
-        // Calculate the maximum reach for each tap
-        for (int i = 0; i < ranges.length; i++) {
-            // Calculate the leftmost position the tap can reach
-            int start = Math.max(0, i - ranges[i]);
-            // Calculate the rightmost position the tap can reach
-            int end = Math.min(n, i + ranges[i]);
-
-            // Update the maximum reach for the leftmost position
-            maxReach[start] = Math.max(maxReach[start], end);
+        int min = 0;
+        int max = 0;
+        int index = 0;
+        int tap = 0;
+        
+        while(max<n){
+            
+            for(int i = index; i<ranges.length; i++){
+                if(i-ranges[i]<=min && i+ranges[i]>max){
+                    max = i+ranges[i];
+                    index = i;
+                }
+            }
+            if(min==max) return -1;
+            tap++;
+            min=max;
         }
         
-        // Number of taps used
-        int taps = 0;
-        // Current rightmost position reached
-        int currEnd = 0;
-        // Next rightmost position that can be reached
-        int nextEnd = 0;
-
-        // Iterate through the garden
-        for (int i = 0; i <= n; i++) {
-            // Current position cannot be reached
-            if (i > nextEnd) {
-                return -1;
-            }
-
-            // Increment taps when moving to a new tap
-            if (i > currEnd) {
-                taps++;
-                // Move to the rightmost position that can be reached
-                currEnd = nextEnd;
-            }
-
-            // Update the next rightmost position that can be reached
-            nextEnd = Math.max(nextEnd, maxReach[i]);
-        }
-
-        // Return the minimum number of taps used
-        return taps;
+        return tap;
     }
 }
