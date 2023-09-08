22b70329323c305882f559c9991222d114d6c15c
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-09-08 23:58:14 +0530

    Time: 179 ms (47.77%), Space: 40.7 MB (51.10%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -2,35 +2,33 @@ class Solution {
     int[] dirX = {0,1,0,-1};
     int[] dirY = {1,0,-1,0};
     public boolean exist(char[][] board, String word) {
-        int n = board.length;
-        int m = board[0].length;
-        
+        int n = board.length, m = board[0].length;
+        boolean[][] vis = new boolean[n][m];
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
-                if(dfs(i,j,board,word,0)) return true;        
+                 if(dfs(i,j,board,word,0,vis)) return true;
             }
         }
         
         return false;
     }
     
-    boolean dfs(int i, int j, char[][] board, String word, int pos){
+    boolean dfs(int i, int j, char[][] board, String word, int pos, boolean[][] vis){
         if(pos==word.length()){
             return true;
         }
-        if(i>=board.length || i<0 || j>=board[0].length || j<0 || board[i][j]!=word.charAt(pos)){
+        
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length || word.charAt(pos)!=board[i][j] || vis[i][j]){
             return false;
         }
-        char orginal = board[i][j];
-        board[i][j] = '#';
         
-         for(int k = 0; k<dirX.length; k++){
-             int newi = i+dirX[k];
-             int newj = j+dirY[k];
-             if(dfs(newi,newj,board,word,pos+1)) return true;
-         }
-        board[i][j] = orginal;
-        
+        vis[i][j]=true;
+        for(int k = 0; k<dirX.length; k++){
+            int r = i + dirX[k];
+            int c = j + dirY[k];
+            if(dfs(r,c,board,word,pos+1,vis)) return true;
+        }
+        vis[i][j]=false;
         return false;
     }
 }
