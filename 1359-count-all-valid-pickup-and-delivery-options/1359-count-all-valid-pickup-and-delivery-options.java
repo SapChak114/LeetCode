6fbcf46dcc8f06899c2abc6db68d9be08b5880ac
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-09-10 17:37:43 +0530

    Time: 26 ms (6.10%), Space: 47.9 MB (5.49%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -1,21 +1,33 @@
 class Solution {
     int mod = (int)1e9 + 7;
-    public int countOrders(int n) {
-        long ans = 1;
-        for(int i = 1; i<=n*2; i++){
-            ans *= i;
-            if(i%2==0){
-                ans /= 2;
-            }
-            ans %= mod;
-        }
+    Integer[][] dp;
+//     public int countOrders(int n) {
+//         long ans = 1;
+//         for(int i = 1; i<=n*2; i++){
+//             ans *= i;
+//             if(i%2==0) ans /= 2;
+//             ans %= mod;
+//         }
         
-        return (int)ans;
+//         return (int)ans;
+//     }
+    public int countOrders(int n) {
+        dp=new Integer[n+1][n+1];
+        return rec(n,n);
+    }
+    
+    int rec(int pickup, int drop){
+        if(pickup<0 || drop<pickup || drop<0){
+            return 0;
+        }
+        if(pickup==0 && drop==0){
+            return 1;
+        }
+        if(dp[pickup][drop]!=null) return dp[pickup][drop];
+        long temp = (1L*(pickup%mod)*(rec(pickup-1,drop)%mod))%mod;
+        temp += ((1L*(drop-pickup)%mod)*(rec(pickup,drop-1))%mod)%mod;
+        temp %=mod;
+        return dp[pickup][drop] = (int)temp;
+        
     }
 }
-
-/*
-2
-4*3*2*1
- (P1,P2,D1,D2), (P1,P2,D2,D1), (P1,D1,P2,D2), (P2,P1,D1,D2), (P2,P1,D2,D1) and (P2,D2,P1,D1   
- */
