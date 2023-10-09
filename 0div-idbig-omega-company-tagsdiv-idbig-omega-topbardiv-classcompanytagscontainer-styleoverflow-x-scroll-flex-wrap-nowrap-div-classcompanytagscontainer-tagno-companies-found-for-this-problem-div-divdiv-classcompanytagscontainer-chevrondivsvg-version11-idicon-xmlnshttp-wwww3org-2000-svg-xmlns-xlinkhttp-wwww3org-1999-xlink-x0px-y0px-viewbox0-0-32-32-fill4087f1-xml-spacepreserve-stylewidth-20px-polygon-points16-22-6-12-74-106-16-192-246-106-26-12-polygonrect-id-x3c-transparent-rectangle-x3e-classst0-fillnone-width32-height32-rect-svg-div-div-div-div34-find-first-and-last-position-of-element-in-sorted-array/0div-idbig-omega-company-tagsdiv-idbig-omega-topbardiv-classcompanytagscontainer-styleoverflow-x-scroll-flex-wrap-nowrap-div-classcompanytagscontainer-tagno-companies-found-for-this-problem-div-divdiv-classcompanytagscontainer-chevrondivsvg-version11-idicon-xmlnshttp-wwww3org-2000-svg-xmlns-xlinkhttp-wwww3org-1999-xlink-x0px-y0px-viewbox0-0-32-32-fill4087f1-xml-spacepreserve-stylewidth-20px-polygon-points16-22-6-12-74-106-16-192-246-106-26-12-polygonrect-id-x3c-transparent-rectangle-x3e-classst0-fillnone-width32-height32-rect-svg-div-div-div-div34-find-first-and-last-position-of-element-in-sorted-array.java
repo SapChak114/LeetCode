dfd73f59e80136dd0f2c7e6669f84c6d4079c11c
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-10-09 08:13:52 +0530

    Time: 0 ms (100.00%), Space: 44.5 MB (38.52%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        return new int[]{findFirst(nums,target),findLast(nums,target)};
+        return new int[]{find(nums,target,false),find(nums,target,true)};
     }
 
-    int findLast(int[] nums, int target){
+    int find(int[] nums, int target, boolean flag){
         int l = 0, r = nums.length-1, ans = -1;
         
         while(l<=r){
@@ -11,27 +11,8 @@ class Solution {
             
             if(nums[mid]==target){
                 ans = mid;
-                l = mid + 1;
-            }
-            else if(nums[mid]<target){
-                l = mid + 1;
-            } else{
-                r = mid - 1;
-            }
-        }
-        
-        return ans;
-    }
-    
-    int findFirst(int[] nums, int target){
-        int l = 0, r = nums.length-1, ans = -1;
-        
-        while(l<=r){
-            int mid = l + (r-l)/2;
-            
-            if(nums[mid]==target){
-                ans = mid;
-                r = mid - 1;
+                if(flag) l = mid + 1;
+                else r = mid - 1;
             }
             else if(nums[mid]<target){
                 l = mid + 1;
