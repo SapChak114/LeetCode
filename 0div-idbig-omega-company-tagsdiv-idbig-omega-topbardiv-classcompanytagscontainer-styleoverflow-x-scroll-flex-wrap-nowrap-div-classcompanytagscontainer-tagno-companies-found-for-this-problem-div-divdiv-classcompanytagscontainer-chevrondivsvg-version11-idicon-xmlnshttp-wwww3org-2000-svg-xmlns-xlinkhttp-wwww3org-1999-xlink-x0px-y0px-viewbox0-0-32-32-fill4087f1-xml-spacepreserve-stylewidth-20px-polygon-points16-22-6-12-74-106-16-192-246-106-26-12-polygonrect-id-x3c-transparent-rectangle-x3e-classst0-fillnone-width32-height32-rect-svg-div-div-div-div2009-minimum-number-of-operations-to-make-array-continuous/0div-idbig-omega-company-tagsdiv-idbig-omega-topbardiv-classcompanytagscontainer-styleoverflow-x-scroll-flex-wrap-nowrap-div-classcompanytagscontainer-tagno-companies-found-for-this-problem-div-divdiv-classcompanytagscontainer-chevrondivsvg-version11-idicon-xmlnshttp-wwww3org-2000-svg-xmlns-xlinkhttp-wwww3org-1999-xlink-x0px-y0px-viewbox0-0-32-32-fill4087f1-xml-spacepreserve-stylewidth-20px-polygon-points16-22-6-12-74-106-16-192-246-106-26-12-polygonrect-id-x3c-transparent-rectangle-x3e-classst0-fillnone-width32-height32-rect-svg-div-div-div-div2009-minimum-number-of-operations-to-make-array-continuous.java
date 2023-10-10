c971e2787d5055fe50145a0d9c3e06bd9a45fecc
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-10-11 02:04:43 +0530

    Time: 88 ms (26.21%), Space: 58.4 MB (28.16%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2009-minimum-number-of-operations-to-make-array-continuous/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2009-minimum-number-of-operations-to-make-array-continuous.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2009-minimum-number-of-operations-to-make-array-continuous/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2009-minimum-number-of-operations-to-make-array-continuous.java
@@ -3,30 +3,40 @@ class Solution {
         int n = nums.length;
         int ans = n;
         
-        HashSet<Integer> unique = new HashSet<>();
-        for (int num : nums) {
-            unique.add(num);
-        }
+        Set<Integer> set = new HashSet<>();
         
-        int[] newNums = new int[unique.size()];
-        int index = 0;
+        for(int num : nums) set.add(num);
         
-        for (int num : unique) {
-            newNums[index++] = num;
-        }
-            
-        Arrays.sort(newNums);
+        int[] arr = new int[set.size()];
         
-        int j = 0;
-        for (int i = 0; i < newNums.length; i++) {
-            while (j < newNums.length && newNums[j] < newNums[i] + n) {
-                j++;
-            }
-
+        int idx = 0;
+        
+        for(int num : set) arr[idx++] = num;
+        Arrays.sort(arr);
+        
+        for(int i = 0; i<arr.length; i++){
+            int left = arr[i];
+            int right = left + n - 1;
+            int j = bs(arr,right);
             int count = j - i;
-            ans = Math.min(ans, n - count);
+            ans = Math.min(ans,n-count);
         }
         
         return ans;
+    }
+    
+    int bs(int[] arr, int targ){
+        int l = 0, r = arr.length;
+        
+        while(l<r){
+            int mid = l + (r-l)/2;
+            if(targ < arr[mid]){
+                r = mid;
+            } else{
+                l = mid + 1;
+            }
+        }
+        
+        return l;
     }
 }
