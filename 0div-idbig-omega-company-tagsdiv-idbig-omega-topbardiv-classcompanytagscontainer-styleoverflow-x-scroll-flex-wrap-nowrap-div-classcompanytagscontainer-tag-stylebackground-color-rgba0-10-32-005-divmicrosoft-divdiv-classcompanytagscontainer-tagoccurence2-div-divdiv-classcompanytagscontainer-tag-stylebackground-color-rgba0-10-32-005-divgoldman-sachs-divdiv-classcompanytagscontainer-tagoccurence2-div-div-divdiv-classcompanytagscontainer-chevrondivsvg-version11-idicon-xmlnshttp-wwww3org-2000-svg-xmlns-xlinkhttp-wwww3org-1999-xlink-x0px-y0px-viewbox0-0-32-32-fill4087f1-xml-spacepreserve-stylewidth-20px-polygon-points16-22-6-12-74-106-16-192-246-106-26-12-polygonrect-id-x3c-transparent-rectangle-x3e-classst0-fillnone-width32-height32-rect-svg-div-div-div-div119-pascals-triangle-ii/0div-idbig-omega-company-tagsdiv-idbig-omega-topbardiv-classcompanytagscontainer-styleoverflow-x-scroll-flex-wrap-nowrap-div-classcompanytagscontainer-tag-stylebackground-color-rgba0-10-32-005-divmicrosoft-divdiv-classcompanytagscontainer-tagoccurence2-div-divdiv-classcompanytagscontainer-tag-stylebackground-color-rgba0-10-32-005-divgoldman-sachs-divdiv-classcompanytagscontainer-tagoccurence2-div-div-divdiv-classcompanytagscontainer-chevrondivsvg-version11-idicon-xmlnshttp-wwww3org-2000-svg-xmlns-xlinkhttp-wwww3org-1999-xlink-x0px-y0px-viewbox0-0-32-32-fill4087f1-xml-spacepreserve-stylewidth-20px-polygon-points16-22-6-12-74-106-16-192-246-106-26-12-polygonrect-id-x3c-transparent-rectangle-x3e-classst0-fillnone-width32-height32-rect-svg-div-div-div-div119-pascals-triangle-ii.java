608f0b0ea73441883d8939735625e22db63677b9
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-10-16 22:39:53 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (32.70%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoldman-sachs-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div119-pascals-triangle-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoldman-sachs-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div119-pascals-triangle-ii.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoldman-sachs-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div119-pascals-triangle-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoldman-sachs-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div119-pascals-triangle-ii.java
@@ -1,26 +1,35 @@
 class Solution {
-    List<List<Integer>> list;
-    public List<Integer> getRow(int rowIndex) {
-        list = new ArrayList<>();
-        pascal(rowIndex);
-        return list.get(rowIndex);
+   //List<List<Integer>> list;
+    public List<Integer> getRow(int n) {
+         List<Integer> list2 = new ArrayList<>();
+        // pascal(n);
+        // return list.get(n);
+        list2.add(1);
+        long curr = 1;
+        for(int i = 1; i<=n; i++){
+            long val = curr*(n-i+1);
+            val /=i;
+            curr = val;
+            list2.add((int)curr);
+        }
+        
+        return list2;
     }
     
-    void pascal(int n){
-        list.add(Arrays.asList(1));
-        list.add(Arrays.asList(1,1));
+//     void pascal(int n){
+//         list.add(Arrays.asList(1));
         
-        for(int i = 2; i<=n; i++){
-            List<Integer> l = list.get(i-1);
-            List<Integer> l2 = new ArrayList<>();
-            l2.add(1);
-            for(int j = 0; j<l.size()-1; j++){
-                int a = l.get(j);
-                int b = l.get(j+1);
-                l2.add((a+b));
-            }
-            l2.add(1);
-            list.add(l2);
-        }
-    }
+//         for(int i = 1; i<=n; i++){
+//             List<Integer> l = list.get(i-1);
+//             List<Integer> l2 = new ArrayList<>();
+//             l2.add(1);
+//             for(int j = 0; j<l.size()-1; j++){
+//                 int a = l.get(j);
+//                 int b = l.get(j+1);
+//                 l2.add((a+b));
+//             }
+//             l2.add(1);
+//             list.add(l2);
+//         }
+//     }
 }
