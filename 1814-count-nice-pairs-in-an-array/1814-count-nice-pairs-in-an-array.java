1642d8a5e108aca1ab635ea0fba4e2692644c6a5
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-11-22 00:26:21 +0530

    Time: 33 ms (71.04%), Space: 54.5 MB (73.40%) - LeetHub

---
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int countNicePairs(int[] nums) {
+        int n = nums.length;
+        
+        int[] arr = new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = nums[i] - rev(nums[i]);
+        }
+        
+        Map<Integer, Integer> dic = new HashMap();
+        int ans = 0;
+        int MOD = (int) 1e9 + 7;
+        for (int num : arr) {
+            ans = (ans + dic.getOrDefault(num, 0)) % MOD;
+            dic.put(num, dic.getOrDefault(num, 0) + 1);
+        }
+        
+        return ans;
+        
+    }
+    
+    public int rev(int num) {
+        int result = 0;
+        while (num > 0) {
+            result = result * 10 + num % 10;
+            num /= 10;
+        }
+        
+        return result;
+    }
+}
