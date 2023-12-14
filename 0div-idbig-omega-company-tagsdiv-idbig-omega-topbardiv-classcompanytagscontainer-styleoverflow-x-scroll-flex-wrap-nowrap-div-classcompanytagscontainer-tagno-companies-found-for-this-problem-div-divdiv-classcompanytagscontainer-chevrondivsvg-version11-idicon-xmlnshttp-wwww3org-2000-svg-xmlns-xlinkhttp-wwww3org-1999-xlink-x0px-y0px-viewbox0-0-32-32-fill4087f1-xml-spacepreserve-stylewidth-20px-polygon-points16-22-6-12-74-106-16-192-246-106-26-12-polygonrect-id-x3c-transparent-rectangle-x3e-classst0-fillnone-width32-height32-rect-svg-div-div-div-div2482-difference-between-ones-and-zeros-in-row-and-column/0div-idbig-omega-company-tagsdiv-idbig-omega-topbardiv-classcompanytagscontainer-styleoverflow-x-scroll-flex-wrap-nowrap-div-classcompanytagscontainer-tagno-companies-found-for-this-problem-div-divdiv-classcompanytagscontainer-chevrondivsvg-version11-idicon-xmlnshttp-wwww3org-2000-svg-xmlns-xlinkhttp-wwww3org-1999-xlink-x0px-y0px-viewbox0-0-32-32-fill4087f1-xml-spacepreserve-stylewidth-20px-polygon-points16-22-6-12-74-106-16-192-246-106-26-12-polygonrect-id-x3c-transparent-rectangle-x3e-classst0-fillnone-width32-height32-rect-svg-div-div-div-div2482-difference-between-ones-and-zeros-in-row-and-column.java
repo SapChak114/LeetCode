9306b9a7d2c6616d89deacc069f16fe71ad34166
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2023-12-14 23:48:01 +0530

    Time: 12 ms (48.54%), Space: 76.3 MB (28.48%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2482-difference-between-ones-and-zeros-in-row-and-column/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2482-difference-between-ones-and-zeros-in-row-and-column.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[][] onesMinusZeros(int[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        
+        int[][] res = new int[n][m];
+        
+        int[] rowOnes = new int[n];
+        int[] colOnes = new int[m];
+        
+        int[] rowZeros = new int[n];
+        int[] colZeros = new int[m];
+        
+ 
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(grid[i][j]==1) rowOnes[i]++;
+                else rowZeros[i]++;
+            }
+        }
+        
+        for(int j = 0; j<m; j++){
+            for(int i = 0; i<n; i++){
+                if(grid[i][j]==1) colOnes[j]++;
+                else colZeros[j]++;
+            }
+        }
+        
+        for(int row = 0; row<n; row++){
+            for(int col = 0; col<m; col++){
+                res[row][col] = rowOnes[row] + colOnes[col] - rowZeros[row] - colZeros[col];
+            }
+        }
+        
+        return res;
+    }
+}
