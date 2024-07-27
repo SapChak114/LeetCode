d0494d42234cf58278ec4ea64ee12153815893d0
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-07-27 18:26:03 +0530

    Time: 4 ms (48.52%), Space: 49.2 MB (75.95%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -4,31 +4,34 @@ class Solution {
     public int numIslands(char[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        
+
         boolean[][] vis = new boolean[n][m];
-        
         int count = 0;
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(!vis[i][j] && grid[i][j]=='1'){
+        for (int i = 0; i<n; i++) {
+            for (int j = 0; j<m; j++) {
+                if (!vis[i][j] && grid[i][j] == '1') {
                     count++;
-                    dfs(vis,grid,i,j);
+                    dfs(vis, grid, i, j);
                 }
             }
         }
-        
+
         return count;
     }
-    
-    void dfs(boolean[][] vis, char[][] grid, int i, int j){
-        vis[i][j]=true;
-        int n = grid.length, m = grid[0].length;
-        for(int x = 0; x<dirX.length; x++){
-                int newX = dirX[x] + i;
-                int newY = dirY[x] + j;
-                if(newX>=0 && newX<n && newY>=0 && newY<m && grid[newX][newY]=='1' && !vis[newX][newY]){
-                    dfs(vis,grid,newX,newY);
-                }
+
+    void dfs(boolean[][] vis, char[][] grid, int i, int j) {
+        vis[i][j] = true;
+
+        for (int a = 0; a<dirX.length; a++) {
+            int newX = i + dirX[a];
+            int newY = j + dirY[a];
+            if (check(grid, vis, newX, newY)) {
+                dfs(vis, grid, newX, newY);
+            }
         }
+    }
+
+    boolean check(char[][] grid, boolean[][] vis, int a, int b) {
+        return a>=0 && a<grid.length && b>=0 && b<grid[0].length && !vis[a][b] && grid[a][b] == '1';
     }
 }
