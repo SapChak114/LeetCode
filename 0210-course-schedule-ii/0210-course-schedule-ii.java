3b7e50130b411b858857ebcc522492b95749ed07
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-07-27 23:50:45 +0530

    Time: 5 ms (77.11%), Space: 45.6 MB (65.62%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,77 +1,68 @@
 class Solution {
-    public int[] findOrder(int numCourses, int[][] pre) {
-        
-        List<Integer>[] adjList = new ArrayList[numCourses];
-        
-        for(int i = 0; i<numCourses; i++) adjList[i] = new ArrayList<>();
-        
-        for(int i = 0; i<pre.length; i++){
-            int u = pre[i][0];
-            int v = pre[i][1];
-            
-            adjList[u].add(v);
+    public int[] findOrder(int num, int[][] pre) {
+        List<Integer>[] adjList = new ArrayList[num];
+
+        for (int i = 0; i<num; i++) {
+            adjList[i] = new ArrayList<>();
         }
-        
-        if(checkCycle(adjList)){
-            return new int[]{};
-        }
-        
-        boolean[] vis = new boolean[numCourses];
-        List<Integer> ans = new ArrayList<>();
-        for(int i = 0; i<numCourses; i++){
-            if(!vis[i]){
-                dfs(i,vis,adjList,ans);
+
+        for (int i = 0; i<pre.length; i++) {
+            for (int j = 0; j<pre[i].length; j++) {
+                int u = pre[i][0];
+                int v = pre[i][1];
+                adjList[u].add(v);
             }
         }
-        
+
+        int[] vis = new int[num];
+        for (int i = 0; i<num; i++) {
+            if (dfsCy(adjList, vis, i)) {
+                return new int[]{};
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        Set<Integer> seen = new HashSet<>();
+        for (int i = 0; i<num; i++) {
+            if (!seen.contains(i)) {
+                dfs(adjList, seen, i, ans);
+            }
+        }
+
         int[] res = new int[ans.size()];
-        
-        for(int i = 0; i<res.length; i++){
+        for (int i = 0; i<res.length; i++) {
             res[i] = ans.get(i);
         }
-        
+
         return res;
     }
-    
-    void dfs(int node, boolean[] vis, List<Integer>[] adjList, List<Integer> ans){
-        
-        vis[node] = true;
-        
-        for(int n : adjList[node]){
-            if(!vis[n]){
-                dfs(n,vis,adjList,ans);
-            }
-        }
-        
-        ans.add(node);
-    }
-    
-    boolean checkCycle(List<Integer>[] adjList){
-        int[] vis = new int[adjList.length];
-        
-        for(int i = 0; i<adjList.length; i++){
-            if(vis[i]==0){
-                if(dfsc(i,vis,adjList)) return true;
-            }
-        }
-        
-        return false;
-    }
-    
-    boolean dfsc(int node, int[] vis, List<Integer>[] adjList){
+
+    boolean dfsCy(List<Integer>[] adjList, int[] vis, int node) {
         vis[node] = 1;
-        
-        for(int n : adjList[node]){
-            if(vis[n]==0){
-                if(dfsc(n,vis,adjList)) return true;
-            }
-            else if(vis[n]==1){
+
+        for (int nod : adjList[node]) {
+            if (vis[nod] == 0) {
+                if (dfsCy(adjList, vis, nod)) {
+                    return true;
+                }
+            } else if (vis[nod] == 1) {
                 return true;
             }
         }
-        
+
         vis[node] = 2;
-        
         return false;
+    }
+
+    void dfs(List<Integer>[] adjList, Set<Integer> seen, int node, List<Integer> ans) {
+        seen.add(node);
+
+        for (int nod : adjList[node]) {
+            if (!seen.contains(nod)) {
+                dfs(adjList, seen, nod, ans);
+            }
+        }
+
+        ans.add(node);
     }
 }
