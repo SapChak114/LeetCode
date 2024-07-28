6f9db4598ffa697ba2618317cd681cfda2777012
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-07-28 10:38:13 +0530

    Time: 4 ms (52.32%), Space: 42 MB (19.3%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.java
+++ 0778-reorganize-string/0778-reorganize-string.java
@@ -18,42 +18,24 @@ class Solution {
         PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> b.freq - a.freq);
 
         for (Map.Entry<Character, Integer> e : freq.entrySet()) {
+            if (e.getValue() > (s.length()+1)/2) {
+                return "";
+            }
             pq.add(new Pair(e.getKey(), e.getValue()));
         }
 
         StringBuilder sb = new StringBuilder();
-        Map<Character, Integer> freq2 = new HashMap<>();
+        Pair prev = new Pair('#', -1);
         while (!pq.isEmpty()) {
-            Pair p1 = pq.poll();
-            if (!pq.isEmpty()) {
-                sb.append(p1.c);
-                freq2.put(p1.c, freq2.getOrDefault(p1.c, 0)+1);
-                Pair p2 = pq.poll();
-                sb.append(p2.c);
-                freq2.put(p2.c, freq2.getOrDefault(p2.c, 0)+1);
-                
-                int newFreq1 = p1.freq - 1;
-                if (newFreq1 > 0) {
-                    pq.add(new Pair(p1.c, newFreq1));
-                }
-                int newFreq2 = p2.freq - 1;
-                if (newFreq2 > 0) {
-                    pq.add(new Pair(p2.c, newFreq2));
-                }
-            } else {
-                int len = freq2.getOrDefault(p1.c, 0);
-                System.out.println(len+" "+sb.length());
-                if (len > sb.length()/2) {
-                    return "";
-                }
-                sb.append(p1.c);
-                freq2.put(p1.c, freq2.getOrDefault(p1.c, 0)+1);
-                int newFreq1 = p1.freq - 1;
-                if (newFreq1 > 0) {
-                    pq.add(new Pair(p1.c, newFreq1));
-                }
+            Pair curr = pq.poll();
+            sb.append(curr.c);
+
+            if (prev.freq > 0) {
+                pq.add(prev);
             }
-            
+
+            curr.freq--;
+            prev = curr;
         }
 
         return sb.toString();
