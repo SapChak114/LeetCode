1037325a48535dc6022eb667833596d25dcaf593
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-07-28 13:55:14 +0530

    Time: 601 ms (8.76%), Space: 48.4 MB (5.07%) - LeetHub

--- 3235-minimum-cost-to-convert-string-i/3235-minimum-cost-to-convert-string-i.java
+++ 3235-minimum-cost-to-convert-string-i/3235-minimum-cost-to-convert-string-i.java
@@ -1,47 +1,82 @@
+class Pair{
+    char b;
+    int wt;
+
+    public Pair(char b, int wt) {
+        this.b = b;
+        this.wt = wt;
+    }
+}
 class Solution {
-    final long INF = Long.MAX_VALUE/2;
     public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
-        long[][] dist = new long[26][26];
+        Map<Character, List<Pair>> adjMap = new HashMap<>();
 
-        for (int i = 0; i<26; i++) {
-            for (int j = 0; j<26; j++) {
-                dist[i][j] = i == j ? 0 : INF;
-            }
-        }
-
-        for (int i = 0; i<original.length; i++) {
-            int a = original[i]-'a';
-            int b = changed[i]-'a';
-            dist[a][b] = Math.min(dist[a][b], cost[i]);
-        }
-
-        flyodWarshal(dist);
-
-        long totalDist = 0;
-        int n = source.length();
+        int n = original.length;
         for (int i = 0; i<n; i++) {
-            int a = source.charAt(i)-'a';
-            int b = target.charAt(i)-'a';
-            if (dist[a][b] == INF) {
-                return -1;
-            }
-            totalDist += dist[a][b];
+            char a = original[i];
+            char b = changed[i];
+            int wt = cost[i];
+
+            adjMap.computeIfAbsent(a, k -> new ArrayList<>()).add(new Pair(b, wt));
         }
 
-        return totalDist;
+        int m = source.length(), val = 0;
+        long sum = 0;
+        Map<String, Integer> memo = new HashMap<>();
+        for(int i = 0; i<m; i++) {
+            char src = source.charAt(i);
+            char dest = target.charAt(i);
+            String key = src + "-" + dest;
+            if (memo.containsKey(key)) {
+                val = memo.get(key);
+            } else {
+                val = dijkstra(src, dest, adjMap);
+                if (val == Integer.MAX_VALUE) {
+                    return -1;
+                }
+                memo.put(key, val);
+            }
+            sum += val;
+        }
+
+        return sum;
     }
 
-    void flyodWarshal(long[][] dist) {
-        
-        for (int k = 0; k<26; k++) {
-            for (int i = 0; i<26; i++) {
-                for (int j = 0; j<26; j++) {
-                    if (dist[i][k] == INF || dist[k][j] == INF){
-                        continue;
-                    } 
-                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+    int dijkstra(char src, char dest, Map<Character, List<Pair>> adjMap) {
+        Map<Character, Integer> dist = new HashMap<>();
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.wt - b.wt);
+        pq.add(new Pair(src, 0));
+        dist.put(src, 0);
+
+        while (!pq.isEmpty()) {
+            Pair p = pq.poll();
+            char currChar = p.b;
+            int currDist = p.wt;
+
+            if (currChar == dest) {
+                return currDist;
+            }
+
+            if (currDist > dist.getOrDefault(currChar, Integer.MAX_VALUE)) {
+                continue;
+            }
+
+            if (!adjMap.containsKey(currChar)) {
+                continue;
+            }
+
+            for (Pair nei : adjMap.get(currChar)) {
+                int newDist = currDist + nei.wt;
+                if (newDist < dist.getOrDefault(nei.b, Integer.MAX_VALUE)) {
+                    pq.add(new Pair(nei.b, newDist));
+                    dist.put(nei.b, newDist);
                 }
             }
         }
+
+        return Integer.MAX_VALUE;
     }
 }
+/**
+Input: source = "abcd", target = "acbe", original = ["a", "b", "c", "c", "e", "d"], changed = ["b", "c", "b", "e", "b", "e"], cost = [2, 5, 5, 1, 2, 20]
+ */
