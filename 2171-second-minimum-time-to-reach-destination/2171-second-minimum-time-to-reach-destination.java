6ee312fb554291ad509ee6cff57f16ddd956c7d6
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-07-30 02:26:12 +0530

    Time: 85 ms (46.91%), Space: 58.2 MB (6.27%) - LeetHub

--- 2171-second-minimum-time-to-reach-destination/2171-second-minimum-time-to-reach-destination.java
+++ 2171-second-minimum-time-to-reach-destination/2171-second-minimum-time-to-reach-destination.java
@@ -1,78 +1,55 @@
-// class Solution {
-//     public int secondMinimum(int n, int[][] edges, int time, int change) {
-//         List<Integer>[] adjList = new ArrayList[n];
-
-//         for (int[] edge : edges) {
-//             int u = edge[0];
-//             int v = edge[1];
-
-//             adjList[u].add(v);
-//             adjList[v].add(u);
-//         }
-
-        
-//     }
-// }
-
 class Solution {
     public int secondMinimum(int n, int[][] edges, int time, int change) {
-        Map<Integer, List<Integer>> adj = new HashMap<>();
-        for (int[] edge : edges) {
-            int a = edge[0], b = edge[1];
-            adj.computeIfAbsent(a, value -> new ArrayList<Integer>()).add(b);
-            adj.computeIfAbsent(b, value -> new ArrayList<Integer>()).add(a);
-        }
-        int[] dist1 = new int[n + 1], dist2 = new int[n + 1], freq = new int[n + 1];
-        // dist1[i] stores the minimum time taken to reach node i from node 1. dist2[i]
-        // stores the second minimum time taken to reach node from node 1. freq[i] stores
-        // the number of times a node is popped out of the heap.
-        for (int i = 1; i <= n; i++) {
-            dist1[i] = dist2[i] = Integer.MAX_VALUE;
-            freq[i] = 0;
+        List<Integer>[] adjList = new ArrayList[n+1];
+
+        for (int i = 0; i<=n; i++) {
+            adjList[i] = new ArrayList<>();
         }
 
-        PriorityQueue<int []> pq = new PriorityQueue<>((a, b) -> (a[1] - b[1]));
-        pq.offer(new int[] {1, 0});
-        dist1[1] = 0;
+        for (int i = 0; i<edges.length; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
 
-        while (!pq.isEmpty()) {
-            int [] temp = pq.poll();
-            int node = temp[0];
-            int time_taken = temp[1];
+            adjList[u].add(v);
+            adjList[v].add(u);
+        }
 
-            freq[node]++;
+        List<Set<Integer>> dist = new ArrayList<>();
+        for (int i = 0; i<=n; i++) {
+            dist.add(new HashSet<>());
+        }
+        
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{1,0});
+        
+        dist.get(1).add(0);
 
-            // If the node is being visited for the second time and is 'n', return the
-            // answer.
-            if (freq[node] == 2 && node == n)
-                return time_taken;
-            // If the current light is red, wait till the path turns green.
-            if ((time_taken / change) % 2 == 1)
-                time_taken = change * (time_taken / change + 1) + time;
-            else
-                time_taken = time_taken + time;
+        while (!q.isEmpty()) {
+            int[] val = q.poll();
+            int node = val[0];
+            int t = val[1];
+            int qt = t/change;
 
-            if (!adj.containsKey(node))
-                continue;
-            for (int neighbor : adj.get(node)) {
-                // Ignore nodes that have already popped out twice, we are not interested in
-                // visiting them again.
-                if (freq[neighbor] == 2)
-                    continue;
-
-                // Update dist1 if it's more than the current time_taken and store its value in
-                // dist2 since that becomes the second minimum value now.
-                if (dist1[neighbor] > time_taken) {
-                    dist2[neighbor] = dist1[neighbor];
-                    dist1[neighbor] = time_taken;
-                    pq.offer(new int [] {neighbor, time_taken});
-                } else if (dist2[neighbor] > time_taken && dist1[neighbor] != time_taken) {
-                    dist2[neighbor] = time_taken;
-                    pq.offer(new int[] {neighbor, time_taken});
-                }
+            if (node == n && dist.get(node).size() == 2) {
+                return Collections.max(dist.get(node));
             }
 
+            if ((qt&1) == 1) {
+                t = (qt+1)*change;
+            }
+
+            for (int nod : adjList[node]) {
+
+                if (dist.get(nod).size() > 1) {
+                    continue;
+                }
+
+                dist.get(nod).add(t+time);
+                q.add(new int[]{nod, t+time});
+            }
         }
-        return 0;
+
+        return -1;
+
     }
 }
