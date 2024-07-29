a7cca0d6de78fc788396a3f094ea1075d6ddc481
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-07-29 22:12:44 +0530

    Time: 8 ms (55.19%), Space: 40.6 MB (94.63%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,54 +1,61 @@
 class Solution {
-    char[][] board;
     public void solveSudoku(char[][] board) {
-        this.board = board;
-        solve(0);
+        sudoku(board, 0);
     }
-    
-    boolean solve(int pos){
-        if(pos==81){
+
+    boolean sudoku(char[][] board, int pos) {
+        if (pos == 81) {
             return true;
         }
-        
+
         int row = pos/9;
         int col = pos%9;
-        
-        if(board[row][col]!='.'){
-            return solve(pos+1);
-        } else{
-            for(char c = '1'; c<='9'; c++){
-                if(check(c,pos)){
-                    board[row][col]=c;
-                    if(solve(pos+1)) return true;
-                    board[row][col]='.';
+
+        if (board[row][col] != '.') {
+            return sudoku(board, pos+1);
+        } else {
+
+            for (char c = '1'; c<='9'; c++) {
+                if (valid(board, c, pos)) {
+                    board[row][col] = c;
+                    if (sudoku(board, pos+1)) {
+                        return true;
+                    }
+                    board[row][col] = '.';
                 }
             }
         }
-        
+
         return false;
     }
-    
-    boolean check(char c, int pos){
+
+    boolean valid(char[][] board, char c, int pos) {
         int row = pos/9;
         int col = pos%9;
-        
-        int rowStart = (row/3)*3;
-        int colStart = (col/3)*3;
-        
-        for(int i = rowStart; i<rowStart+3; i++){
-            for(int j = colStart; j<colStart+3; j++){
-                if(board[i][j]==c) return false;
+
+        int rowCell = (row/3)*3;
+        int colCell = (col/3)*3;
+
+        for (int i = rowCell; i<rowCell+3; i++) {
+            for (int j = colCell; j<colCell+3; j++) {
+                if (board[i][j] == c) {
+                    return false;
+                }
             }
         }
-        
-        for(int i = 0; i<9; i++){
-            if(board[i][col]==c) return false;
+
+        for (int i = 0; i<9; i++) {
+            if (board[i][col] == c) {
+                return false;
+            }
         }
-        
-        for(int j = 0; j<9; j++){
-            if(board[row][j]==c) return false;
+
+        for (int j = 0; j<9; j++) {
+            if (board[row][j] == c) {
+                return false;
+            }
         }
-        
+
         return true;
     }
 }
