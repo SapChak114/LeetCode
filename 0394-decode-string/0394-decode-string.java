297a99e31a7c9771c7f44dc5716d90215733caab
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-07-29 11:08:41 +0530

    Time: 1 ms (77.99%), Space: 41.3 MB (67.01%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,32 +1,32 @@
 class Solution {
     public String decodeString(String s) {
         Stack<String> st = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) != ']') {
-                st.push(s.charAt(i) + "");
+
+        for (char c : s.toCharArray()) {
+            if (c != ']') {
+                st.push(""+c);
             } else {
-                // When ']' is encountered, process the substring
                 StringBuilder sb = new StringBuilder();
                 while (!st.empty() && !st.peek().equals("[")) {
-                    sb.insert(0, st.pop());
+                    sb.insert(0,st.pop());
                 }
-                st.pop(); // Pop '['
+                st.pop();
 
-                // Get the number
                 StringBuilder numStr = new StringBuilder();
                 while (!st.empty() && Character.isDigit(st.peek().charAt(0))) {
                     numStr.insert(0, st.pop());
                 }
+
                 int num = Integer.parseInt(numStr.toString());
                 st.push(sb.toString().repeat(num));
             }
         }
 
-        // Construct the final result from the stack
-        StringBuilder result = new StringBuilder();
+        StringBuilder ans = new StringBuilder();
         while (!st.empty()) {
-            result.insert(0, st.pop());
+            ans.insert(0, st.pop());
         }
-        return result.toString();
+
+        return ans.toString();
     }
 }
