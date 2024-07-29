801377a3a63efdd6eacf0a24e4646d8b2d9e9091
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-07-30 00:49:57 +0530

    Time: 1997 ms (13.51%), Space: 41.9 MB (97.61%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.java
+++ 1511-count-number-of-teams/1511-count-number-of-teams.java
@@ -1,29 +1,19 @@
 class Solution {
     public int numTeams(int[] rating) {
-        int res = 0;
         int n = rating.length;
 
-        for (int m = 0; m<n; m++) {
-            int leftSmaller = 0, rightLarger = 0;
-
-            for (int i = 0; i<m; i++) {
-                if (rating[i] < rating[m]) {
-                    leftSmaller++;
+        int ans = 0;
+        for (int i = 0; i<n; i++) {
+            for (int j = i+1; j<n; j++) {
+                for (int k = j+1; k<n; k++) {
+                    if ((rating[i] < rating[j] && rating[j] < rating[k]) ||
+                    (rating[i] > rating[j] && rating[j] > rating[k])) {
+                        ans++;
+                    }
                 }
             }
-
-            for (int i = m+1; i<n; i++) {
-                if (rating[i] > rating[m]) {
-                    rightLarger++;
-                }
-            }
-            res += leftSmaller * rightLarger;
-
-            int leftLarger = m - leftSmaller;
-            int rightSmaller = n - m - 1 - rightLarger;
-            res += leftLarger * rightSmaller;
         }
 
-        return res;
+        return ans;
     }
 }
