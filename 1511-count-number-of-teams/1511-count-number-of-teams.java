bb9991bda171c8f118843a3babbaffca33e43a37
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-07-30 00:50:24 +0530

    Time: 1992 ms (14.49%), Space: 41.4 MB (100%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.java
+++ 1511-count-number-of-teams/1511-count-number-of-teams.java
@@ -1,8 +1,7 @@
 class Solution {
     public int numTeams(int[] rating) {
-        int n = rating.length;
+        int n = rating.length, ans = 0;
 
-        int ans = 0;
         for (int i = 0; i<n; i++) {
             for (int j = i+1; j<n; j++) {
                 for (int k = j+1; k<n; k++) {
@@ -13,7 +12,7 @@ class Solution {
                 }
             }
         }
-
+        
         return ans;
     }
 }
