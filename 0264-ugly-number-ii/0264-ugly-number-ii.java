a1f477e6ddbb326aee2c8cb6a366d6379830201c
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-08-18 17:31:16 +0530

    Time: 60 ms (9.28%), Space: 44.9 MB (5.92%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -1,30 +1,19 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        int two = 2, three = 3, five = 5;
-        int i2 = 0, i3 = 0, i5 = 0;
-
-        int[] res = new int[n];
-        res[0] = 1;
-        for (int i = 1; i<n; i++) {
-            int min = Math.min(two, Math.min(three, five));
-            res[i] = min;
-
-            if (min == two) {
-                i2++;
-                two = res[i2] * 2;
-            }
-
-            if (min == three) {
-                i3++;
-                three = res[i3] * 3;
-            }
-
-            if (min == five) {
-                i5++;
-                five = res[i5] * 5;
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        pq.add(1l);
+        long num = 1;
+        Set<Long> set = new HashSet<>();
+        for (int i = 0; i<n; i++) {
+            num = pq.poll();
+            for (long factor : Arrays.asList(2, 3, 5)) {
+                if (!set.contains(num * factor)) {
+                    set.add((num * factor));
+                    pq.add((num * factor));
+                }
             }
         }
 
-        return res[n-1];
+        return (int) num;
     }
 }
