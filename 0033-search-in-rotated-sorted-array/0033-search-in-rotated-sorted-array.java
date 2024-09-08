bdd5a8f774459759ebacbfc60e2d077d34d61988
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-09-08 22:19:03 +0530

    Time: 0 ms (100%), Space: 41.7 MB (84.82%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,41 +1,31 @@
 class Solution {
     public int search(int[] nums, int target) {
         int n = nums.length;
-        int left = 0, right = n - 1;
-        
-        // Find the index of the pivot element (the smallest element)
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] > nums[n - 1]) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-    
-        // Binary search over elements on the pivot element's left
-        int answer = binarySearch(nums, 0, left - 1, target);
-        if (answer != -1) {
-            return answer;
-        }
-        
-        // Binary search over elements on the pivot element's right
-        return binarySearch(nums, left, n - 1, target);
-    }
-    
-    // Binary search over an inclusive range [left_boundary ~ right_boundary]
-    private int binarySearch(int[] nums, int leftBoundary, int rightBoundary, int target) {
-        int left = leftBoundary, right = rightBoundary;
-        while (left <= right) {
-            int mid = (left + right) / 2;
+        int l = 0, r = n-1;
+
+        while (l <= r) {
+            int mid = l + (r-l)/2;
+
             if (nums[mid] == target) {
                 return mid;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
+            }
+
+            if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
             } else {
-                left = mid + 1;
+                if (target <= nums[r] && nums[mid] < target) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
             }
         }
+
         return -1;
+        
     }
 }
