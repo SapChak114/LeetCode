1dbb1586b21c161f98dd5980f815967553d688da
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-09-14 15:00:29 +0530

    Time: 42 ms (60.67%), Space: 44.9 MB (84.52%) - LeetHub

--- 0691-stickers-to-spell-word/0691-stickers-to-spell-word.java
+++ 0691-stickers-to-spell-word/0691-stickers-to-spell-word.java
@@ -1,54 +1,53 @@
 class Solution {
-    List<Map<Character, Integer>> stickerCount;
+    List<Map<Character, Integer>> stickerList;
     Map<String, Integer> dp;
-
     public int minStickers(String[] stickers, String target) {
-        stickerCount = new ArrayList<>();
-        dp = new HashMap<>();
+        this.stickerList = new ArrayList<>();
+        this.dp = new HashMap<>();
 
         for (String st : stickers) {
             Map<Character, Integer> map = new HashMap<>();
             for (char c : st.toCharArray()) {
                 map.put(c, map.getOrDefault(c, 0) + 1);
             }
-            stickerCount.add(map);
+            this.stickerList.add(map);
         }
-
         dp.put("", 0);
-        int res = dfs(target);
 
+        int res = dfs(target);
         return res != (int)1e9 + 7 ? res : -1;
     }
 
-    int dfs(String t) {
+    int dfs (String t) {
         if (dp.containsKey(t)) {
             return dp.get(t);
         }
 
         int res = (int)1e9 + 7;
-        for (Map<Character, Integer> st : stickerCount) {
+
+        for (Map<Character, Integer> st : stickerList) {
             if (!st.containsKey(t.charAt(0))) {
                 continue;
             }
 
-            StringBuilder rem = new StringBuilder();
             Map<Character, Integer> cpSt = new HashMap<>(st);
-
-            for (char c : t.toCharArray()) {
-                if (cpSt.getOrDefault(c, 0) > 0) {
-                    cpSt.put(c, cpSt.get(c) - 1);
+            StringBuilder rem = new StringBuilder();
+            
+            for (int i = 0; i<t.length(); i++) {
+                if (cpSt.getOrDefault(t.charAt(i), 0) > 0) {
+                    cpSt.put(t.charAt(i), cpSt.get(t.charAt(i)) - 1);
                 } else {
-                    rem.append(c);
+                    rem.append(t.charAt(i));
                 }
             }
 
-            if (rem.length() != 0) {
+            if (rem.length() > 0) {
                 res = Math.min(res, 1 + dfs(rem.toString()));
             } else {
                 res = Math.min(res, 1);
             }
         }
-
+        
         dp.put(t, res);
         return res;
     }
