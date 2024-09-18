1b22243d1ea5bd1d173356d0adf71501adf0d4f6
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-09-19 02:29:48 +0530

    Time: 4 ms (100%), Space: 57.1 MB (27.49%) - LeetHub

--- 1659-get-the-maximum-score/1659-get-the-maximum-score.java
+++ 1659-get-the-maximum-score/1659-get-the-maximum-score.java
@@ -1,14 +1,16 @@
 class Solution {
     private static final int MOD = 1000000007;
-
     public int maxSum(int[] nums1, int[] nums2) {
-        int n = nums1.length, m = nums2.length, i = 0, j = 0;
+        int n = nums1.length;
+        int m = nums2.length;
+
+        int i = 0, j = 0;
         long s1 = 0, s2 = 0, res = 0;
 
         while (i < n && j < m) {
             if (nums1[i] < nums2[j]) {
                 s1 += nums1[i++];
-            } else if (nums2[j] < nums1[i]) {
+            } else if (nums1[i] > nums2[j]) {
                 s2 += nums2[j++];
             } else {
                 res += Math.max(s1, s2) + nums1[i];
@@ -26,6 +28,10 @@ class Solution {
 
         while (j < m) {
             s2 += nums2[j++];
+        }
+
+        while (i < n) {
+            s1 += nums1[i++];
         }
 
         res += Math.max(s1, s2);
