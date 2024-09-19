a893ace318307b7e96e9f0a6c3cb2e86a04124af
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-09-19 09:40:23 +0530

    Time: 1 ms (99.49%), Space: 45.4 MB (38.98%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
@@ -1,18 +1,18 @@
 class Solution {
     Integer[][] dp;
-    int[][] matrix;
     int n, m;
-    int inf = (int)1e9 + 7;
+    int[][] matrix;
     public int minFallingPathSum(int[][] matrix) {
         this.n = matrix.length;
         this.m = matrix[0].length;
 
-        this.dp = new Integer[n+1][m+1];
         this.matrix = matrix;
 
-        int ans = inf;
-        for (int c = 0; c<m; c++) {
-            ans = Math.min(ans, dfs(0, c));
+        this.dp = new Integer[n+1][m+1];
+
+        int ans = (int)1e9 + 7;
+        for (int i = 0; i<m; i++) {
+            ans = Math.min(ans, dfs(0, i));
         }
 
         return ans;
@@ -22,13 +22,15 @@ class Solution {
         if (r == n) {
             return 0;
         }
+
         if (c < 0 || c == m) {
-            return inf;
+            return (int)1e9 + 7;
         }
+
         if (dp[r][c] != null) {
             return dp[r][c];
         }
 
-        return dp[r][c] = matrix[r][c] + Math.min(dfs(r + 1, c - 1), Math.min(dfs(r + 1, c), dfs(r + 1, c + 1)));
+        return dp[r][c] = matrix[r][c] + Math.min(dfs(r+1, c - 1), Math.min(dfs(r+1,c), dfs(r+1, c+1)));
     }
 }
