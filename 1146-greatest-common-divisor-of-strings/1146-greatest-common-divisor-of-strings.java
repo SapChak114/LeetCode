84b60e23a78db5861118c43c315f0320f40ddd40
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-09-19 12:12:40 +0530

    Time: 0 ms (100%), Space: 41.9 MB (89.47%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
@@ -1,11 +1,10 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        //System.out.println(str1+" "+str2);
         if (str2.length() > str1.length()) {
             return gcdOfStrings(str2, str1);
         }
 
-        if (str2.equals(str1)) {
+        if (str1.equals(str2)) {
             return str1;
         }
 
