081ef7b3d17b7d4570c30ef34f035b0b895a1a2f
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-09-26 16:41:41 +0530

    Time: 329 ms (8.55%), Space: 45.7 MB (43.74%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,13 +1,25 @@
 class Solution {
+    int[] dp;
     public boolean canJump(int[] nums) {
-        int goal = nums.length-1;
-        
-        for(int i = nums.length-1; i>=0; i--){
-            if(nums[i]+i>=goal){
-                goal = i;
+        this.dp = new int[nums.length];
+        return recc(0, nums) == 1 ? true : false;
+    }
+
+public int recc(int idx, int[] nums){
+        if(idx==nums.length-1){
+            return 1;
+        }
+        if(dp[idx]!=0){
+            return dp[idx];
+        }
+        int steps = nums[idx];
+        for (int i = idx + 1; i <= idx + steps && i < nums.length; i++) {
+            if (recc(i, nums) == 1) {
+                dp[idx] = 1;
+                return 1;
             }
         }
-        
-        return goal==0?true:false;
+        dp[idx] = -1;
+        return -1;
     }
 }
