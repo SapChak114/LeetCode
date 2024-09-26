c3e758878b688587b80a5fe5d1159cbe2f9d9b4f
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-09-26 10:50:47 +0530

    Time: 2 ms (99.86%), Space: 45.5 MB (6.66%) - LeetHub

--- 0902-minimum-number-of-refueling-stops/0902-minimum-number-of-refueling-stops.java
+++ 0902-minimum-number-of-refueling-stops/0902-minimum-number-of-refueling-stops.java
@@ -1,13 +1,13 @@
 class Solution {
     public int minRefuelStops(int target, int startFuel, int[][] stations) {
-        int n = stations.length, refill = 0;
+        int n = stations.length, refill = 0, i = 0;
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
 
         int distance = startFuel;
 
         while (distance < target) {
-            for (int i = 0; i<n && distance > stations[i][0]; i++) {
-                pq.add(stations[i]);
+            while (i<n && distance >= stations[i][0]) {
+                pq.add(stations[i++]);
             }
 
             if (pq.isEmpty()) {
