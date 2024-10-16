eb2f72f1d6213b5264cf5348aeabcf8e817cd359
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-10-16 17:34:33 +0530

    Time: 12 ms (8.64%), Space: 45.3 MB (7.27%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,32 +1,33 @@
 class Solution {
-    Map<String, Integer> dp;
     int[] prices;
+    Map<String, Integer> dp;
     public int maxProfit(int[] prices) {
-        this.dp = new HashMap<>();
+        int n = prices.length;
         this.prices = prices;
-
+        this.dp = new HashMap<>();
         return dfs(0, true);
     }
 
-    int dfs(int idx, Boolean isBuy) {
+    int dfs(int idx, boolean isBuy) {
         if (idx >= prices.length) {
             return 0;
         }
 
-        String key = idx + "-" + isBuy;
+        String key = idx+"_"+isBuy;
+
         if (dp.containsKey(key)) {
             return dp.get(key);
         }
 
         if (isBuy) {
-            int buy = dfs(idx + 1, !isBuy) - prices[idx];
-            int coolDown = dfs(idx + 1, isBuy);
-
+            int buy = dfs(idx+1, !isBuy) - prices[idx];
+            int coolDown = dfs(idx+1, isBuy);
             dp.put(key, Math.max(buy, coolDown));
             return Math.max(buy, coolDown);
         } else {
-            int sell = dfs(idx + 2, !isBuy) + prices[idx];
-            int coolDown = dfs(idx + 1, isBuy);
+            int sell = dfs(idx+2, !isBuy) + prices[idx];
+            int coolDown = dfs(idx+1, isBuy);
+
             dp.put(key, Math.max(sell, coolDown));
             return Math.max(sell, coolDown);
         }
