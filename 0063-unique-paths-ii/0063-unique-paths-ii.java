05a391267bd1653f645adb9b14d9bf93ba94dc58
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-10-16 17:43:09 +0530

    Time: 0 ms (100%), Space: 42 MB (12.95%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,29 +1,27 @@
 class Solution {
     int n, m;
-    int[][] obs;
+    int[][] grid;
     Integer[][] dp;
-    public int uniquePathsWithObstacles(int[][] obs) {
-        this.n = obs.length;
-        this.m = obs[0].length;
-        this.obs = obs;
+    public int uniquePathsWithObstacles(int[][] grid) {
+        this.n = grid.length;
+        this.m = grid[0].length;
+        this.grid = grid;
         this.dp = new Integer[this.n][this.m];
 
-        return unique(0, 0);
+        return dfs(0, 0);
     }
 
-    int unique(int i, int j) {
-        if (i == n-1 && j == m-1 && obs[i][j] == 0) {
+    int dfs(int i, int j) {
+        if (i == n-1 && j == m-1 && grid[i][j] == 0) {
             return 1;
         }
-
-        if (i >= n || j >= m || obs[i][j] == 1) {
+        if (i >= n || j >= m || grid[i][j] == 1) {
             return 0;
         }
-
         if (dp[i][j] != null) {
             return dp[i][j];
         }
 
-        return dp[i][j] = unique(i+1, j) + unique(i, j+1);
+        return dp[i][j] = dfs(i+1, j) + dfs(i, j+1);
     }
 }
