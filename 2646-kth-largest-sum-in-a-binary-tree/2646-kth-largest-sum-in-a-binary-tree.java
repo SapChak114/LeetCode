6177f1c4628bb201145ba6f668fe2bd77450e29c
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-10-22 23:59:56 +0530

    Time: 80 ms (5.23%), Space: 85.6 MB (5.09%) - LeetHub

---
+++ 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public long kthLargestLevelSum(TreeNode root, int k) {
+        Map<Integer, Long> map = new HashMap<>();
+        dfs(root, map, 0);
+
+        System.out.println(map);
+        List<Long> ans = new ArrayList<>(map.values());
+        Collections.sort(ans, Collections.reverseOrder());
+        if (ans.size() >= k) {
+            return ans.get(k-1);
+        } else {
+            return -1;
+        }
+    }
+
+    void dfs(TreeNode root, Map<Integer, Long> map, int level) {
+        if (root == null) {
+            return;
+        }
+
+        map.put(level, map.getOrDefault(level, 0L) + root.val);
+        dfs(root.left, map, level + 1);
+        dfs(root.right, map, level + 1);
+    }
+}
