1cfe87ba788ec3c1789922cfca0335e0641506bb
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-11-02 02:41:51 +0530

    Time: 23 ms (79.37%), Space: 44.9 MB (8.15%) - LeetHub

--- 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.java
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.java
@@ -1,42 +1,53 @@
 class Solution {
-
     public int minimumMountainRemovals(int[] nums) {
-        int N = nums.length;
+        List<Integer> integerList = Arrays.stream(nums)
+                                          .boxed()
+                                          .collect(Collectors.toList());
+        List<Integer> leftList = findSize(integerList);
+        Collections.reverse(integerList);
+        List<Integer> rightList = findSize(integerList);
+        Collections.reverse(rightList);
 
-        int[] lisLength = new int[N];
-        int[] ldsLength = new int[N];
+        int min = 0;
 
-        Arrays.fill(lisLength, 1);
-        Arrays.fill(ldsLength, 1);
+        for (int i = 0; i<leftList.size(); i++) {
+            if (leftList.get(i) > 1 && rightList.get(i) > 1) {
+                min = Math.max(min, leftList.get(i) + rightList.get(i)-1);
+            }
+        }
+        return nums.length - min;
 
-        // Stores the length of longest increasing subsequence that ends at i.
-        for (int i = 0; i < N; i++) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (nums[i] > nums[j]) {
-                    lisLength[i] = Math.max(lisLength[i], lisLength[j] + 1);
-                }
+    }
+
+    List<Integer> findSize(List<Integer> nums) {
+        List<Integer> list = new ArrayList<>();
+        List<Integer> sizeList = new ArrayList<>();
+        for (int num : nums) {
+            int idx = bs(num, list);
+            if (idx == list.size()) {
+                list.add(num);
+            }
+            list.set(idx, num);
+            sizeList.add(list.size());
+        }
+        return sizeList;
+    }
+
+    int bs(int targ, List<Integer> list) {
+        int l = 0, r = list.size();
+
+        while (l < r) {
+            int m = (l + r) >> 1;
+
+            if (list.get(m) == targ) {
+                return m;
+            } else if (list.get(m) < targ) {
+                l = m + 1;
+            } else {
+                r = m;
             }
         }
 
-        // Stores the length of longest decreasing subsequence that starts at i.
-        for (int i = N - 1; i >= 0; i--) {
-            for (int j = i + 1; j < N; j++) {
-                if (nums[i] > nums[j]) {
-                    ldsLength[i] = Math.max(ldsLength[i], ldsLength[j] + 1);
-                }
-            }
-        }
-
-        int minRemovals = Integer.MAX_VALUE;
-        for (int i = 0; i < N; i++) {
-            if (lisLength[i] > 1 && ldsLength[i] > 1) {
-                minRemovals = Math.min(
-                    minRemovals,
-                    N - lisLength[i] - ldsLength[i] + 1
-                );
-            }
-        }
-
-        return minRemovals;
+        return l;
     }
 }
