8cb509bc57f4462ee5521751baa247d99542b9dc
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-11-07 16:34:16 +0530

    Time: 33 ms (53.97%), Space: 56.7 MB (35.46%) - LeetHub

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.java
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.java
@@ -1,23 +1,15 @@
 class Solution {
-
     public int largestCombination(int[] candidates) {
-        // Initialize an array to store the count of each bit position.
         int[] bitCount = new int[24];
-        for (int i = 0; i < 24; i++) {
-            for (int num : candidates) {
-                // Check if the i-th bit is set.
-                if ((num & (1 << i)) != 0) {
+
+        for (int i = 0; i<24; i++) {
+            for (int candidate : candidates) {
+                if ((candidate & (1 << i)) != 0) {
                     bitCount[i]++;
                 }
             }
         }
-        // Return the maximum count.
-        int max = 0;
-        for (int count : bitCount) {
-            if (count > max) {
-                max = count;
-            }
-        }
-        return max;
+
+        return Arrays.stream(bitCount).max().getAsInt();
     }
 }
