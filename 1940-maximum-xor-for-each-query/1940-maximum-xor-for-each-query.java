14aab7062bb2ec95668229ce4047382ed354a2de
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-11-08 21:17:23 +0530

    Time: 2 ms (100%), Space: 58.6 MB (73.09%) - LeetHub

--- 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.java
+++ 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.java
@@ -8,10 +8,10 @@ class Solution {
 
         int[] answer = new int[n];
         int mask = (1 << maximumBit) - 1;
-        int idx = 0;
-        for (int i = nums.length - 1; i>=0; i--) {
-            answer[idx++] = xor ^ mask;
-            xor ^= nums[i];
+        
+        for (int i = 0; i < n; i++) {
+            answer[i] = xor ^ mask;
+            xor ^= nums[n - i - 1];
         }
 
         return answer;
