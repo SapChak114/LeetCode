9cd67c0ef99de778611ac8aea7c59d59e57c6374
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2024-12-15 16:54:33 +0530

    Time: 3 ms (94.34%), Space: 57.8 MB (74.44%) - LeetHub

---
+++ 2868-continuous-subarrays/2868-continuous-subarrays.java
@@ -0,0 +1,35 @@
+class Solution {
+    public long continuousSubarrays(int[] nums) {
+        int left = 0, right = 0, currMin = Integer.MAX_VALUE, currMax = Integer.MIN_VALUE;
+        long win = 0, total = 0;
+        
+        for (right = 0; right < nums.length; right++) {
+            currMin = Math.min(currMin, nums[right]);
+            currMax = Math.max(currMax, nums[right]);
+
+            if (currMax - currMin > 2) {
+                win = right - left;
+                total += ((win * (win + 1))/2);
+
+                left = right;
+                currMin = currMax = nums[right];
+
+                while (left > 0 && Math.abs(nums[left-1] - nums[right]) <= 2) {
+                    left--;
+                    currMin = Math.min(currMin, nums[left]);
+                    currMax = Math.max(currMax, nums[left]);
+                }
+
+                if (left < right) {
+                    win = right - left;
+                    total -= ((win * (win + 1))/2);
+                }
+            }
+        }
+
+        win = right - left;
+        total += ((win * (win + 1))/2);
+
+        return total;
+    }
+}
