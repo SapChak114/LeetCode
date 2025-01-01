9e875ab89c3b36badf20921efbafddaa8d845832
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-01-01 12:35:22 +0530

    Time: 1 ms (97.36%), Space: 41.1 MB (96.84%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
@@ -1,39 +1,21 @@
 class Solution {
     public int maxScore(String s) {
-        char[] ch = s.toCharArray();
-        List<Integer> nums = new ArrayList<>();
+        int n = s.length();
+        int right = 0;
 
-        for (int i = 0; i<ch.length; i++) {
-            nums.add(ch[i]-'0');
-        }
-
-        int[] pfOnes = new int[nums.size()];
-        int[] pfZeros = new int[nums.size()];
-
-        int n = pfOnes.length;
-        if (nums.get(0) == 0) {
-            pfZeros[0] = 1;
-        }
-
-        if (nums.get(0) == 1) {
-            pfOnes[0] = 1;
-        }
-
-        for (int i = 1; i<n; i++) {
-            if (nums.get(i) == 1) {
-                pfOnes[i] = pfOnes[i-1] + 1;
-                pfZeros[i] = pfZeros[i-1];
-            } else {
-                pfZeros[i] = pfZeros[i-1] + 1;
-                pfOnes[i] = pfOnes[i-1];
+        for (int i = 0; i<n; i++) {
+            if (s.charAt(i) == '1') {
+                right++;
             }
         }
 
-        int ans = 0;
-        for (int i = 0; i<n-1; i++) {
-            int left = pfZeros[i];
-            int right = pfOnes[n-1] - pfOnes[i];
-            int sum =  left + right;
+        int left = 0, ans = 0;
+        for (int i = 0 ;i<n-1; i++) {
+            if (s.charAt(i) == '1') {
+                left++;
+            }
+            
+            int sum = ((i+1) - left) +  (right - left);
             ans = Math.max(ans, sum);
         }
 
