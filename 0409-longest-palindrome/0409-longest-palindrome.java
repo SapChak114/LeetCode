5182de5f14c1756cc1ae6836442797d78f13012f
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-01-05 01:20:41 +0530

    Time: 5 ms (66.65%), Space: 42.5 MB (8.42%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        Set<Character> set = new HashSet<>();
+
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (set.contains(c)) {
+                set.remove(c);
+                count += 2;
+            } else {
+                set.add(c);
+            }
+        }
+
+        return set.isEmpty() ? count : count + 1;
+    }
+}
