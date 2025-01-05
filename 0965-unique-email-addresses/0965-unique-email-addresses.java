bdbb6d62669e871ba06320675f79249286db2636
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-01-05 11:37:14 +0530

    Time: 25 ms (18.13%), Space: 45.7 MB (9.43%) - LeetHub

--- 0965-unique-email-addresses/0965-unique-email-addresses.java
+++ 0965-unique-email-addresses/0965-unique-email-addresses.java
@@ -1,26 +1,22 @@
 class Solution {
     public int numUniqueEmails(String[] emails) {
         Set<String> set = new HashSet<>();
-
-        for (String s : emails) {
-            StringBuilder sb = new StringBuilder();
-            int idx = s.indexOf("@");
-            for (int i = 0; i<=idx; i++) {
-                if (s.charAt(i) != '.') {
-                    if (s.charAt(i) == '+') {
-                        sb.append(s.charAt(idx));
-                        break;
-                    }
-                    // System.out.print(s.charAt(i)+" ");
-                    sb.append(s.charAt(i));
+        for (String email : emails) {
+            int endIdx = email.indexOf("@");
+            email = email.substring(0, endIdx).replaceAll("\\.", "")+email.substring(endIdx);
+            int idx = email.indexOf("+");
+            if (idx != -1) {
+                endIdx = email.indexOf("@");
+                if (idx < endIdx) {
+                    String subString = email.substring(idx, endIdx);
+                    email = email.replace(subString, "");
                 }
+
             }
-            for (int i = idx+1; i<s.length(); i++) {
-                sb.append(s.charAt(i));
-            }
-            set.add(sb.toString());
-            System.out.println();
+
+            set.add(email);
         }
+
         System.out.println(set);
         return set.size();
     }
