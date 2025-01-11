833d2e3ce6ed20dcd526c859f8e9dffa65df82d6
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-01-12 00:50:55 +0530

    Time: 11 ms (10.49%), Space: 70.8 MB (98.06%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.java
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.java
@@ -1,38 +1,33 @@
 class Solution {
-
     public String shiftingLetters(String s, int[][] shifts) {
         int n = s.length();
-        int[] diffArray = new int[n]; // Initialize a difference array with all elements set to 0.
+        int[] sums = new int[n+1];
 
-        // Process each shift operation
         for (int[] shift : shifts) {
-            if (shift[2] == 1) { // If direction is forward (1)
-                diffArray[shift[0]]++; // Increment at the start index
-                if (shift[1] + 1 < n) {
-                    diffArray[shift[1] + 1]--; // Decrement at the end+1 index
-                }
-            } else { // If direction is backward (0)
-                diffArray[shift[0]]--; // Decrement at the start index
-                if (shift[1] + 1 < n) {
-                    diffArray[shift[1] + 1]++; // Increment at the end+1 index
-                }
+            int delta = shift[2] == 0 ? -1 : 1;
+            int start = shift[0];
+            int end = shift[1] + 1;
+
+            sums[start] += delta;
+            sums[end] += -delta;
+        }
+
+        for (int i = 1; i<sums.length; i++) {
+            sums[i] += sums[i-1];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i<n; i++) {
+            int ascii = s.charAt(i) - 'a';
+            ascii = (ascii + sums[i]) % 26;
+
+            if (ascii < 0) {
+                ascii += 26;
             }
+
+            sb.append(""+(char)(ascii + 'a'));
         }
 
-        StringBuilder result = new StringBuilder(s);
-        int numberOfShifts = 0;
-
-        // Apply the shifts to the string
-        for (int i = 0; i < n; i++) {
-            numberOfShifts = (numberOfShifts + diffArray[i]) % 26; // Update cumulative shifts, keeping within the alphabet range
-            if (numberOfShifts < 0) numberOfShifts += 26; // Ensure non-negative shifts
-
-            // Calculate the new character by shifting `s[i]`
-            char shiftedChar = (char) ('a' +
-                ((s.charAt(i) - 'a' + numberOfShifts) % 26));
-            result.setCharAt(i, shiftedChar);
-        }
-
-        return result.toString();
+        return sb.toString();
     }
 }
