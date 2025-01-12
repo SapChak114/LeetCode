3a2bcdd0363c14625b223ff995c5a377988e4379
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-01-12 09:30:55 +0530

    Time: 6 ms (37.91%), Space: 45.6 MB (19.69%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
@@ -14,28 +14,31 @@
  * }
  */
 class Solution {
-    Map<Integer, Integer> map;
+    Map<Integer, Integer> map;    
     public int[] findMode(TreeNode root) {
         this.map = new HashMap<>();
         inorder(root);
+        int maxFreq = 0, maxElem = 0;
 
-        int maxFreq = 0;
-        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
-            maxFreq = Math.max(maxFreq, e.getValue());
-        }
-        
         List<Integer> ans = new ArrayList<>();
         for (Map.Entry<Integer, Integer> e : map.entrySet()) {
-            if (e.getValue() == maxFreq) {
+            if (e.getValue() > maxFreq) {
+                maxFreq = e.getValue();
+                maxElem = e.getKey();
+            }
+        }
+        
+        ans.add(maxElem);
+        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+            if (e.getValue() == maxFreq && e.getKey() != maxElem) {
                 ans.add(e.getKey());
             }
         }
-
         int[] res = new int[ans.size()];
-        for (int i = 0; i<ans.size(); i++) {
+
+        for (int i = 0; i<res.length; i++) {
             res[i] = ans.get(i);
         }
-
         return res;
     }
 
@@ -43,8 +46,9 @@ class Solution {
         if (root == null) {
             return;
         }
+
         inorder(root.left);
-        map.put(root.val, map.getOrDefault(root.val, 0)+1);
+        map.put(root.val, map.getOrDefault(root.val, 0) + 1);
         inorder(root.right);
     }
 }
