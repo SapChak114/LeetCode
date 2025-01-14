507e40f7ffb53ec6d561c7e72e490ee3f532050d
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-01-14 17:27:48 +0530

    Time: 9 ms (57.2%), Space: 46 MB (28.81%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.java
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -1,44 +1,45 @@
 class Pair implements Comparable<Pair>{
-    int num;
     int idx;
+    int num;
 
-    public Pair(int num, int idx) {
-        this.num = num;
+    public Pair(int idx, int num) {
         this.idx = idx;
+        this.num = num;
     }
 
     public int compareTo(Pair other) {
         if (this.num == other.num) {
-            return Integer.compare(other.idx, this.idx);
+            return Integer.compare(this.idx, other.idx);
         }
+
         return Integer.compare(other.num, this.num);
     }
 }
 class Solution {
     public String[] findRelativeRanks(int[] score) {
-        List<Pair> list = new ArrayList<>();
         int n = score.length;
 
+        List<Pair> list = new ArrayList<>();
         for (int i = 0; i<n; i++) {
-            list.add(new Pair(score[i], i));
+            list.add(new Pair(i, score[i]));
         }
 
         Collections.sort(list);
 
-        String[] sb = new String[n];
+        String[] res = new String[n];
         for (int i = 0; i<n; i++) {
             Pair p = list.get(i);
             if (i == 0) {
-                sb[p.idx] = "Gold Medal";
+                res[p.idx] = "Gold Medal";
             } else if (i == 1) {
-                sb[p.idx] = "Silver Medal";
+                res[p.idx] = "Silver Medal";
             } else if (i == 2) {
-                sb[p.idx] = "Bronze Medal";
+                res[p.idx] = "Bronze Medal";
             } else {
-                sb[p.idx] = Integer.toString(i + 1);
+                res[p.idx] = Integer.toString(i + 1);
             }
         }
 
-        return sb;
+        return res;
     }
 }
