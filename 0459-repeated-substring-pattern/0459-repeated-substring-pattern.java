f0804e1ccd9c2380988ecfaa4fd17396f50d25e6
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-01-14 17:43:51 +0530

    Time: 7 ms (93.93%), Space: 45.3 MB (52.66%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -3,12 +3,12 @@ class Solution {
         int n = s.length();
 
         for (int i = 1; i<n; i++) {
-            //if (n % i == 0) {
+            if (n % i == 0) {
                 String sub = s.substring(0, i).repeat(n/i);
                 if (s.equals(sub)) {
                     return true;
                 }
-            //}
+            }
         }
 
         return false;
