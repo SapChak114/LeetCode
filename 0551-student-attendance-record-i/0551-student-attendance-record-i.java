2a2810df4b39dd7255e8e9a2848daadbd2ccd32e
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-01-15 23:41:42 +0530

    Time: 1 ms (35.78%), Space: 41.7 MB (30.86%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.java
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.java
@@ -1,24 +1,24 @@
 class Solution {
     public boolean checkRecord(String s) {
-        int A = 0, L = 0;
-        for (char c : s.toCharArray()) {
-            if (c == 'A') {
-                A++;
-                if (A >= 2) {
-                    return false;
-                }
-            }
-            
-            if (c == 'L') {
-                L++;
-                if (L >= 3) {
-                    return false;
-                }
-            } else {
-                L = 0;
+        Map<Character, Integer> map = new HashMap<>();
+
+        int countA = 0, countL = 0;
+
+        int n = s.length();
+
+        countA += s.charAt(0) == 'A' ? 1 : 0;
+        for (int i = 1; i<n-1; i++) {
+            if (s.charAt(i-1) == 'L' && s.charAt(i) == 'L' && s.charAt(i+1) == 'L') {
+                return false;
+            } else if (s.charAt(i) == 'A') {
+                countA++;
             }
         }
 
-        return true;
+        if (n > 1) {
+            countA += s.charAt(n-1) == 'A' ? 1 : 0;
+        }
+
+        return countA < 2;
     }
 }
