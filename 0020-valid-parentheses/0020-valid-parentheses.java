5db3ef183e15ce9adcad20dc7f40e5f70a0a2ccb
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-01-16 21:01:39 +0530

    Time: 2 ms (97.71%), Space: 42 MB (32.32%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,25 +1,33 @@
 class Solution {
     public boolean isValid(String s) {
         char[] ch = s.toCharArray();
-        int n = s.length();
 
         Stack<Character> st = new Stack<>();
-
-        for (int i = 0; i<n; i++) {
-            char c = ch[i];
-
-            if (c == '(' || c == '{' || c == '[') {
+        for (char c : ch) {
+            if (c == '(' || c == '[' || c == '{') {
                 st.push(c);
+            } else if (st.isEmpty()) {
+                return false;
             } else {
-                if (!st.isEmpty() && 
-                (
-                (st.peek() == '(' && s.charAt(i) == ')')
-                || (st.peek() == '{' && s.charAt(i) == '}')
-                || (st.peek() == '[' && s.charAt(i) == ']')
-                )) {
-                    st.pop();
-                } else {
-                    return false;
+                switch (c) {
+                    case ')':
+                        char cc = st.pop();
+                        if (cc == '[' || cc == '{') {
+                            return false;
+                        }
+                        break;
+                    case ']':
+                        cc = st.pop();
+                        if (cc == '(' || cc == '{') {
+                            return false;
+                        }
+                        break;
+                    case '}':
+                        cc = st.pop();
+                        if (cc == '[' || cc == '(') {
+                            return false;
+                        }
+                        break;
                 }
             }
         }
