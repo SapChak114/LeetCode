ddc5b22cc4cb4e7b305f9c78fbd1696b5fd6438f
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-01-19 12:58:58 +0530

    Time: 21 ms (61.54%), Space: 45.2 MB (56.64%) - LeetHub

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -1,48 +1,47 @@
-class Solution {
-    class Pair {
-        int x, y, cost;
+class Pair{
+    int x;
+    int y;
+    int w;
 
-        public Pair(int x, int y, int cost) {
-            this.x = x;
-            this.y = y;
-            this.cost = cost;
-        }
+    public Pair(int x, int y, int w){
+        this.x = x;
+        this.y = y;
+        this.w = w;
     }
-
+}
+class Solution {
     int[] dx = {0, 0, 1, -1};
     int[] dy = {1, -1, 0, 0};
+    public int minCost(int[][] B) {
+        int n = B.length, m = B[0].length;
+        PriorityQueue<Pair> pq = new PriorityQueue<>((o1,o2)->o1.w-o2.w);
+        pq.add(new Pair(0,0,0));
 
-    public int minCost(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.cost - b.cost);
-        int[][] dist = new int[m][n];
-        
-        for (int[] d : dist) {
+        int[][] dist = new int[n][m];
+
+        for(int[] d : dist){
             Arrays.fill(d, Integer.MAX_VALUE);
         }
-
-        pq.add(new Pair(0, 0, 0));
         dist[0][0] = 0;
 
-        while (!pq.isEmpty()) {
+        while(!pq.isEmpty()){
             Pair p = pq.poll();
 
-            if (p.x == m - 1 && p.y == n - 1) {
-                return p.cost;
+            if (p.x == n - 1 && p.y == m - 1) {
+                return p.w;
             }
-
-            for (int i = 0; i < 4; i++) {
-                int nx = p.x + dx[i];
-                int ny = p.y + dy[i];
-                int newCost = p.cost + (grid[p.x][p.y] == i + 1 ? 0 : 1);
-
-                if (nx >= 0 && nx < m && ny >= 0 && ny < n && dist[nx][ny] > newCost) {
-                    dist[nx][ny] = newCost;
-                    pq.add(new Pair(nx, ny, newCost));
+            
+            for(int i = 0; i<4; i++){
+                int x = p.x + dx[i];
+                int y = p.y + dy[i];
+                int newCost = p.w + (B[p.x][p.y] == i + 1 ? 0 : 1);
+                if (x >= 0 && x < n && y >= 0 && y < m && dist[x][y] > newCost) {
+                    dist[x][y] = newCost;
+                    pq.add(new Pair(x, y, newCost));
                 }
             }
         }
 
-        return -1; // Shouldn't reach here
+        return -1;
     }
 }
