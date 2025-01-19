1e62baa9af49ce72c74247da98d461a82961a506
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-01-19 20:08:09 +0530

    Time: 0 ms (100%), Space: 40.6 MB (70.33%) - LeetHub

--- 2509-minimize-xor/2509-minimize-xor.java
+++ 2509-minimize-xor/2509-minimize-xor.java
@@ -1,23 +1,22 @@
 class Solution {
     public int minimizeXor(int num1, int num2) {
-        int setBit = Integer.bitCount(num2);
+        int setBits = Integer.bitCount(num2);
         int x = 0;
 
         for (int bits = 31; bits>=0; bits--) {
-            if ((((num1 >> bits) & 1) == 1) && setBit > 0) {
-                setBit--;
+            if (((num1 >> bits) & 1) == 1 && setBits > 0) {
+                setBits--;
                 x |= (1 << bits);
             }
         }
 
         for (int bits = 0; bits<=31; bits++) {
-            if (setBit > 0 && ((x >> bits) & 1) == 0) {
-                setBit--;
-                x |= (1<<bits);
+            if ((setBits > 0) && ((x >> bits) & 1) == 0) {
+                setBits--;
+                x |= (1 << bits);
             }
         }
 
         return x;
-        
     }
 }
