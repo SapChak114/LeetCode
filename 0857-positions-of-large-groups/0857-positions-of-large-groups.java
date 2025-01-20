af0da4b39620d74c4316ae0a39590744a73e1cbb
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-01-20 20:19:47 +0530

    Time: 2 ms (32.91%), Space: 44.8 MB (56.96%) - LeetHub

--- 0857-positions-of-large-groups/0857-positions-of-large-groups.java
+++ 0857-positions-of-large-groups/0857-positions-of-large-groups.java
@@ -1,24 +1,26 @@
 class Solution {
     public List<List<Integer>> largeGroupPositions(String s) {
-        int last = 0;
-        s += "#";
+        s += '#';
         List<List<Integer>> ans = new ArrayList<>();
-        int total = 1;
-        int n = s.length();
+        char[] ch = s.toCharArray();
+        int n = ch.length, total = 1, last = 0;
         for (int i = 1; i<n; i++) {
-            if (s.charAt(i) == s.charAt(i-1)) {
+            if (ch[i] == ch[i-1]) {
                 total++;
+            } else if (total >= 3) {
+                List<Integer> li = new ArrayList<>();
+                li.add(last);
+                li.add(i-1);
+                ans.add(li);
+
+                total = 1;
+                last = i;
             } else {
-                if (total >= 3) {
-                    List<Integer> li = new ArrayList<>();
-                    li.add(last);
-                    li.add(i-1);
-                    ans.add(li);
-                }
-                total=1;
-                last=i;
+                last = i;
+                total = 1;
             }
         }
+
         return ans;
     }
 }
