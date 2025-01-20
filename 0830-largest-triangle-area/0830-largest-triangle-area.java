b08e906d85652771cfd33bbdfbf611d5ebdda93e
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-01-20 21:07:30 +0530

    Time: 5 ms (95.27%), Space: 41.7 MB (40.45%) - LeetHub

--- 0830-largest-triangle-area/0830-largest-triangle-area.java
+++ 0830-largest-triangle-area/0830-largest-triangle-area.java
@@ -1,12 +1,12 @@
 class Solution {
-    public double largestTriangleArea(int[][] P) {
-        double ans = 0.0d;
-        int N = P.length;
+    public double largestTriangleArea(int[][] points) {
+        int n = points.length;
 
-        for (int i = 0; i<N; i++) {
-            for (int j = i+1; j<N; j++) {
-                for (int k = j+1; k<N; k++) {
-                    ans = Math.max(ans, area(P[i], P[j], P[k]));
+        double ans = 0;
+        for (int i = 0; i<n; i++) {
+            for (int j = i+1; j<n; j++) {
+                for (int k = j+1; k<n; k++) {
+                    ans = Math.max(ans, area(points[i], points[j], points[k]));
                 }
             }
         }
@@ -14,8 +14,7 @@ class Solution {
         return ans;
     }
 
-    double area(int[] P, int[] Q, int[] R) {
-        return 0.5 * Math.abs((P[0]*Q[1] + Q[0]*R[1] + R[0]*P[1] 
-                    - P[1]*Q[0] - Q[1]*R[0] - R[1]*P[0]));
+    public double area(int[] P, int[] Q, int[] R) {
+        return 0.5 * Math.abs((P[0]*Q[1] + Q[0]*R[1] + R[0]*P[1]) - P[1]*Q[0] - Q[1]*R[0] - R[1]*P[0]);
     }
 }
