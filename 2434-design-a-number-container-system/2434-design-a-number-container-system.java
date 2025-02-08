0d223c2df7fa4e12354cdf588d6dd155be79d777
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-02-09 00:03:20 +0530

    Time: 85 ms (44.52%), Space: 98.4 MB (72.6%) - LeetHub

--- 2434-design-a-number-container-system/2434-design-a-number-container-system.java
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.java
@@ -1,31 +1,30 @@
 class NumberContainers {
 
-    private Map<Integer, TreeSet<Integer>> numberToIndices;
+    private Map<Integer, TreeSet<Integer>> numberToIndex;
     private Map<Integer, Integer> indexToNumbers;
 
 
     public NumberContainers() {
-        this.numberToIndices = new HashMap<>();
+        this.numberToIndex = new HashMap<>();
         this.indexToNumbers = new HashMap<>();
     }
     
     public void change(int index, int number) {
         if (indexToNumbers.containsKey(index)) {
-            int previousNumber = indexToNumbers.get(index);
-            numberToIndices.get(previousNumber).remove(index);
-            if (numberToIndices.get(previousNumber).isEmpty()) {
-                numberToIndices.remove(previousNumber);
+            numberToIndex.get(indexToNumbers.get(index)).remove(index);
+            if (numberToIndex.get(indexToNumbers.get(index)).isEmpty()) {
+                numberToIndex.remove(indexToNumbers.get(index));
             }
         }
 
         indexToNumbers.put(index, number);
-        numberToIndices.putIfAbsent(number, new TreeSet<>());
-        numberToIndices.get(number).add(index);
+        numberToIndex.putIfAbsent(number, new TreeSet<>());
+        numberToIndex.get(number).add(index);
     }
     
     public int find(int number) {
-        if (numberToIndices.containsKey(number)) {
-            return numberToIndices.get(number).first();
+        if (numberToIndex.containsKey(number)) {
+            return numberToIndex.get(number).first();
         }
         return -1;
     }
