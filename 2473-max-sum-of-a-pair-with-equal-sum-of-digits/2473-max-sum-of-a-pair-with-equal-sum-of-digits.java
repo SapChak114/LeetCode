15768eed2a8715fd2729d5fef5d2788250aa5710
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-02-13 19:22:20 +0530

    Time: 19 ms (99.64%), Space: 55.3 MB (94.85%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -1,27 +1,21 @@
 class Solution {
-
     public int maximumSum(int[] nums) {
-        int result = -1;
-        // Array to map digit sums to the largest element with that sum
-        // (82 to cover all possible sums from 0 to 81)
         int[] digitMapping = new int[82];
+        int result = -1;
 
-        for (int element : nums) {
+        for (int num : nums) {
             int digitSum = 0;
-            // Calculating digit sum
-            for (int currValue = element; currValue != 0; currValue /= 10) {
-                int currDigit = currValue % 10;
+
+            for (int currVal = num; currVal != 0; currVal /= 10) {
+                int currDigit = currVal % 10;
                 digitSum += currDigit;
             }
 
-            // Check if there is already an element with the same digit sum
             if (digitMapping[digitSum] > 0) {
-                // Update the result if the sum of the current and mapped element is greater
-                result = Math.max(result, digitMapping[digitSum] + element);
+                result = Math.max(result, digitMapping[digitSum] + num);
             }
 
-            // Update the mapping to store the larger of the current or previous element for this digit sum
-            digitMapping[digitSum] = Math.max(digitMapping[digitSum], element);
+            digitMapping[digitSum] = Math.max(digitMapping[digitSum], num);
         }
 
         return result;
