0ae435d27fac02f3204c0bab8164ee346622db00
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-02-27 22:56:05 +0530

    Time: 0 ms (100%), Space: 42.5 MB (9.56%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,25 +1,26 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int n = nums.length;
-        int i = 0, l = 0, r = n-1;
-        
-        while(i<=r){
-            if(nums[i]==0){
-                swap(nums,i,l);
-                l++;
+        int red = 0, white = 0, blue = 0;
+
+        for (int num : nums) {
+            if (num == 0) {
+                red++;
+            } else if (num == 1) {
+                white++;
+            } else {
+                blue++;
             }
-            else if(nums[i]==2){
-                swap(nums,i,r);
-                r--;
-                i--;
-            }
-            i++;
         }
+
+        fill(0, red, nums, 0);
+        fill(red, red+white, nums, 1);
+        fill(red+white, red+white+blue, nums, 2);
+        
     }
-    
-    void swap(int[] arr, int l, int r){
-        int temp = arr[l];
-        arr[l] = arr[r];
-        arr[r] = temp;
+
+    void fill(int s, int e, int[] nums, int val) {
+        for (int i = s; i<e; i++) {
+            nums[i] = val;
+        }
     }
 }
