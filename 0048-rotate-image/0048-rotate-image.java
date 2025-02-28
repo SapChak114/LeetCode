f852e6401b26c958e994c18673241a6fb27754f4
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-03-01 00:28:33 +0530

    Time: 0 ms (100%), Space: 42 MB (87.85%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,23 +1,29 @@
 class Solution {
-    public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        
-        int i = 0, j = n-1;
-        
-        while(i<j){
-            int[] temp = matrix[i];
-            matrix[i++] = matrix[j];
-            matrix[j--] = temp;
-        }
-        
-        for(i=0; i<n; i++){
-            for(j=i+1; j<m; j++){
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+    public void rotate(int[][] m) {
+        int l = 0, r = m.length - 1;
+
+        while (l < r) {
+            for (int i = 0; i< (r - l); i++) {
+                int top = l, bottom = r;
+
+                // save top left
+                int topLeft = m[top][l + i];
+
+                // move bottom left into top left
+                m[top][l + i] = m[bottom - i][l];
+
+                // move bottom right into bottom left
+                m[bottom - i][l] = m[bottom][r - i];
+
+                // move top right into bottom right
+                m[bottom][r - i] = m[top + i][r];
+
+                // move top left into top right
+                m[top + i][r] = topLeft;
             }
+
+            r -= 1;
+            l += 1;
         }
-        
     }
 }
