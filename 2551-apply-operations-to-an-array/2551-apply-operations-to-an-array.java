62faf51643444ab25fd7df4f6005a32e19a06da7
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-03-01 23:17:25 +0530

    Time: 1 ms (96.37%), Space: 44.6 MB (5.12%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -2,29 +2,26 @@ class Solution {
 
     public int[] applyOperations(int[] nums) {
         int n = nums.length;
-        int[] modifiedNums = new int[n];
-        int zeroCount = 0;
+        int[] res = new int[n];
+        int idx = 0;
 
-        // Step 1: Apply operations on the array
-        for (int index = 0; index < n - 1; index++) {
-            if (nums[index] == nums[index + 1] && nums[index] != 0) {
-                nums[index] *= 2;
-                nums[index + 1] = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
             }
         }
 
-        // Step 2: Move non-zero elements to the front
         for (int num : nums) {
             if (num != 0) {
-                modifiedNums[zeroCount++] = num;
+                res[idx++] = num;
             }
         }
 
-        // Step 3: Append zeros to maintain the original size
-        while (zeroCount < n) {
-            modifiedNums[zeroCount++] = 0;
+        while (idx < n) {
+            res[idx++] = 0;
         }
 
-        return modifiedNums;
+        return res;
     }
 }
