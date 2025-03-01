f94b40d9a198f126f9876ed78bf46267c81f307b
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-03-01 23:06:23 +0530

    Time: 0 ms (100%), Space: 43.5 MB (10.19%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,19 +1,27 @@
 class Solution {
-    public double myPow(double x, int n) {
-        if (x == 0) return 0;
-        if (n == 0) return 1;
-        
-        double res = helper(x, Math.abs((long)n));
-        
-        return (n >= 0) ? res: 1/res;
+    public double myPow(double x, long n) {
+        double res = helper(x, Math.abs(n));
+        if (n >= 0) {
+            return res;
+        } else {
+            return 1/res;
+        }
     }
-    
-    double helper(double x, long n){
-        if(n==0) return 1;
-        
-        double res = helper(x,n/2);
-        res = res * res;
-        
-        return (n%2==0)?res:(x*res);
+
+    double helper(double x, long n) {
+        if (x == 0) {
+            return 0;
+        }
+        if (n == 0) {
+            return 1;
+        }
+
+        double res = helper(x*x, n/2);
+
+        if (n % 2 == 1) {
+            return x * res;
+        } else {
+            return res;
+        }
     }
 }
