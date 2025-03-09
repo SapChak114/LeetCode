409fda49310fe9e59c72c3c33f03417990fdb529
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-03-09 17:09:00 +0530

    Time: 5 ms (30.6%), Space: 42.4 MB (40.13%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,32 +1,39 @@
 class Solution {
+    Map<Character, String> map;
     List<String> ls;
-    Map<Character,String> map;
     char[] ch;
     public List<String> letterCombinations(String digits) {
-        if(digits.isEmpty()) return new ArrayList<>();
-        ls = new ArrayList<>();
-        
-        map = new HashMap<>();
-        map.put('2',"abc");
-        map.put('3',"def");
-        map.put('4',"ghi");
-        map.put('5',"jkl");
-        map.put('6',"mno");
-        map.put('7',"pqrs");
-        map.put('8',"tuv");
-        map.put('9',"wxyz");
-        ch = digits.toCharArray();
-        dfs(0,"");
+        if (digits.isEmpty()) {
+            return new ArrayList<>();
+        }
+        this.map = new HashMap<>();
+
+        this.map = new HashMap<>();
+        this.map.put('2',"abc");
+        this.map.put('3',"def");
+        this.map.put('4',"ghi");
+        this.map.put('5',"jkl");
+        this.map.put('6',"mno");
+        this.map.put('7',"pqrs");
+        this.map.put('8',"tuv");
+        this.map.put('9',"wxyz");
+
+        this.ls = new ArrayList<>();
+        this.ch = digits.toCharArray();
+
+        dfs(0, "");
+
         return ls;
     }
-    
-    void dfs(int i, String combi){
-        if(i==ch.length){
-            ls.add(combi);
+
+    void dfs(int i, String res) {
+        if (i == ch.length) {
+            ls.add(res);
             return;
         }
-        
-        for(char c : map.get(ch[i]).toCharArray()) dfs(i+1,combi+""+c);
+
+        for (char c : map.get(ch[i]).toCharArray()) {
+            dfs(i + 1, res+c);
+        }
     }
-    
 }
