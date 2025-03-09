6d09849956a6f48c526dfbe70eb31bc92feb953e
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-03-09 15:42:39 +0530

    Time: 1 ms (92.09%), Space: 41.2 MB (94.68%) - LeetHub

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,28 +1,24 @@
 class Solution {
     public int minimumRecolors(String blocks, int k) {
-        Queue<Character> bQ = new LinkedList<>();
         int numWhites = 0;
 
-        for (int i = 0; i<k; i++) {
-            char currChar = blocks.charAt(i);
-            if (currChar == 'W') {
+        // Count white blocks in the first window of size k
+        for (int i = 0; i < k; i++) {
+            if (blocks.charAt(i) == 'W') {
                 numWhites++;
             }
-            bQ.add(currChar);
         }
 
         int numRecolors = numWhites;
 
-        for (int i = k; i< blocks.length(); i++) {
-            if (bQ.poll() == 'W') {
+        // Sliding window
+        for (int i = k; i < blocks.length(); i++) {
+            if (blocks.charAt(i - k) == 'W') {
                 numWhites--;
             }
-
-            char currChar = blocks.charAt(i);
-            if (currChar == 'W') {
+            if (blocks.charAt(i) == 'W') {
                 numWhites++;
             }
-            bQ.add(currChar);
 
             numRecolors = Math.min(numRecolors, numWhites);
         }
