69b3c03b5027e2669b383cf454947b8838ffdd4a
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-03-10 22:09:39 +0530

    Time: 193 ms (29.69%), Space: 41.1 MB (99.02%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,34 +1,46 @@
 class Solution {
-    int[] dirX = {0,1,0,-1};
-    int[] dirY = {1,0,-1,0};
+    int[] X = {0, 1, 0, -1};
+    int[] Y = {1, 0, -1, 0};
+    String word;
+    int n, m;
     public boolean exist(char[][] board, String word) {
-        int n = board.length, m = board[0].length;
+        this.n = board.length;
+        this.m = board[0].length;
+        this.word = word;
+
         boolean[][] vis = new boolean[n][m];
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                 if(dfs(i,j,board,word,0,vis)) return true;
+
+        for (int i = 0; i<n; i++) {
+            for (int j = 0; j<m; j++) {
+                if (dfs(i, j, vis, 0, board)) {
+                    return true;
+                }
             }
         }
-        
+
         return false;
     }
-    
-    boolean dfs(int i, int j, char[][] board, String word, int pos, boolean[][] vis){
-        if(pos==word.length()){
+
+    boolean dfs(int i, int j, boolean[][] vis, int pos, char[][] board) {
+        if (pos == word.length()) {
             return true;
-        }
-        
-        if(i<0 || i>=board.length || j<0 || j>=board[0].length || word.charAt(pos)!=board[i][j] || vis[i][j]){
+        }       
+
+        if (i < 0 || i>= n || j < 0 || j >= m || vis[i][j] || word.charAt(pos) != board[i][j]) {
             return false;
         }
-        
-        vis[i][j]=true;
-        for(int k = 0; k<dirX.length; k++){
-            int r = i + dirX[k];
-            int c = j + dirY[k];
-            if(dfs(r,c,board,word,pos+1,vis)) return true;
+
+        vis[i][j] = true;
+        for (int k = 0; k<4; k++) {
+            int x = X[k] + i;
+            int y = Y[k] + j;
+
+            if (dfs(x, y, vis, pos + 1, board)) {
+                return true;
+            }
         }
-        vis[i][j]=false;
+        vis[i][j] = false;
+
         return false;
     }
 }
