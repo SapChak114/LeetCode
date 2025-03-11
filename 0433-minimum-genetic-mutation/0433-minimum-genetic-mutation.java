89e8309ced385418b768a363f485dea61f12784c
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-03-11 17:09:14 +0530

    Time: 13 ms (10.87%), Space: 43.6 MB (7.44%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
@@ -1,52 +1,52 @@
 class Solution {
-    Set<String> genes;
+    Set<String> wordSet;
     public int minMutation(String startGene, String endGene, String[] bank) {
-        genes = new HashSet<>();
-        for(String b : bank) genes.add(b);
-        if(!genes.contains(endGene)){
-            return -1;
+        this.wordSet = new HashSet<>();
+
+        for (String b : bank) {
+            this.wordSet.add(b);
         }
-        
+
         Queue<String> queue = new LinkedList<>();
         Queue<Integer> levels = new LinkedList<>();
-        
         queue.add(startGene);
         levels.add(0);
-        
+
         Set<String> vis = new HashSet<>();
-        
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             String word = queue.poll();
             int level = levels.poll();
-            if(word.equals(endGene)) return level;
-            
-            for(String w : getGenes(word)){
-                if(!vis.contains(w)){
-                    queue.add(w);
-                    levels.add(level+1);
-                    vis.add(w);
+            if (endGene.equals(word)) {
+                return level;
+            }
+
+            for (String s : getNei(word)) {
+                if (!vis.contains(s)) {
+                    queue.add(s);
+                    levels.add(level + 1);
+                    vis.add(s);
                 }
             }
         }
-        
+
         return -1;
-        
     }
-    
-    List<String> getGenes(String gene){
-        List<String> res = new ArrayList<>();
-        
-        for(int i = 0; i<gene.length(); i++){
-            for(char c = 'A'; c<='Z'; c++){
-                if(gene.charAt(i)==c) continue;
-                String nword = gene.substring(0,i)+""+c+""+gene.substring(i+1);
-                //System.out.println(nword);
-                if(genes.contains(nword)){
-                   res.add(nword); 
+
+    List<String> getNei(String word) {
+        List<String> wordList = new ArrayList<>();
+
+        for (int i = 0; i<word.length(); i++) {
+            for (char c = 'A'; c<='Z'; c++) {
+                if (word.charAt(i) == c) {
+                    continue;
+                }
+                String nWord = word.substring(0, i) + c + word.substring(i + 1);
+                if (wordSet.contains(nWord)) {
+                    wordList.add(nWord);
                 }
             }
         }
-        
-        return res;
+
+        return wordList;
     }
 }
