7ba814407400df6fd5f472aadded2f0ec2d4fd10
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-03-11 15:58:48 +0530

    Time: 186 ms (25.25%), Space: 48.2 MB (23.86%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,46 +1,52 @@
 class Solution {
     Set<String> set;
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        set = new HashSet<>(wordList);
-        if(!set.contains(endWord)) return 0;
-        
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {  
+        this.set = new HashSet<>(wordList);
+        if (!set.contains(endWord)) {
+            return 0;
+        }
+
         Queue<String> queue = new LinkedList<>();
         Queue<Integer> levels = new LinkedList<>();
+
         queue.add(beginWord);
         levels.add(1);
         Set<String> vis = new HashSet<>();
-        
-        while(!queue.isEmpty()){
+
+        while (!queue.isEmpty()) {
             String word = queue.poll();
-            int l = levels.poll(); 
-            if(word.equals(endWord)) return l;
-            for(String w : getNeighbours(word)){
-                if(!vis.contains(w)){
+            int l = levels.poll();
+            if (word.equals(endWord)) {
+                return l;
+            }
+
+            for (String w : getNei(word)) {
+                if (!vis.contains(w)) {
                     queue.add(w);
-                    levels.add(l+1);
+                    levels.add(l + 1);
                     vis.add(w);
                 }
             }
         }
-        
+
         return 0;
-        
     }
-    
-    
-    List<String> getNeighbours(String word){
+
+    List<String> getNei(String word) {
         List<String> list = new ArrayList<>();
-        
-        for(int i = 0; i<word.length(); i++){
-            for(char c = 'a'; c<='z'; c++){
-                if(c==word.charAt(i)) continue;
-                String nWord = word.substring(0,i)+c+word.substring(i+1);
-                if(set.contains(nWord)){
+
+        for (int i = 0; i<word.length(); i++) {
+            for (char c = 'a'; c<='z'; c++) {
+                if (c == word.charAt(i)) {
+                    continue;
+                }
+                String nWord = word.substring(0, i) + c + word.substring(i+1);
+                if (set.contains(nWord)) {
                     list.add(nWord);
                 }
             }
         }
-        
+
         return list;
     }
 }
