44a30b811754e5847bc0351eed403a1d2834b2a0
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-03-11 22:17:53 +0530

    Time: 11 ms (23.8%), Space: 45.6 MB (6.35%) - LeetHub

---
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int snakesAndLadders(int[][] board) {
+        Map<Integer, Integer> posMap = new HashMap<>();
+        int n = board.length;
+        int pos = 1;
+        List<Integer> flatBoard = new ArrayList<>();
+
+        for (int i = n-1; i>=0; i--) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j<n; j++) {
+                row.add(board[i][j]);
+            }
+
+            if ((n - i - 1) % 2 == 1) {
+                Collections.reverse(row);
+            }
+
+            flatBoard.addAll(row);
+        }
+
+        for (int i = 0; i<flatBoard.size(); i++) {
+            if (flatBoard.get(i) != -1) {
+                posMap.put(i+1, flatBoard.get(i));
+            }
+        }
+
+
+        Queue<int[]> q = new LinkedList<>();
+        boolean[] vis = new boolean[n * n + 1];
+        q.add(new int[]{1, 0});
+        vis[1] = true;
+
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            int currPos = curr[0];
+            int moves = curr[1];
+
+            if (currPos == n*n) {
+                return moves;
+            }
+
+            for (int dice = 1; dice <= 6; dice++) {
+                int nextPos = currPos + dice;
+                if (nextPos > n*n) {
+                    break;
+                }
+
+                if (posMap.containsKey(nextPos)) {
+                    nextPos = posMap.get(nextPos);
+                }
+
+                if (!vis[nextPos]) {
+                    vis[nextPos] = true;
+                    q.add(new int[]{nextPos, moves + 1});
+                }
+            }
+        }
+
+        return -1;
+    }
+}
