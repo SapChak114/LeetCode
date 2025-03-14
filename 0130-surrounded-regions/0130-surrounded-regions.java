1ee28ec8d79a3c5948e73cd7a24b122f833d075d
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-03-14 18:53:45 +0530

    Time: 3 ms (33.51%), Space: 45.9 MB (15.66%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,43 +1,47 @@
 class Solution {
     int n, m;
+    char[][] board;
     public void solve(char[][] board) {
         this.n = board.length;
         this.m = board[0].length;
-        
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(board[i][j]=='O' && ((i==0 || i==n-1) ||(j==0 || j==m-1))){
-                    dfs(board,i,j);
+        this.board = board;
+
+        for (int i = 0; i<n; i++) {
+            for (int j = 0; j<m; j++) {
+                if (board[i][j] == 'O' && ((i == 0 || i == n-1) || (j == 0 || j == m-1))) {
+                    dfs(i, j);
                 }
             }
         }
-        
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(board[i][j]=='O'){
-                    board[i][j]='X';
+
+        for (int i = 0; i<n; i++) {
+            for (int j = 0; j<m; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
                 }
             }
         }
-        
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(board[i][j]=='#'){
-                    board[i][j]='O';
+
+        for (int i = 0; i<n; i++) {
+            for (int j = 0; j<m; j++) {
+                if (board[i][j] == '#') {
+                    board[i][j] = 'O';
                 }
             }
         }
-        
     }
-    
-    void dfs(char[][] board, int r, int c){
-        if(r>=n || r<0 || c>=m || c<0 || board[r][c]!='O') return;
-        
-        board[r][c]='#';
-        
-        dfs(board,r+1,c);
-        dfs(board,r-1,c);
-        dfs(board,r,c+1);
-        dfs(board,r,c-1);
+
+
+    void dfs(int r, int c) {
+        if (r >= n || r < 0 || c >= m || c < 0 || board[r][c] != 'O') {
+            return;
+        }
+
+        board[r][c] = '#';
+
+        dfs(r + 1, c);
+        dfs(r - 1, c);
+        dfs(r, c + 1);
+        dfs(r, c - 1);
     }
 }
