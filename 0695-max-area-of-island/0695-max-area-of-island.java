dae84b0e31d571435f66256bc2f87b457bca4784
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-03-15 20:14:27 +0530

    Time: 2 ms (71.68%), Space: 45 MB (31.94%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,38 +1,40 @@
 class Solution {
-    int[] dirX = {0,1,0,-1};
-    int[] dirY = {1,0,-1,0};
+    int[][] grid;
+    int[] X = {0, 1, 0, -1};
+    int[] Y = {1, 0, -1, 0};
+    boolean[][] vis;
     public int maxAreaOfIsland(int[][] grid) {
-        int max = 0;
-        
-        for(int i = 0; i<grid.length; i++){
-            for(int j = 0; j<grid[i].length; j++){
-                if(grid[i][j]==1){
-                    max = Math.max(max,dfs(grid,i,j));
+        int n = grid.length;
+        int m = grid[0].length;
+        int maxSize = 0;
+        this.grid = grid;
+        this.vis = new boolean[n][m];
+
+        for (int i = 0; i<n; i++){
+            for (int j = 0; j<m; j++) {
+                if (grid[i][j] == 1 && !vis[i][j]) {
+                    int size = dfs(i, j);
+                    maxSize = Math.max(maxSize, size);
                 }
             }
         }
-        
-        return max;
+
+        return maxSize;
     }
-    
-    int dfs(int[][] grid, int i, int j){
-        grid[i][j]=0;
-        
-        int count = 1;
-        for(int k = 0; k<dirX.length; k++){
-            int r = i + dirX[k];
-            int c = j + dirY[k];
-            
-            if(isValid(grid,r,c)){
-                count += dfs(grid,r,c);
-            }
+
+    int dfs(int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || vis[i][j] || grid[i][j] == 0) {
+            return 0;
         }
-        return count;
-    }
-    
-    boolean isValid(int[][] grid, int r, int c){
-        int n = grid.length, m = grid[0].length;
-        if(r<0 || r>=n || c<0 || c>=m || grid[r][c]!=1) return false;
-        return true;
+        
+        vis[i][j] = true;
+        int size = 1;
+        for (int a = 0; a<X.length; a++) {
+            int r = i + X[a];
+            int c = j + Y[a];
+            size += dfs(r, c);
+        }
+
+        return size;
     }
 }
