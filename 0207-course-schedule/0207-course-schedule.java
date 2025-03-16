fd2f090599ad65921f3780ef27f99c74079adfa3
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-03-16 20:39:24 +0530

    Time: 3 ms (99.9%), Space: 45.3 MB (63.57%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,39 +1,45 @@
 class Solution {
-    public boolean canFinish(int num, int[][] pre) {
-        List<Integer>[] adjList = new ArrayList[num];
-        int[] vis = new int[num];
+    public boolean canFinish(int nc, int[][] pre) {
+        List<Integer>[] adjList = new ArrayList[nc];
         
-        for(int i = 0; i<num; i++) adjList[i] = new ArrayList<>();
-        
-        for(int i = 0; i<pre.length; i++){
+        for (int i = 0; i<nc; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i<pre.length; i++) {
             int u = pre[i][0];
             int v = pre[i][1];
-            
+
             adjList[u].add(v);
         }
-        
-        for(int i = 0; i<num; i++){
-            if(vis[i]==0){
-                if(dfs(vis,adjList,i)) return false;
+
+        int[] vis = new int[nc];
+        for (int i = 0; i<nc; i++) {
+            if (vis[i] == 0) {
+                if (dfs(adjList, vis, i)) {
+                    return false;
+                }
             }
         }
-        
+
         return true;
-        
     }
-    
-    boolean dfs(int[] vis, List<Integer>[] adjList, int node){
+
+    boolean dfs(List<Integer>[] adjList, int[] vis, int node) {
         vis[node] = 1;
-        
-        for(int n : adjList[node]){
-            if(vis[n]==0){
-                if(dfs(vis,adjList,n)) return true;
+
+        for (int n : adjList[node]) {
+            if (vis[n] == 0) {
+                if (dfs(adjList, vis, n)) {
+                    return true;
+                }
+            } else if (vis[n] == 1) {
+                return true;
             }
-            else if(vis[n]==1) return true;
         }
-        
+
         vis[node] = 2;
-        
+
         return false;
     }
 }
