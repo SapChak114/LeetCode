b935401bd4c56017435276b8a4fb9ca7d9fcef6a
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-03-21 00:36:27 +0530

    Time: 20 ms (4.34%), Space: 44.8 MB (5.56%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,21 +1,14 @@
 class Solution {
-    int[] nums;
-    Integer[] dp;
     public int combinationSum4(int[] nums, int target) {
-        dp = new Integer[target+1];
-        this.nums=nums;
-        return rec(target);
-    }
-    
-    int rec(int sum){
-        if(sum==0) return 1;
-        if(sum<0) return 0;
-        
-        if(dp[sum]!=null) return dp[sum];
-        
-        int ans = 0;
-        for(int i = 0; i<nums.length; i++) ans += rec(sum - nums[i]);
-        
-        return dp[sum] = ans;
+        Map<Integer, Integer> dp = new HashMap<>();
+        dp.put(0, 1);
+        for (int total = 1; total <= target; total++) {
+            dp.put(total, 0);
+            for (int n : nums) {
+                dp.put(total, dp.getOrDefault(total, 0) + dp.getOrDefault(total - n, 0));
+            }
+        }
+
+        return dp.get(target);
     }
 }
