35f841d8dcdd1c811eea8df7589121fce4f8d95c
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-05-03 15:11:17 +0530

    Time: 124 ms (43.56%), Space: 59.7 MB (66.26%) - LeetHub

---
+++ 2055-describe-the-painting/2055-describe-the-painting.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Long>> splitPainting(int[][] segments) {
+        TreeMap<Long, Long> map = new TreeMap<>();
+
+        for (int[] seg : segments) {
+            long start = seg[0], end = seg[1], color = seg[2];
+            map.put(start, map.getOrDefault(start, 0L) + color);
+            map.put(end, map.getOrDefault(end, 0L) - color);
+        }
+
+        List<List<Long>> result = new ArrayList<>();
+        long prevTime = 0;
+        long totalColor = 0;
+
+        for (Map.Entry<Long, Long> entry : map.entrySet()) {
+            long currTime = entry.getKey();
+            if (prevTime != currTime && totalColor != 0) {
+                result.add(Arrays.asList(prevTime, currTime, totalColor));
+            }
+            totalColor += entry.getValue();
+            prevTime = currTime;
+        }
+
+        return result;
+    }
+}
