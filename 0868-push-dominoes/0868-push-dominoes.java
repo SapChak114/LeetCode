696a7a735edacec83f6de8ce6ac7c1d9e90ffa9a
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-05-05 01:34:04 +0530

    Time: 24 ms (29.43%), Space: 56.6 MB (5.06%) - LeetHub

--- 0868-push-dominoes/0868-push-dominoes.java
+++ 0868-push-dominoes/0868-push-dominoes.java
@@ -1,35 +1,32 @@
 class Solution {
-    public String pushDominoes(String dominoes) {
-        int N = dominoes.length();
-        int[] indexes = new int[N+2];
-        char[] symbols = new char[N+2];
-        int len = 1;
-        indexes[0] = -1;
-        symbols[0] = 'L';
-
-        for (int i = 0; i < N; ++i)
-            if (dominoes.charAt(i) != '.') {
-                indexes[len] = i;
-                symbols[len++] = dominoes.charAt(i);
+    public String pushDominoes(String domin) {
+        Queue<Pair<Character, Integer>> queue = new LinkedList<>();
+        char[] dominoes = domin.toCharArray();
+        int n = dominoes.length;
+        for (int i = 0; i<n; i++) {
+            if (dominoes[i] != '.') {
+                queue.add(new Pair(dominoes[i], i));
             }
+        }
+      
+        while (!queue.isEmpty()) {
+            Pair p = queue.poll();
+            char domino = (char) p.getKey();
+            int index = (int) p.getValue();
 
-        indexes[len] = N;
-        symbols[len++] = 'R';
-
-        char[] ans = dominoes.toCharArray();
-        for (int index = 0; index < len - 1; ++index) {
-            int i = indexes[index], j = indexes[index+1];
-            char x = symbols[index], y = symbols[index+1];
-            char write;
-            if (x == y) {
-                for (int k = i+1; k < j; ++k)
-                    ans[k] = x;
-            } else if (x > y) { // RL
-                for (int k = i+1; k < j; ++k)
-                    ans[k] = k-i == j-k ? '.' : k-i < j-k ? 'R' : 'L';
+            if (domino == 'L' && index > 0 && dominoes[index - 1] == '.') {
+                queue.add(new Pair(domino, index - 1));
+                dominoes[index -1] = 'L';
+            } else if (domino == 'R' && index + 1 < n && dominoes[index + 1] == '.') {
+                if (index + 2 < n && dominoes[index + 2] == 'L') {
+                    queue.poll();
+                } else {
+                    queue.add(new Pair('R', index + 1));
+                    dominoes[index + 1] = 'R';
+                }
             }
         }
 
-        return String.valueOf(ans);
+        return new String(dominoes);
     }
 }
