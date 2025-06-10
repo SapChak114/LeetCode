c1dc75afedcad616c095bf45101eaab2f95d2b22
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-06-11 00:44:25 +0530

    Time: 0 ms (100%), Space: 40.2 MB (77.73%) - LeetHub

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.java
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.java
@@ -1,18 +1,15 @@
 class Solution {
-
     public int findKthNumber(int n, int k) {
         int curr = 1;
         k--;
 
         while (k > 0) {
-            int step = countSteps(n, curr, curr + 1);
-            // If the steps are less than or equal to k, we skip this prefix's subtree
-            if (step <= k) {
-                // Move to the next prefix and decrease k by the number of steps we skip
+            int steps = countSteps(n, curr, curr + 1);
+
+            if (steps <= k) {
                 curr++;
-                k -= step;
+                k -= steps;
             } else {
-                // Move to the next level of the tree and decrement k by 1
                 curr *= 10;
                 k--;
             }
@@ -21,14 +18,15 @@ class Solution {
         return curr;
     }
 
-    // To count how many numbers exist between prefix1 and prefix2
-    private int countSteps(int n, long prefix1, long prefix2) {
+    int countSteps(int n, long prefix1, long prefix2) {
         int steps = 0;
+
         while (prefix1 <= n) {
             steps += Math.min(n + 1, prefix2) - prefix1;
             prefix1 *= 10;
             prefix2 *= 10;
         }
+
         return steps;
     }
 }
