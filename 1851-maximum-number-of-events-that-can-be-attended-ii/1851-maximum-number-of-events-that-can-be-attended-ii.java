acd20509005d37c666aa2d157ad488af58cc3611
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-07-10 00:41:13 +0530

    Time: 134 ms (21.97%), Space: 140 MB (11.21%) - LeetHub

--- 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.java
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.java
@@ -1,49 +1,42 @@
 class Solution {
-    private int[][] events;
-    private int n;
-    private int[][] dp;
+    int[][] dp;
     public int maxValue(int[][] events, int k) {
-        dp = new int[events.length+1][k+1];
-        for(int[] d : dp){
-            Arrays.fill(d,-1);
+        Arrays.sort(events, (a, b) -> a[0] - b[0]);
+        int n = events.length;
+
+        dp = new int[k + 1][n];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
         }
-        Arrays.sort(events,(a,b)->a[0]-b[0]);
-        this.events=events;
-        this.n=events.length;
-        return dfs(0,k);
+
+        return dfs(0, k, events);
     }
-    
-    int dfs(int index, int k){
-        if(index==n || k==0){
+
+    int dfs(int currIndex, int count, int[][] events) {
+        if (count == 0 || currIndex == events.length) {
             return 0;
         }
-        String key = index+"-"+k;
-        
-        if(dp[index][k]!=-1) return dp[index][k];
-        
-        int dont = dfs(index+1,k);
-        int value = events[index][2];
-        int end = events[index][1];
-        int idx = bs(events,end);
-        int take = value + dfs(idx,k-1);
-        
-        dp[index][k]=Math.max(dont,take);
-        
-        return Math.max(dont,take);
+
+        if (dp[count][currIndex] != -1) {
+            return dp[count][currIndex];
+        }
+
+        int nextIndex = bs(events, events[currIndex][1]);
+        dp[count][currIndex] = Math.max(dfs(currIndex + 1, count, events), events[currIndex][2] + dfs(nextIndex, count - 1, events));
+        return dp[count][currIndex];
     }
-    
-    private int bs(int[][] events, int targ){
-        int l = 0, r = events.length;
-        
-        while(l<r){
-            int mid = l + (r-l)/2;
-            if(targ>=events[mid][0]){
-                l = mid + 1;
-            } else{
-                r = mid;
+
+    public int bs(int[][] events, int target) {
+        int left = 0, right = events.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (events[mid][0] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
             }
         }
-        
-        return l;
+
+        return left;
     }
 }
