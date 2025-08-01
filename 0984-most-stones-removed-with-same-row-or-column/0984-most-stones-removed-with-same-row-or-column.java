1462ad0e382ba8297f330dddc0578e6f9333c10c
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-08-01 22:17:20 +0530

    Time: 35 ms (17.01%), Space: 44.9 MB (74.8%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.java
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.java
@@ -1,41 +1,57 @@
 class Solution {
+    List<Integer>[] adjList;
+    boolean[] vis;
+    int n;
     public int removeStones(int[][] stones) {
-        int n = stones.length;
-
-        List<Integer>[] adjList = new ArrayList[n];
+        n = stones.length;
+        adjList = new ArrayList[n];
         for (int i = 0; i<n; i++) {
             adjList[i] = new ArrayList<>();
         }
-
+        
         for (int i = 0; i<n; i++) {
-            for (int j = i+1; j<n; j++) {
-                if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+            for(int j=i+1;j<n;j++){
+                if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]){
                     adjList[i].add(j);
                     adjList[j].add(i);
                 }
             }
         }
-
-        int numOfConnectedComponents = 0;
-        boolean[] vis = new boolean[n];
-
+        
+        vis = new boolean[n];
+        int count = 0;
         for (int i = 0; i<n; i++) {
             if (!vis[i]) {
-                dfs(adjList, vis, i);
-                numOfConnectedComponents++;
+                dfs(i);
+                count++;
             }
         }
-
-        return n - numOfConnectedComponents;
+        
+        return n - count;
     }
-
-    void dfs(List<Integer>[] adjList, boolean[] vis, int stone) {
-        vis[stone] = true;
-
-        for (int neighbor : adjList[stone]) {
-            if (!vis[neighbor]) {
-                dfs(adjList, vis, neighbor);
+    
+    void dfs(int node) {
+        vis[node] = true;
+        for (int nei : adjList[node]) {
+            if (!vis[nei]) {
+                dfs(nei);
             }
         }
     }
 }
+/**
+[[0,0],
+[0,1],
+[1,0],[1,2],[2,1],[2,2]]
+
+0 0 0
+0 0 1
+0 0 0
+
+count = 3
+1 0 0
+1 0 0
+0 0 1
+
+count = 4
+ */
