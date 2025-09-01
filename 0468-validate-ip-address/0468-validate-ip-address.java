c4c03ad06de3969ffd5f8a043766352ab520719a
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-09-01 22:23:07 +0530

    Time: 3 ms (34.38%), Space: 41.7 MB (52.13%) - LeetHub

--- 0468-validate-ip-address/0468-validate-ip-address.java
+++ 0468-validate-ip-address/0468-validate-ip-address.java
@@ -1,22 +1,59 @@
-import java.util.regex.Pattern;
 class Solution {
-    public String validIPAddress(String IP) {
-
-        String v4Pattern = "(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])";
-        
-        String v6Pattern = "((([0-9a-fA-F]){1,4})\\:){7}([0-9a-fA-F]){1,4}";
-
-        Pattern v4 = Pattern.compile(v4Pattern);
-        Pattern v6 = Pattern.compile(v6Pattern);
-
-        if (v4.matcher(IP).matches()) {
+    public String validIPAddress(String queryIP) {
+        if (validIpv4(queryIP)) {
             return "IPv4";
-        }
-
-        if (v6.matcher(IP).matches()) {
+        } else if (validIpv6(queryIP)) {
             return "IPv6";
         }
 
         return "Neither";
+    }
+
+    boolean validIpv4(String q) {
+        String[] sq = q.split("\\.", -1);
+
+        if (sq.length != 4) {
+            return false;
+        }
+
+        for (String s : sq) {
+            for (char c : s.toCharArray()) {
+                if (!Character.isDigit(c)) {
+                    return false;
+                }
+            }
+
+            if (s.length() > 1 && s.charAt(0) == '0') {
+                return false;
+            }
+
+            if (s.isEmpty() || s.length() > 3 || Integer.parseInt(s) > 255) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    boolean validIpv6(String q) {
+        String[] sq = q.split("\\:", -1);
+        System.out.println(Arrays.toString(sq));
+        if (sq.length != 8) {
+            return false;
+        }
+
+        for (String s : sq) {
+            for (char c : s.toCharArray()) {
+                if (!Character.isDigit(c) && !((c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F'))) {
+                    return false;
+                }
+            }
+
+            if (s.isEmpty() || s.length() > 4) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
