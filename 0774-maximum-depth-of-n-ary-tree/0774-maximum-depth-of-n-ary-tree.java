7163eedb477b7ba9e905f84ef578dcc9f255ef2a
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-09-30 15:39:11 +0530

    Time: 0 ms (100%), Space: 44 MB (68.31%) - LeetHub

---
+++ 0774-maximum-depth-of-n-ary-tree/0774-maximum-depth-of-n-ary-tree.java
@@ -0,0 +1,37 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public int maxDepth(Node root) {
+        return find(root);
+    }
+
+    int find(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int max = 0;
+        for (Node node : root.children) {
+            max = Math.max(max, find(node));
+        }
+
+        return 1 + max;
+    }
+}
