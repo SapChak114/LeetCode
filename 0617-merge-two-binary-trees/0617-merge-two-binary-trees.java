8463adacac6ca8ba3279a1d442b131938bee9d64
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-10-05 14:08:05 +0530

    Time: 0 ms (100%), Space: 44.8 MB (81.35%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
@@ -15,6 +15,10 @@
  */
 class Solution {
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        return dfs(root1, root2);
+    }
+
+    TreeNode dfs(TreeNode root1, TreeNode root2) {
         if (root1 == null) {
             return root2;
         }
@@ -24,8 +28,8 @@ class Solution {
         }
 
         root1.val += root2.val;
-        root1.left = mergeTrees(root1.left, root2.left);
-        root1.right = mergeTrees(root1.right, root2.right);
+        root1.left = dfs(root1.left, root2.left);
+        root1.right = dfs(root1.right, root2.right);
         return root1;
     }
 }
