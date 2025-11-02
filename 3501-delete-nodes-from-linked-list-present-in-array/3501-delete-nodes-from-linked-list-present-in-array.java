1b651966bc31564443718beb2ce0858d3960a34a
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-11-02 20:18:35 +0530

    Time: 21 ms (46.94%), Space: 119.1 MB (5.6%) - LeetHub

--- 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.java
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.java
@@ -1,34 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-
+    
     public ListNode modifiedList(int[] nums, ListNode head) {
-        // Create a HashSet for efficient lookup of values in nums
-        Set<Integer> valuesToRemove = new HashSet<>();
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Set<Integer> set = new HashSet<>();
         for (int num : nums) {
-            valuesToRemove.add(num);
+            set.add(num);
         }
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode curr = head;
 
-        // Handle the case where the head node needs to be removed
-        while (head != null && valuesToRemove.contains(head.val)) {
-            head = head.next;
-        }
-
-        // If the list is empty after removing head nodes, return null
-        if (head == null) {
-            return null;
-        }
-
-        // Iterate through the list, removing nodes with values in the set
-        ListNode current = head;
-        while (current.next != null) {
-            if (valuesToRemove.contains(current.next.val)) {
-                // Skip the next node by updating the pointer
-                current.next = current.next.next;
+        while (curr != null) {
+            if (set.contains(curr.val)) {
+                prev.next = curr.next;
             } else {
-                // Move to the next node
-                current = current.next;
+                prev = prev.next;
             }
+
+            curr = curr.next;
         }
 
-        return head;
+        return dummy.next;
+
     }
 }
