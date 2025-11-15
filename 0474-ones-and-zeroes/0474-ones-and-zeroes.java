e8819fb68d1fa0f2d306277dbe9eee99131d6566
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-11-15 15:10:31 +0530

    Time: 931 ms (5%), Space: 276 MB (5%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.java
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.java
@@ -1,45 +1,42 @@
 class Solution {
-    // public int findMaxForm(String[] S, int M, int N) {
-    //     int[][] dp = new int[M+1][N+1];
-    //     for (String str : S) {
-    //         int zeros = 0, ones = 0;
-    //         for (char c : str.toCharArray())
-    //             if (c == '0') zeros++;
-    //             else ones++;
-    //         for (int i = M; i >= zeros; i--)
-    //             for (int j = N; j >= ones; j--)
-    //                 dp[i][j] = Math.max(dp[i][j], dp[i-zeros][j-ones] + 1);
-    //     }
-    //     return dp[M][N];
-    // }
-
+    Map<String, Integer> dp;
+    String[] strs;
+    int k;
     public int findMaxForm(String[] strs, int m, int n) {
-        // Using a HashMap for memoization
-        Map<String, Integer> dp = new HashMap<>();
-        return dfs(strs, 0, m, n, dp);
+        this.dp = new HashMap<>();
+        this.strs = strs;
+        this.k = strs.length;
+        return dfs(0, m, n);
     }
 
-    private int dfs(String[] strs, int i, int m, int n, Map<String, Integer> dp) {
-        if (i == strs.length) {
+    int dfs(int i, int m, int n) {
+        if (i == k) {
             return 0;
         }
-        String key = i + "," + m + "," + n;
+
+        String key = i + "-" + m + "-" + n;
+
         if (dp.containsKey(key)) {
             return dp.get(key);
         }
-        int mCount = 0, nCount = 0;
-        for (char c : strs[i].toCharArray()) {
-            if (c == '0') mCount++;
-            if (c == '1') nCount++;
-        }
-        // Option 1: Skip the current string
-        int result = dfs(strs, i + 1, m, n, dp);
+        
+        int res = dfs(i + 1, m, n);
 
-        // Option 2: Include the current string if possible
-        if (mCount <= m && nCount <= n) {
-            result = Math.max(result, 1 + dfs(strs, i + 1, m - mCount, n - nCount, dp));
+        int mCount = 0, nCount = 0;
+        for (int l = 0; l<strs[i].length(); l++) {
+            if (strs[i].charAt(l) == '0') {
+                mCount++;
+            } else {
+                nCount++;
+            }
         }
-        dp.put(key, result);
-        return result;
+
+        if (mCount <= m && nCount <= n) {
+            res = Math.max(res, 1 + dfs(i+1, m - mCount, n - nCount));
+        }
+        
+        dp.put(key, res);
+
+        return res;
     }
 }
