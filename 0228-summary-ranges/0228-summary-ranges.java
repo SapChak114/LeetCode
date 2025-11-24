0de74c1dcc80103fdd39f06e0cc5d84c87252acd
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-11-24 14:59:15 +0530

    Time: 3 ms (83.12%), Space: 43.2 MB (5.77%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -1,25 +1,33 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> ans = new ArrayList<>();
-        if (nums == null || nums.length == 0) {
+        if (nums.length == 0) {
             return ans;
         }
 
-        int start = 0; // Start index of the current range
-        for (int i = 1; i <= nums.length; i++) {
-            // Check if the current number breaks the sequence or we are at the end
-            if (i == nums.length || nums[i] != nums[i - 1] + 1) {
-                if (start == i - 1) {
-                    // Single number range
-                    ans.add(String.valueOf(nums[start]));
+        int prev = 0;
+        for (int i = 1; i<=nums.length; i++) {
+            if (i == nums.length || nums[i-1] + 1 != nums[i]) {
+                //System.out.println(nums[i]);
+                if (prev == i-1) {
+                    ans.add(""+nums[prev]);
                 } else {
-                    // Multi-number range
-                    ans.add(nums[start] + "->" + nums[i - 1]);
+                    ans.add(nums[prev]+"->"+nums[i-1]);
                 }
-                // Update start for the next range
-                start = i;
+                prev = i;
             }
         }
         return ans;
     }
 }
+
+/*
+prev = 0
+0 + 1 = 1 == 1
+1 + 1 == 2
+2 + 1 != 4
+0->2
+
+prev = 4
+4 
+*/
