156ddea5e4926faadcb57cd0fb8cb2bd69c9f6f4
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-12-21 22:05:36 +0530

    Time: 0 ms (100%), Space: 43.7 MB (35.62%) - LeetHub

--- 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.java
+++ 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.java
@@ -1,34 +1,35 @@
+import java.util.List;
+import java.util.Arrays;
+
 class Solution {
-    public int minDeletionSize(String[] strs) {
-        int n = strs.length;
-        int m = strs[0].length();
-        int count = 0;
+    public int minDeletionSize(String[] words) {
+        int n = words.length;
+        boolean[] sortedPairs = new boolean[n - 1];
+        int deletions = 0;
 
-        String[] newStr = new String[n];
-
-        for (int j = 0; j<m; j++) {
-            for (int i = 0; i<n; i++) {
-                newStr[i] += strs[i].charAt(j);
-            }
-
-            if (notLexo(newStr)) {
-                for (int i = 0; i<n; i++) {
-                    newStr[i] = new StringBuilder(newStr[i]).deleteCharAt(newStr[i].length()-1).toString();
+        for (int col = 0; col < words[0].length(); col++) {
+            // Check if any unresolved pair violates sorting order
+            boolean shouldDelete = false;
+            for (int i = 0; i < n - 1; i++) {
+                if (!sortedPairs[i] && words[i].charAt(col) > words[i + 1].charAt(col)) {
+                    shouldDelete = true;
+                    break;
                 }
-                count++;
+            }
+            
+            if (shouldDelete) {
+                deletions++;
+                continue;
+            }
+
+            // Mark pairs as sorted if this column confirms strict increase
+            for (int i = 0; i < n - 1; i++) {
+                if (!sortedPairs[i] && words[i].charAt(col) < words[i + 1].charAt(col)) {
+                    sortedPairs[i] = true;
+                }
             }
         }
-
-        return count;
-    }
-
-    boolean notLexo(String[] newStr) {
-        for (int i = 1; i<newStr.length; i++) {
-            if (newStr[i].compareTo(newStr[i-1]) < 0) {
-                return true;
-            }
-        }
-
-        return false;
+        
+        return deletions;
     }
 }
