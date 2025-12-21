33d7f3510f008f86cdee1024d368fdfefcf515d6
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-12-21 21:58:56 +0530

    Time: 16 ms (8.22%), Space: 47 MB (6.85%) - LeetHub

--- 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.java
+++ 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.java
@@ -1,36 +1,34 @@
 class Solution {
-    public int minDeletionSize(String[] A) {
-        int N = A.length;
-        int W = A[0].length();
-        int ans = 0;
+    public int minDeletionSize(String[] strs) {
+        int n = strs.length;
+        int m = strs[0].length();
+        int count = 0;
 
-        // cur : all rows we have written
-        // For example, with A = ["abc","def","ghi"] we might have
-        // cur = ["ab", "de", "gh"].
-        String[] cur = new String[N];
-        for (int j = 0; j < W; ++j) {
-            // cur2 : What we potentially can write, including the
-            //        newest column col = [A[i][j] for i]
-            // Eg. if cur = ["ab","de","gh"] and col = ("c","f","i"),
-            // then cur2 = ["abc","def","ghi"].
-            String[] cur2 = Arrays.copyOf(cur, N);
-            for (int i = 0; i < N; ++i)
-                cur2[i] += A[i].charAt(j);
+        String[] newStr = new String[n];
 
-            if (isSorted(cur2))
-                cur = cur2;
-            else
-                ans++;
+        for (int j = 0; j<m; j++) {
+            for (int i = 0; i<n; i++) {
+                newStr[i] += strs[i].charAt(j);
+            }
+
+            if (notLexo(newStr)) {
+                for (int i = 0; i<n; i++) {
+                    newStr[i] = new StringBuilder(newStr[i]).deleteCharAt(newStr[i].length()-1).toString();
+                }
+                count++;
+            }
         }
 
-        return ans;
+        return count;
     }
 
-    public boolean isSorted(String[] A) {
-        for (int i = 0; i < A.length - 1; ++i)
-            if (A[i].compareTo(A[i+1]) > 0)
-                return false;
+    boolean notLexo(String[] newStr) {
+        for (int i = 1; i<newStr.length; i++) {
+            if (newStr[i].compareTo(newStr[i-1]) < 0) {
+                return true;
+            }
+        }
 
-        return true;
+        return false;
     }
 }
