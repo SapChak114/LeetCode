36e252e7a3d657a8347d413625ce03621cff2690
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2025-12-31 09:29:54 +0530

    Time: 12 ms (99.49%), Space: 79 MB (65.48%) - LeetHub

--- 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.java
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.java
@@ -1,61 +1,68 @@
 class Solution {
-    int row;
-    int col;
-    int[][] cells;
-    int[] dirX = {0,1,0,-1};
-    int[] dirY = {1,0,-1,0};
     public int latestDayToCross(int row, int col, int[][] cells) {
-        this.cells = cells;
-        this.row=row;
-        this.col=col;
-        
-        int left = 1, right = cells.length;
-        int mid;
-        int ans = 0;
-        while(left<=right){
-            mid = left + (right-left)/2;
-            //System.out.println(mid);
-            if(canCross(mid)){
-                ans = mid;
-                left = mid + 1;
-            } else{
-                right = mid - 1;
-            }
-        }
-        
-        return ans;
-    }
-    
-    boolean canCross(int day){
+        DSU dsu = new DSU(row * col + 2);
         int[][] grid = new int[row][col];
-        
-        for(int i = 0; i<day; i++){
-            grid[cells[i][0]-1][cells[i][1]-1]=1;
-        }
-        
-        for(int i = 0; i<col; i++){
-            if(grid[0][i]==0 && dfs(grid,0,i)){
-                return true;
+        int[][] dirs = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 }, { 1, 1 }, { 1, -1 }, { -1, 1 }, { -1, -1 } };
+
+        for (int i = 0; i < row * col; i++) {
+            int r = cells[i][0] - 1;
+            int c = cells[i][1] - 1;
+            grid[r][c] = 1;
+
+            int id1 = r * col + c + 1;
+
+            for (int[] d : dirs) {
+                int nr = r + d[0];
+                int nc = c + d[1];
+                if (nr >= 0 && nr < row && nc >= 0 && nc < col && grid[nr][nc] == 1) {
+                    dsu.union(id1, nr * col + nc + 1);
+                }
             }
+
+            if (c == 0)
+                dsu.union(0, id1);
+            if (c == col - 1)
+                dsu.union(row * col + 1, id1);
+
+            if (dsu.find(0) == dsu.find(row * col + 1))
+                return i;
         }
-        
-        return false;
+
+        return -1;
     }
-    
-    boolean dfs(int[][] grid, int r, int c){
-        if(r==row-1){
-            return true;
+}
+
+class DSU {
+    int[] root, size;
+
+    public DSU(int n) {
+        root = new int[n];
+        size = new int[n];
+        for (int i = 0; i < n; i++)
+            root[i] = i;
+        Arrays.fill(size, 1);
+    }
+
+    public int find(int x) {
+        if (root[x] != x)
+            root[x] = find(root[x]);
+        return root[x];
+    }
+
+    public void union(int x, int y) {
+        int rx = find(x);
+        int ry = find(y);
+
+        if (rx == ry)
+            return;
+
+        if (size[rx] > size[ry]) {
+            int tmp = rx;
+            rx = ry;
+            ry = tmp;
         }
-        
-        grid[r][c]=-1;
-        
-        for(int i = 0; i<dirX.length; i++){
-            int x = r + dirX[i];
-            int y = c + dirY[i];
-            if(x<0 || x>=grid.length || y<0 || y>=grid[0].length || grid[x][y]!=0) continue;
-            if(dfs(grid,x,y)) return true;
-        }
-        
-        return false;
+
+        root[rx] = ry;
+        size[ry] += size[rx];
     }
 }
