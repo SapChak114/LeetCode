5614a7e33ad053bdc127daf5dda9f5ad87e8e62d
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-01-01 14:11:27 +0530

    Time: 0 ms (100%), Space: 43.6 MB (7.44%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,9 +1,7 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length, carr = 1;
-        reverse(digits);
-        //System.out.println(Arrays.toString(digits));
-        for (int i = 0; i<n; i++) {
+        for (int i = n-1; i>=0; i--) {
             if (digits[i] == 9) {
                 digits[i] = 0;
             } else {
@@ -16,7 +14,7 @@ class Solution {
         int[] ans = new int[carr == 0 ? n : n + 1];
 
         for (int i = carr; i<n; i++) {
-            ans[i] = digits[n - i - 1];
+            ans[i] = digits[i];
         }
 
         if (carr == 1) {
@@ -24,15 +22,5 @@ class Solution {
         }
 
         return ans;
-    }
-
-    void reverse(int[] digits) {
-        int n = digits.length, i = 0;
-        while (i != (n / 2)) {
-            int temp = digits[i];
-            digits[i] = digits[n - i - 1];
-            digits[n - i - 1] = temp;
-            i++;
-        }
     }
 }
