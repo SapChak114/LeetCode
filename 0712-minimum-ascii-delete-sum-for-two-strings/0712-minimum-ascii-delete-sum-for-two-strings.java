1cab7198d2835a58f9d1416c6635d32bb267a1cd
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-01-10 09:21:27 +0530

    Time: 34 ms (7.6%), Space: 50.6 MB (5.07%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -1,42 +1,43 @@
 class Solution {
-    String s1, s2;
     int n, m;
+    String s1, s2;
     Integer[][] dp;
     public int minimumDeleteSum(String s1, String s2) {
-        this.s1=s1;
-        this.s2=s2;
-        this.n=s1.length();
-        this.m=s2.length();
-        dp = new Integer[n+1][m+1];
-        return rec(0,0);
+        this.s1 = s1;
+        this.s2 = s2;
+        this.n = s1.length();
+        this.m = s2.length();
+        this.dp = new Integer[n][m];
+
+        return rec(0, 0);
     }
-    
-    int rec(int i, int j){
-        if(i>=n && j>=m){
+
+    int rec(int i, int j) {
+        if (i >= n && j >= m) {
             return 0;
         }
-        
-        if(i>=n){
+
+        if (i >= n) {
             int c = s2.charAt(j);
-            return c + rec(i,j+1);
+            return c + rec(i, j+1);
         }
-        
-        if(j>=m){
+
+        if (j >= m) {
             int c = s1.charAt(i);
-            return c + rec(i+1,j);
+            return c + rec(i+1, j);
         }
-        
-        if(dp[i][j]!=null) return dp[i][j];
-        
-        if(s1.charAt(i)==s2.charAt(j)){
-            return rec(i+1,j+1);
+
+        if (dp[i][j] != null) {
+            return dp[i][j];
         }
-        
-        
-        
-        int del1 = (int)s1.charAt(i) + rec(i+1,j);
-        int del2 = (int)s2.charAt(j) + rec(i,j+1);
-        
-        return dp[i][j]=Math.min(del1,del2);
+
+        if (s1.charAt(i) == s2.charAt(j)) {
+            return rec(i+1, j+1);
+        }
+
+        int a = ((int) s1.charAt(i)) + rec(i+1, j);
+        int b = ((int) s2.charAt(j)) + rec(i, j+1);
+
+        return dp[i][j] = Math.min(a, b);
     }
 }
