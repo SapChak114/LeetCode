3cc3acb67ff84c245a3690b072c4bef6c3ebf96d
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-01-11 22:51:32 +0530

    Time: 25 ms (21.04%), Space: 47.9 MB (93.95%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,58 +1,64 @@
 class Solution {
-    public int maximalRectangle(char[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int[] mat = new int[m];
-        
+    public int maximalRectangle(char[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+
+        int[] ma = new int[m];
         int max = Integer.MIN_VALUE;
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(matrix[i][j]=='1'){
-                    mat[j] += 1;
-                } else{
-                    mat[j] = 0;
+        for (int i = 0; i<n; i++) {
+            for (int j = 0; j<m; j++) {
+                if (mat[i][j] == '1') {
+                    ma[j]++;
+                } else {
+                    ma[j] = 0;
                 }
             }
-            max = Math.max(max,lra(mat));
+            max = Math.max(max, lra(ma));
         }
-        
+
         return max;
     }
-    
-     private int lra(int[] A) {
+
+    int lra(int[] A) {
         int[] leftSmall = leftSmall(A);
         int[] rightSmall = rightSmall(A);
 
         int max = Integer.MIN_VALUE;
-        for(int i = 0; i<A.length; i++){
+        for (int i = 0; i<A.length; i++) {
             int val = rightSmall[i] - leftSmall[i] - 1;
-            max = Math.max(max,A[i]*val);
+            max = Math.max(max, A[i] * val);
         }
 
         return max;
     }
 
-    int[] leftSmall(int[] A){
+    int[] leftSmall(int[] A) {
+        int n = A.length;
+        int[] ans = new int[n];
         Stack<Integer> st = new Stack<>();
-        int[] ans = new int[A.length];
-        for(int i = 0; i<A.length; i++){
-            while(!st.isEmpty() && A[st.peek()]>=A[i]) st.pop();
-            if(st.isEmpty()) ans[i] = -1;
-            else ans[i] = st.peek();
-            st.add(i);
+        for (int i = 0; i<n; i++) {
+            while (!st.isEmpty() && A[st.peek()] >= A[i]) {
+                st.pop();
+            }
+            ans[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
         }
+
         return ans;
     }
 
-    int[] rightSmall(int[] A){
+    int[] rightSmall(int[] A) {
+        int n = A.length;
+        int[] ans = new int[n];
         Stack<Integer> st = new Stack<>();
-        int[] ans = new int[A.length];
-        for(int i = A.length-1; i>=0; i--){
-            while(!st.isEmpty() && A[st.peek()]>=A[i]) st.pop();
-            if(st.isEmpty()) ans[i] = A.length;
-            else ans[i] = st.peek();
-            st.add(i);
+        for (int i = n-1; i>=0; i--) {
+            while (!st.isEmpty() && A[st.peek()] >= A[i]) {
+                st.pop();
+            }
+            ans[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
         }
+
         return ans;
     }
 }
