e57a9cbd8899f577b1fe128a57bf6b33966ec1ad
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-05-20 20:55:17 +0530

    Time: 770 ms (10.82%), Space: 55.7 MB (5.28%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -2,8 +2,8 @@ class Solution {
     public List<String> findWords(char[][] board, String[] words) {
         Trie trie = new Trie(board);
 
-        for(String word : words){
-            trie.insert(word);
+        for (String w : words) {
+            trie.insert(w);
         }
 
         return trie.findAll();
@@ -11,76 +11,71 @@ class Solution {
 }
 
 class TrieNode{
-    Map<Character,TrieNode> hm = new HashMap<>();
+    Map<Character, TrieNode> hm = new HashMap<>();
     boolean isEnd;
 }
 
 class Trie{
-    TrieNode root;
-    char[][] b;
+    TrieNode trie;
+    char[][] board;
     List<String> ans;
-    int[] dirX = {0,1,0,-1};
-    int[] dirY = {1,0,-1,0};
+    int[] dirX = {0, 1, 0, -1};
+    int[] dirY = {1, 0, -1, 0};
 
-    public Trie(char[][] b){
-        this.b = b;
-        this.root = new TrieNode();
+    public Trie(char[][] board) {
+        this.board = board;
+        this.trie = new TrieNode();
     }
 
-    public void insert(String word){
-        TrieNode curr = root;
-        for(char c : word.toCharArray()){
-            if(!curr.hm.containsKey(c)){
-                curr.hm.put(c, new TrieNode());
+    public void insert(String word) {
+        char[] chs = word.toCharArray();
+        TrieNode node = this.trie;
+        for (char c : chs) {
+            if (!node.hm.containsKey(c)) {
+                node.hm.put(c, new TrieNode());
             }
-            curr = curr.hm.get(c);
+            node = node.hm.get(c);
+
         }
-        curr.isEnd = true;
+        node.isEnd = true;
     }
 
-    public List<String> findAll(){
-        TrieNode curr = root;
-        int n = b.length, m = b[0].length;
+    public List<String> findAll() {
         this.ans = new ArrayList<>();
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                char c = b[i][j];
-                if(curr.hm.containsKey(c)){
-                    dfs(curr.hm.get(c),i,j,b,""+c);
+        for (int i = 0; i<board.length; i++) {
+            for (int j = 0; j<board[i].length; j++) {
+                char c = board[i][j];
+                if (trie.hm.containsKey(c)) {
+                    dfs(trie.hm.get(c), board, i, j, ""+c);
                 }
             }
         }
         return this.ans;
     }
 
-    void dfs(TrieNode node, int x, int y, char[][] b, String word){
-        if(node.isEnd && !word.isEmpty()){
-            node.isEnd = false;
+    void dfs(TrieNode trie, char[][] board, int x, int y, String word) {
+        if (trie.isEnd && !word.isEmpty()) {
             ans.add(word);
+            trie.isEnd = false;
         }
 
-        char c = b[x][y];
-        b[x][y] = '*';
+        char c = board[x][y];
+        board[x][y] = '*';
 
-        for(int i = 0; i<dirX.length; i++){
-            int newX = dirX[i] + x;
-            int newY = dirY[i] + y;
-            if(check(newX, newY)){
-                char ch = b[newX][newY];
-                if(node.hm.containsKey(ch)){
-                    dfs(node.hm.get(ch), newX, newY, b, word+b[newX][newY]);
-                }
+        for (int i = 0; i<dirX.length; i++) {
+            int newX = x + dirX[i];
+            int newY = y + dirY[i];
+
+            if (newX < 0 || newX >= board.length || newY < 0 || newY >= board[0].length) {
+                continue;
+            }
+
+            char ch = board[newX][newY];
+            if (trie.hm.containsKey(ch)) {
+                dfs(trie.hm.get(ch), board, newX, newY, word + ch);
             }
         }
 
-        b[x][y] = c;
-    }
-
-    boolean check(int x, int y){
-        int n = b.length, m = b[0].length;
-        if(x<0 || x>=n || y<0 || y>=m){
-            return false;
-        }
-        return true;
+        board[x][y] = c;
     }
 }
