3a9d6a8927a845596dfd088b84a81157b4a4e72d
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-05-25 23:02:21 +0530

    Time: 5 ms (69.69%), Space: 46.9 MB (87.5%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,27 +1,28 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int n = word1.length(), m = word2.length();
+        int n = word1.length();
+        int m = word2.length();
 
-        int[][] cache = new int[n+1][m+1];
+        int[][] dp = new int[n+1][m+1];
 
-        for (int j = 0; j<=m; j++) {
-            cache[n][j] = m - j;
+        for (int i = n; i>=0; i--) {
+            dp[i][m] = n - i;
         }
 
-        for (int i = 0; i<=n; i++) {
-            cache[i][m] = n-i;
+        for (int j = m; j>=0; j--) {
+            dp[n][j] =  m - j;
         }
 
         for (int i = n-1; i>=0; i--) {
             for (int j = m-1; j>=0; j--) {
                 if (word1.charAt(i) == word2.charAt(j)) {
-                    cache[i][j] = cache[i+1][j+1];
+                    dp[i][j] = dp[i+1][j+1];
                 } else {
-                    cache[i][j] = 1 + Math.min(cache[i][j+1], Math.min(cache[i+1][j], cache[i+1][j+1]));
+                    dp[i][j] = 1 + Math.min(dp[i+1][j], Math.min(dp[i][j+1], dp[i+1][j+1]));
                 }
             }
         }
 
-        return cache[0][0];
+        return dp[0][0];
     }
 }
