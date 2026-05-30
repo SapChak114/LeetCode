6d624f73f4889d74973dee6ef36820ddcc9623e5
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-05-30 13:35:34 +0530

    Time: 3 ms (26.28%), Space: 46.6 MB (50.4%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,35 +1,35 @@
 class Solution {
     int[] dirX = {0, 1, 0, -1};
     int[] dirY = {1, 0, -1, 0};
-    int max, count;
     public int maxAreaOfIsland(int[][] grid) {
         int n = grid.length, m = grid[0].length;
         boolean[][] vis = new boolean[n][m];
-        this.max = Integer.MIN_VALUE;
+        int max = Integer.MIN_VALUE;
 
         for (int i = 0; i<n; i++) {
             for (int j = 0; j<m; j++) {
                 if (!vis[i][j] && grid[i][j] == 1) {
-                    this.count = 0;
-                    dfs(grid, vis, i, j);
-                    this.max = Math.max(this.max, this.count);
+                    max = Math.max(max, dfs(grid, vis, i, j));
                 }
             }
         }
 
-        return this.max == Integer.MIN_VALUE ? 0 : this.max;
+        return max == Integer.MIN_VALUE ? 0 : max;
     }
 
-    void dfs(int[][] grid, boolean[][] vis, int i, int j) {
+    int dfs(int[][] grid, boolean[][] vis, int i, int j) {
         vis[i][j] = true;
-        this.count++;
+        
+        int size = 1;
         for (int a = 0; a<dirX.length; a++) {
             int newX = i + dirX[a];
             int newY = j + dirY[a];
 
             if (newX >= 0 && newX < grid.length && newY >= 0 && newY < grid[0].length && grid[newX][newY] == 1 && !vis[newX][newY]) {
-                dfs(grid, vis, newX, newY);
+                size += dfs(grid, vis, newX, newY);
             }
         }
+
+        return size;
     }
 }
