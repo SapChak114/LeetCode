7bffdb5223bbe25682b511f41716b68a149004ca
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-05-31 15:23:35 +0530

    Time: 0 ms (100%), Space: 42.3 MB (16.46%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,20 +2,22 @@ class Solution {
     Integer[] dp;
     public int climbStairs(int n) {
         this.dp = new Integer[n+1];
-        return climb(n);
+        return dfs(n);
     }
 
-    int climb(int n) {
+    int dfs(int n) {
         if (n == 0) {
             return 1;
         }
+
         if (n < 0) {
             return 0;
         }
+
         if (dp[n] != null) {
             return dp[n];
         }
 
-        return dp[n] = climb(n-1) + climb(n-2);
+        return dp[n] = dfs(n-1) + dfs(n-2);
     }
 }
