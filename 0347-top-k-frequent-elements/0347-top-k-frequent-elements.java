ee82736fba3324ceea3d3020feb9046e6bf9983d
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-05-31 18:15:30 +0530

    Time: 11 ms (96.38%), Space: 47.7 MB (31.42%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,33 +1,40 @@
-class Pair{
-    int num;
-    int freq;
-    
-    public Pair(int num, int freq){
-        this.num=num;
-        this.freq=freq;
-    }
-}
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer,Integer> map = new HashMap<>();
-        
-        for(int i = 0; i<nums.length; i++){
-            if(map.containsKey(nums[i])){
-               map.put(nums[i],map.get(nums[i])+1); 
-            } else{
-                map.put(nums[i],1);
+        int n = nums.length;
+        Map<Integer, Integer> freq = new HashMap<>();
+
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0)+1);
+        }
+
+        List<Integer>[] bucket = new ArrayList[n+1];
+
+        for (Map.Entry<Integer, Integer> e : freq.entrySet()) {
+            int key = e.getKey();
+            int val = e.getValue();
+
+            if (bucket[val] == null) {
+                bucket[val] = new ArrayList<>();
+            }
+
+            bucket[val].add(key);
+        }
+
+        int[] ans = new int[k];
+        int idx = 0;
+        for (int i = bucket.length-1; i>=0 && idx<k; i--) {
+            
+            if (bucket[i] != null) {
+                for (int num : bucket[i]) {
+                    ans[idx++] = num;
+
+                    if (idx == k) {
+                        break;
+                    }
+                }
             }
         }
-        PriorityQueue<Pair> pq = new PriorityQueue<>((o1,o2)->o2.freq-o1.freq);
-        for(Integer e : map.keySet()){
-            pq.add(new Pair(e,map.get(e)));
-        }
-        int[] ans = new int[k];
-        int x = 0;
-        while(k-- > 0){
-            ans[x++]=pq.poll().num;
-        }
-        
+
         return ans;
     }
 }
