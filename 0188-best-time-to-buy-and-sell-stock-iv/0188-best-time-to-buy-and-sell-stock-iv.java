9d352a0e08cf0a93097a812a1f3d332013324f19
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-05-31 18:41:27 +0530

    Time: 9 ms (20.88%), Space: 51.7 MB (5.12%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,32 +1,33 @@
 class Solution {
-    int k, n;
+    int n, k;
     int[] prices;
     Integer[][][] dp;
     public int maxProfit(int k, int[] prices) {
-        this.prices = prices;
-        this.k = k;
         this.n = prices.length;
-        this.dp = new Integer[prices.length][2][k+1];
-        return rec(0, 1, 0);
+        this.k = k;
+        this.prices = prices;
+        this.dp = new Integer[n][2][k];
+
+        return dfs(0, 1, 0);
     }
 
-    int rec(int i, int buy, int count) {
-        if (prices.length == i || count == k) {
+    int dfs(int idx, int buy, int count) {
+        if (idx == n || count == k) {
             return 0;
         }
 
-        if (dp[i][buy][count] != null) {
-            return dp[i][buy][count];
+        if (dp[idx][buy][count] != null) {
+            return dp[idx][buy][count];
         }
 
         if (buy == 1) {
-            int take = -prices[i] + rec(i+1, 1-buy, count);
-            int dont = rec(i+1, buy, count);
-            return dp[i][buy][count] = Math.max(take, dont);
+            int take = -prices[idx] + dfs(idx + 1, 1-buy, count);
+            int dont = dfs(idx + 1, buy, count);
+            return dp[idx][buy][count] = Math.max(take, dont);
         } else {
-            int take = prices[i] + rec(i+1, 1-buy, count + 1);
-            int dont = rec(i+1, buy, count);
-            return dp[i][buy][count] = Math.max(take, dont);
+            int take = prices[idx] + dfs(idx + 1, 1-buy, count+1);
+            int dont = dfs(idx + 1, buy, count);
+            return dp[idx][buy][count] = Math.max(take, dont);
         }
     }
 }
