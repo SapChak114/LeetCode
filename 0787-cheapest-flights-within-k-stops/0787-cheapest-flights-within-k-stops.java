038092731d58b7fe32951dd6dd43e01a0e6c4cb1
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-05-31 23:59:58 +0530

    Time: 6 ms (84.9%), Space: 46.7 MB (38.64%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,6 +1,7 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         List<int[]>[] adjList = new ArrayList[n];
+
         for (int i = 0; i<n; i++) {
             adjList[i] = new ArrayList<>();
         }
@@ -8,34 +9,35 @@ class Solution {
         for (int i = 0; i<flights.length; i++) {
             int u = flights[i][0];
             int v = flights[i][1];
-            int p = flights[i][2];
+            int w = flights[i][2];
 
-            adjList[u].add(new int[]{v, p});
+            adjList[u].add(new int[]{v, w});
         }
 
         Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{src, 0, 0});
+        
         int[] dist = new int[n];
         Arrays.fill(dist, Integer.MAX_VALUE);
-        q.add(new int[]{0, src, 0});
         dist[src] = 0;
-
+        
         while (!q.isEmpty()) {
-            int[] pair = q.poll();
-            int stops = pair[0];
-            int node = pair[1];
-            int cost = pair[2];
+            int[] vals = q.poll();
+            int node = vals[0];
+            int weight = vals[1];
+            int stops = vals[2];
 
             if (stops > k) {
                 continue;
             }
 
-            for (int[] newPair : adjList[node]) {
-                int newNode = newPair[0];
-                int newCost = newPair[1];
-
-                if (cost + newCost < dist[newNode] && stops <= k) {
-                    dist[newNode] = cost + newCost;
-                    q.add(new int[]{stops + 1, newNode, dist[newNode]});
+            for (int[] nei : adjList[node]) {
+                int newNode = nei[0];
+                int newWeight = nei[1];
+                
+                if (dist[newNode] > weight + newWeight && stops <= k) {
+                    dist[newNode] = weight + newWeight;
+                    q.add(new int[]{newNode, dist[newNode], stops+1});
                 }
             }
         }
