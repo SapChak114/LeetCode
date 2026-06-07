49b93bab179d82fd3a2803dc1b5dd85628252eae
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-06-07 16:37:24 +0530

    Time: 17 ms (41.76%), Space: 48 MB (21.49%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,48 +1,43 @@
-class Pair{
-    int x;
-    int y;
-    int dist;
-    
-    public Pair(int x, int y, int dist){
-        this.x=x;
-        this.y=y;
-        this.dist=dist;
-    }
-}
 class Solution {
-    int[] dirX = {0,1,0,-1,1,-1,-1,1};
-    int[] dirY = {1,0,-1,0,1,1,-1,-1};
+    int[] dirX = {0, 1, 1, 1, 0, -1, -1, -1};
+    int[] dirY = {1, 1, 0, -1, -1, -1, 0, 1};
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        
-        if(grid[0][0]==1 || grid[n-1][m-1]==1){
+        if (grid[0][0] == 1) {
             return -1;
         }
-        
-        Queue<Pair> q = new LinkedList<>();
-        
-        q.add(new Pair(0,0,1));
-        boolean[][] vis = new boolean[m][n];
-        
-        vis[0][0]=true;
-        
-        while(!q.isEmpty()){
-            Pair p = q.poll();
-            
-            if(p.x==n-1 && p.y==m-1){
-                return p.dist;
+        int n = grid.length;
+        int m = grid[0].length;
+
+        boolean vis[][] = new boolean[n][m];
+
+        Queue<int[]> q = new LinkedList<>();
+
+        q.add(new int[]{0, 0, 1});
+
+        while (!q.isEmpty()) {
+            int[] vals = q.poll();
+            int x = vals[0];
+            int y = vals[1];
+            int t = vals[2];
+
+            if (x == n-1 && y == m-1) {
+                return t;
             }
-            
-            for(int i = 0; i<dirX.length; i++){
-                int newX = p.x + dirX[i];
-                int newY = p.y + dirY[i];
-                if(newX<0 || newX>n-1 || newY<0 || newY>m-1 || grid[newX][newY]==1 || vis[newX][newY]) continue;
-                q.add(new Pair(newX,newY,p.dist+1));
-                vis[newX][newY]=true;
+
+            for (int i = 0; i<dirX.length; i++) {
+                
+                int newX = x + dirX[i];
+                int newY = y + dirY[i];
+
+                if (newX < 0 || newX >= n || newY < 0 || newY >= m || grid[newX][newY] == 1 || vis[newX][newY]) {
+                    continue;
+                }
+
+                vis[newX][newY] = true;
+                q.add(new int[]{newX, newY, t+1});
             }
         }
-        
+
         return -1;
     }
 }
