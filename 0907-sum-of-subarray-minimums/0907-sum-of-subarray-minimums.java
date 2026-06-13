7b10244277b9d35dbb7b7e5df19b179f28247b34
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-06-13 15:51:46 +0530

    Time: 438 ms (5.18%), Space: 79.3 MB (5.85%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -0,0 +1,76 @@
+class Solution {
+    int mod = (int)1e9 + 7;
+    public int sumSubarrayMins(int[] arr) {
+        int n = arr.length;
+
+        int[] arrL = minSubLeft(arr);
+        int[] arrR = minSubRight(arr);
+
+        System.out.println(Arrays.toString(arrL));
+        System.out.println(Arrays.toString(arrR));
+        
+        int[] res = new int[n];
+        long sum = 1;
+
+        for (int i = 0; i<n; i++) {
+            long left = i - arrL[i];
+            long right = arrR[i] - i;
+
+            long totalWays = left * right;
+
+            long totalSum = totalWays * arr[i];
+            sum = (sum + totalSum) % mod;
+        }
+
+        return (int)sum-1;
+    }
+
+    int[] minSubLeft(int[] arr) {
+        int n = arr.length;
+
+        int[] res = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i<n; i++) {
+            
+            if (st.isEmpty()) {
+                res[i] = -1;
+            } else {
+                while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+                    st.pop();
+                }
+
+                res[i] = st.isEmpty() ? -1 : st.peek();
+            }
+
+            st.push(i);
+        }
+        
+        return res;
+    }
+
+
+    int[] minSubRight(int[] arr) {
+        int n = arr.length;
+
+        int[] res = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = n-1; i>=0; i--) {
+            
+            if (st.isEmpty()) {
+                res[i] = n;
+            } else {
+                while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
+                    st.pop();
+                }
+
+                res[i] = st.isEmpty() ? n : st.peek();
+            }
+
+            st.push(i);
+        }
+        
+        return res;
+    }
+}
