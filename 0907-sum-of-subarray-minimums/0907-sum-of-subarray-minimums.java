00e721a7d89f869611bf9540e5b8f0aeea282b04
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-06-13 15:54:05 +0530

    Time: 410 ms (5.18%), Space: 79.3 MB (5.85%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -32,15 +32,15 @@ class Solution {
         Stack<Integer> st = new Stack<>();
 
         for (int i = 0; i<n; i++) {
+
+            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+                st.pop();
+            }
             
             if (st.isEmpty()) {
                 res[i] = -1;
             } else {
-                while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
-                    st.pop();
-                }
-
-                res[i] = st.isEmpty() ? -1 : st.peek();
+                res[i] = st.peek();
             }
 
             st.push(i);
@@ -57,15 +57,14 @@ class Solution {
         Stack<Integer> st = new Stack<>();
 
         for (int i = n-1; i>=0; i--) {
-            
+            while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
+                st.pop();
+            }
+
             if (st.isEmpty()) {
                 res[i] = n;
             } else {
-                while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
-                    st.pop();
-                }
-
-                res[i] = st.isEmpty() ? n : st.peek();
+                res[i] = st.peek();
             }
 
             st.push(i);
