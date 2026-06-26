ca349665f4f1650d73e3a030e81a507887fee81c
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-06-26 19:05:50 +0530

    Time: 2 ms (36.7%), Space: 47.2 MB (37.76%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,26 +1,34 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
-        int[] colors = new int[n];			
-				
-        for (int i = 0; i < n; i++) {              //This graph might be a disconnected graph. So check each unvisited node.
-            if (colors[i] == 0 && !validColor(graph, colors, 1, i)) {
-                return false;
+
+        int[] color = new int[n];
+        Arrays.fill(color, -1);
+        
+
+        for (int start = 0; start < n; start++) {
+            if (color[start] != -1) {
+                continue;
+            }
+            
+            Queue<Integer> q = new LinkedList<>();
+            q.add(start);
+            color[start] = 0;
+            while (!q.isEmpty()) {
+                int node = q.poll();
+
+                for (int nei : graph[node]) {
+                    if (color[nei] == -1) {
+                        color[nei] = 1 - color[node];
+                        q.add(nei);
+                    } else if (color[nei] == color[node]) {
+                        return false;
+                    }
+                }
             }
         }
-        return true;
-    }
-    
-    public boolean validColor(int[][] graph, int[] colors, int color, int node) {
-        if (colors[node] != 0) {
-            return colors[node] == color;
-        }       
-        colors[node] = color;       
-        for (int next : graph[node]) {
-            if (!validColor(graph, colors, -color, next)) {
-                return false;
-            }
-        }
+
+
         return true;
     }
 }
