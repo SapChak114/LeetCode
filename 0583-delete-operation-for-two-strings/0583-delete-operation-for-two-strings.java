b419eb7a1507a5e155c517cef5747ea0438aa54e
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-07-14 23:57:37 +0530

    Time: 7 ms (69.04%), Space: 46.7 MB (13.95%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -1,26 +1,29 @@
 class Solution {
+    String word1, word2;
+    int n,m;
+    Integer[][] dp;
     public int minDistance(String word1, String word2) {
-        int n = word1.length(), m = word2.length();
-        int[][] dp = new int[n+1][m+1];
+        this.word1 = word1;
+        this.word2 = word2;
+        this.n = word1.length();
+        this.m = word2.length();
+        this.dp = new Integer[n][m];
+        return dfs(0, 0);
+    }
 
-        for (int i = 0; i<=n; i++) {
-            dp[i][0] = i;
+    int dfs(int i, int j) {
+        if (i == n || j == m) {
+            return Math.max(n - i, m - j);
         }
 
-        for (int j = 0; j<=m; j++) {
-            dp[0][j] = j;
+        if (dp[i][j] != null) {
+            return dp[i][j];
         }
 
-        for (int i = 1; i<=n; i++) {
-            for (int j = 1; j<=m; j++) {
-                if (word1.charAt(i-1) == word2.charAt(j-1)) {
-                    dp[i][j] = dp[i-1][j-1];
-                } else {
-                    dp[i][j] = 1 + Math.min(dp[i-1][j], dp[i][j-1]);
-                }
-            }
+        if (word1.charAt(i) == word2.charAt(j)) {
+            return dp[i][j] = dfs(i+1, j+1);
         }
 
-        return dp[n][m];
+        return dp[i][j] = 1 + Math.min(dfs(i+1, j), dfs(i, j+1));
     }
 }
