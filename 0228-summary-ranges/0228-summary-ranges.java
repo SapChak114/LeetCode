f7c0f3e6e346c7d2241cf1bd6aa33a7e38bb3e30
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-07-18 15:48:30 +0530

    Time: 5 ms (1.11%), Space: 43.2 MB (20.3%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -1,33 +1,25 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
-        List<String> ans = new ArrayList<>();
-        if (nums.length == 0) {
-            return ans;
-        }
+        int n = nums.length, i = 0;
 
-        int prev = 0;
-        for (int i = 1; i<=nums.length; i++) {
-            if (i == nums.length || nums[i-1] + 1 != nums[i]) {
-                //System.out.println(nums[i]);
-                if (prev == i-1) {
-                    ans.add(""+nums[prev]);
-                } else {
-                    ans.add(nums[prev]+"->"+nums[i-1]);
+        List<String> ans = new ArrayList<>();
+        for (i = 0; i<n-1; i++) {
+            int st = nums[i];
+            if (nums[i]+1 == nums[i+1]) {
+                System.out.println(nums[i]);
+                while (i < n-1 && (nums[i]+1 == nums[i+1])) {
+                    i++;
                 }
-                prev = i;
+                ans.add(st+"->"+nums[i]);
+            } else {
+                ans.add(""+nums[i]);
             }
         }
+
+        if (i != n) {
+            ans.add(""+nums[i]);
+        }
+
         return ans;
     }
 }
-
-/*
-prev = 0
-0 + 1 = 1 == 1
-1 + 1 == 2
-2 + 1 != 4
-0->2
-
-prev = 4
-4 
-*/
