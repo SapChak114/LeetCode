fea4057e0a6a7404093d1e27424d51df05883ba7
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-07-30 19:13:50 +0530

    Time: 0 ms (100%), Space: 43.7 MB (8.6%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n = digits.length, carr = 1;
+        int n = digits.length;
+
         for (int i = n-1; i>=0; i--) {
             if (digits[i] == 9) {
                 digits[i] = 0;
@@ -9,8 +10,9 @@ class Solution {
                 return digits;
             }
         }
-        int[] ans = new int[n + 1];
-        ans[0] = carr;
+
+        int[] ans = new int[n+1];
+        ans[0] = 1;
         return ans;
     }
 }
