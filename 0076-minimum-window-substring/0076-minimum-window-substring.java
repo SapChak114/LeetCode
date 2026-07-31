2ea7e9738a317204a3e8ea04b0f73fb519130b03
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-07-31 22:04:15 +0530

    Time: 17 ms (49.89%), Space: 47 MB (50.22%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,48 +1,51 @@
 class Solution {
     public String minWindow(String s, String t) {
         Map<Character, Integer> freq = new HashMap<>();
-        char[] cht = t.toCharArray();
 
-        for(char c : cht){
-            freq.put(c, freq.getOrDefault(c, 0)+1);
+        char[] ch = t.toCharArray();
+
+        for (char c : ch) {
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
         }
 
         int need = freq.size();
         int i = 0, j = 0, n = s.length(), size = n, minI = 0, minJ = 0;
         boolean flag = false;
 
-        while(j < n){
+        while (j < n) {
             char endChar = s.charAt(j++);
-            if(freq.containsKey(endChar)){
-                freq.put(endChar, freq.get(endChar)-1);
-                if(freq.get(endChar) == 0){
+            if (freq.containsKey(endChar)) {
+                freq.put(endChar, freq.get(endChar) - 1);
+
+                if (freq.get(endChar) == 0) {
                     need--;
                 }
             }
 
-            if(need > 0){
+            if (need > 0) {
                 continue;
             }
 
-            while(need == 0){
+            while (need == 0) {
                 char startChar = s.charAt(i++);
-                if(freq.containsKey(startChar)){
-                    freq.put(startChar, freq.get(startChar)+1);
-                    if(freq.get(startChar) > 0){
+
+                if (freq.containsKey(startChar)) {
+                    freq.put(startChar, freq.get(startChar) + 1);
+
+                    if (freq.get(startChar) > 0) {
                         need++;
                     }
                 }
             }
 
-            if(size > j-i){
+            if (size > j-i) {
                 flag = true;
-                size = j-i;
                 minI = i;
                 minJ = j;
+                size = j - i;
             }
         }
 
-        return flag ? s.substring(minI-1, minJ) : "";
-
+        return flag ? s.substring(minI - 1, minJ) : "";
     }
 }
