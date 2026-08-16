6dd1d3fdb44c53bb4abfa7096ba6ac845d1d3a36
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-08-16 20:33:24 +0530

    Time: 12 ms (99.94%), Space: 47.2 MB (56.21%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.java
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -1,12 +1,62 @@
-class MyCalendar {
-    TreeMap<Integer,Integer> calendar = new TreeMap<>();
-    public MyCalendar() {
-        calendar.put(Integer.MAX_VALUE, Integer.MAX_VALUE);
-    }
-    public boolean book(int start, int end) {
-        Map.Entry<Integer,Integer> pair = calendar.higherEntry(start);
-        boolean res = end <= pair.getValue();
-        if (res) calendar.put(end, start);
-        return res;
+class TreeNode {
+    TreeNode left;
+    TreeNode right;
+    int start;
+    int end;
+
+    public TreeNode(int start, int end) {
+        this.left = null;
+        this.right = null;
+        this.start = start;
+        this.end = end;
     }
 }
+
+class Tree {
+    TreeNode root;
+
+    public Tree(int start, int end) {
+        root = new TreeNode(start, end);
+    }
+
+    public boolean insert(int start, int end) {
+        TreeNode curr = root;
+        while (true) {
+            if (start >= curr.end) {
+                if (curr.right == null) {
+                    curr.right = new TreeNode(start, end);
+                    return true;
+                }
+                curr = curr.right;
+            } else if (end <= curr.start) {
+                if (curr.left == null) {
+                    curr.left = new TreeNode(start, end);
+                    return true;
+                }
+                curr = curr.left;
+            } else {
+                return false;
+            }
+        }
+    }
+}
+class MyCalendar {
+    Tree root;
+    public MyCalendar() {
+        this.root = null;
+    }
+    
+    public boolean book(int startTime, int endTime) {
+        if (root == null) {
+            root = new Tree(startTime, endTime);
+            return true;
+        }
+        return root.insert(startTime, endTime);
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(startTime,endTime);
+ */
