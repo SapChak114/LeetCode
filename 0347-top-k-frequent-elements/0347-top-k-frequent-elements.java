0058a87e11d2657570b1b69f81d365582a746b9c
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-08-22 18:58:38 +0530

    Time: 12 ms (84.74%), Space: 47.4 MB (82.73%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -2,28 +2,28 @@ class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         int n = nums.length;
 
-        Map<Integer, Integer> freq = new HashMap<>();
+        List<Integer>[] bucket = new ArrayList[n+1];
+
+        Map<Integer, Integer> map = new HashMap<>();
 
         for (int num : nums) {
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        List<Integer>[] bucket = new ArrayList[n + 1];
-        for (Map.Entry<Integer, Integer> e : freq.entrySet()) {
+        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
             int key = e.getKey();
-            int value = e.getValue();
+            int val = e.getValue();
 
-            if (bucket[value] == null) {
-                bucket[value] = new ArrayList<>();
+            if (bucket[val] == null) {
+                bucket[val] = new ArrayList<>();
             }
 
-            bucket[value].add(key);
+            bucket[val].add(key);
         }
 
         int[] ans = new int[k];
         int j = 0;
         for (int i = bucket.length-1; i>=0; i--) {
-            
             if (bucket[i] == null) {
                 continue;
             }
