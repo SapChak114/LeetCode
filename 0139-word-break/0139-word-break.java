e2db4813a3d1c748024f5a5d3167cde747b2d875
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-08-27 22:03:59 +0530

    Time: 4 ms (72.48%), Space: 44 MB (71.66%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -4,10 +4,11 @@ class Solution {
 
         boolean[] dp = new boolean[n+1];
         dp[n] = true;
+
         for (int i = n-1; i>=0; i--) {
-            for (String w : wordDict) {
-                if (i + w.length() <= n && s.substring(i, i+ w.length()).equals(w)) {
-                    dp[i] = dp[i + w.length()];
+            for (String word : wordDict) {
+                if (i + word.length() <= n && s.substring(i, i + word.length()).equals(word)) {
+                    dp[i] = dp[i + word.length()];
                 }
                 if (dp[i]) {
                     break;
