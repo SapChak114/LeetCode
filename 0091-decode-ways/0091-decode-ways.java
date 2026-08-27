ff2a1116fd3f6eea5b8b3f7d39bbbcd74a718d0b
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-08-27 21:38:42 +0530

    Time: 1 ms (80.54%), Space: 43.5 MB (7.69%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,26 +1,26 @@
 class Solution {
-    Integer[] dp;
     public int numDecodings(String s) {
-        dp = new Integer[s.length()];
-        return rec(0,s);    
-    }
-    
-    int rec(int idx, String s){
-        if(idx>=s.length()){
-            return 1;
+        if (s == null || s.length() == 0) {
+            return 0;
         }
-        
-        if(dp[idx]!=null) return dp[idx];
-        
-        int ans = 0, op1 = s.charAt(idx)-'0', op2 = 0;
-        
-        if(idx<s.length()-1){
-            op2 = op1*10 + s.charAt(idx+1) - '0';
+
+        int n = s.length();
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        for (int i = 2; i<=n; i++) {
+            int first = Integer.valueOf(s.substring(i-1, i));
+            int second = Integer.valueOf(s.substring(i-2, i));
+
+            if (first >= 1 && first <= 9) {
+                dp[i] += dp[i-1];
+            }
+            if (second >= 10 && second <= 26) {
+                dp[i] += dp[i-2];
+            }
         }
-        
-        if(op1>0)  ans += rec(idx+1,s);
-        if(op1>0 && op2>0 &&  op2<=26) ans += rec(idx+2,s);
-        
-        return dp[idx] = ans;
+
+        return dp[n];
     }
 }
+
