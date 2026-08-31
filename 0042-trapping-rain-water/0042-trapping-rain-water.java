3ebba59cfc47655794f35c386f8eb19c3bb80cef
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-08-31 23:30:07 +0530

    Time: 1 ms (58.2%), Space: 48 MB (37.02%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,7 +1,7 @@
 class Solution {
     public int trap(int[] h) {
-        int n = h.length;
-        int l = 0, r = n-1, maxOfMin = Integer.MIN_VALUE, ans = 0;
+        int l = 0, r = h.length-1;
+        int maxOfMin = Integer.MIN_VALUE, ans = 0;
 
         while (l < r) {
             int min = Math.min(h[l], h[r]);
