3242d8cbea50339e06cf34b5bfaa947f1510d239
SapChak114 <107317471+SapChak114@users.noreply.github.com>
2026-09-05 14:53:07 +0530

    Time: 16 ms (55.25%), Space: 47 MB (48.59%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -2,21 +2,19 @@ class Solution {
     public String minWindow(String s, String t) {
         Map<Character, Integer> freq = new HashMap<>();
 
-        char[] ch = t.toCharArray();
-
-        for (char c : ch) {
+        for (char c : t.toCharArray()) {
             freq.put(c, freq.getOrDefault(c, 0) + 1);
         }
 
         int need = freq.size();
-        int i = 0, j = 0, n = s.length(), size = n, minI = 0, minJ = 0;
-        boolean flag = false;
+        int i = 0, j = 0, minI = 0, minJ = 0, n = s.length(), size = n;
+        boolean found = false;
 
         while (j < n) {
             char endChar = s.charAt(j++);
             if (freq.containsKey(endChar)) {
+                
                 freq.put(endChar, freq.get(endChar) - 1);
-
                 if (freq.get(endChar) == 0) {
                     need--;
                 }
@@ -28,10 +26,8 @@ class Solution {
 
             while (need == 0) {
                 char startChar = s.charAt(i++);
-
                 if (freq.containsKey(startChar)) {
                     freq.put(startChar, freq.get(startChar) + 1);
-
                     if (freq.get(startChar) > 0) {
                         need++;
                     }
@@ -39,13 +35,13 @@ class Solution {
             }
 
             if (size > j-i) {
-                flag = true;
                 minI = i;
                 minJ = j;
-                size = j - i;
+                size = j-i;
+                found = true;
             }
         }
 
-        return flag ? s.substring(minI - 1, minJ) : "";
+        return found ? s.substring(minI-1, minJ) : "";
     }
 }
